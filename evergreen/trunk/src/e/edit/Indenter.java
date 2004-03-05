5ad09c3b7a83dd05f95bd1eeaaf7f8710bbeda57
elliotth <elliotth@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-03-05 18:19:49 +0000

    src/e/edit/Indenter.java: adds some currently unused code to guess the indentation unit appropriate to a given file's contents.

--- evergreen/trunk/src/e/edit/Indenter.java
+++ evergreen/trunk/src/e/edit/Indenter.java
@@ -1,6 +1,8 @@
 package e.edit;
 
 import e.util.*;
+import java.util.*;
+import java.util.regex.*;
 import javax.swing.text.*;
 
 public class Indenter {
@@ -33,4 +35,45 @@ public class Indenter {
     public String getIndentation(ETextArea text, int lineNumber) throws BadLocationException {
         return text.getIndentationOfLine(text.getPreviousNonBlankLineNumber(lineNumber));
     }
+    
+    public String guessIndentationFromFile(String fileContents) {
+        String previousIndent = "";
+        Set indentations = new HashSet();
+        String emergencyAlternative = "    ";
+        String[] lines = fileContents.split("\n");
+        for (int i = 0; i < lines.length; ++i) {
+            String line = lines[i];
+            Matcher matcher = INDENTATION_PATTERN_1.matcher(line);
+            if (matcher.matches()) {
+                String indent = matcher.group(1);
+                if (indent.length() < emergencyAlternative.length()) {
+                    emergencyAlternative = indent;
+                }
+            }
+            matcher = INDENTATION_PATTERN_2.matcher(line);
+            if (matcher.matches()) {
+                String indent = matcher.group(1);
+                if (indent.length() > previousIndent.length()) {
+                    String difference = indent.substring(previousIndent.length());
+                    indentations.add(difference);
+                }
+                previousIndent = indent;
+            }
+        }
+        if (indentations.size() == 0) {
+            return emergencyAlternative;
+        } else {
+            String[] array = (String[]) indentations.toArray(new String[indentations.size()]);
+            String shortest = array[0];
+            for (int i = 1; i < array.length; ++i) {
+                String indentation = (String) array[i];
+                if (indentation.length() < shortest.length()) {
+                    shortest = indentation;
+                }
+            }
+            return shortest;
+        }
+    }
+    private static final Pattern INDENTATION_PATTERN_1 = Pattern.compile("^( +)[A-Za-z].*$");
+    private static final Pattern INDENTATION_PATTERN_2 = Pattern.compile("^( +)[{}]$");
 }
