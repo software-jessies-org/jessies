56b2cc1e2d3fc927f7028bea38472d42f8c9d566
elliotth <elliotth@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-04-15 08:23:31 +0000

    src/e/edit/ECaret.java, src/e/edit/ETextArea.java, src/e/edit/MatchingBracketHighlighter.java: moves the code to highlight matching brackets out into its own class. Removes unnecessary JTextComponent parameters from ECaret methods (ECaret, like Caret, knows what JTextComponent it refers to anyway).
    Martin suggested we should highlight both brackets in a matching pair,
    rather than just the other one (as we did previously). He reckons it will
    be useful, whereas my only temptation before had been that I disliked the
    asymmetry. It certainly does look better now, and it seemed too ironic to
    highlight balanced brackets with an unbalanced visual effect.
    
    Changes the matching bracket highlight color to a pastel green; we haven't
    used green yet, and I found the purple a little too close to the selection
    color. Now we highlight both, I'm not convinced the 'far' bracket
    shouldn't be highlighted in a different color. Maybe there is an inherent
    asymmetry in the information we're trying to present?
    
    The other thing that now strikes me is that there's no visual feedback
    when you type a matching closing bracket, because we've always considered
    the active side of a bracket to be the inside (which gives much less
    weird double-click semantics than you see elsewhere).

---
+++ evergreen/trunk/src/e/edit/MatchingBracketHighlighter.java
@@ -0,0 +1,80 @@
+package e.edit;
+
+import java.awt.*;
+import javax.swing.event.*;
+import javax.swing.text.*;
+
+/**
+ * Highlights matching pairs of brackets in a JTextComponent. Add one of these
+ * as a ChangeListener to your JTextComponent's Caret.
+ */
+public class MatchingBracketHighlighter implements ChangeListener {
+    private static final Highlighter.HighlightPainter MATCHING_BRACKET_PAINTER = new DefaultHighlighter.DefaultHighlightPainter(new Color(0.78f, 0.98f, 0.78f));
+    
+    /**
+     * We keep two matching bracket highlights; one for the bracket before
+     * or after the caret, the other for its partner. Either or both may be
+     * null. We use an array rather than two fields because (a) we don't
+     * care which is which and shouldn't mislead by pretending to distinguish
+     * them and (b) an array lets us avoid duplication when removing the
+     * highlights.
+     */
+    private Object[] matchingBracketHighlights = new Object[2];
+    
+    private JTextComponent textComponent;
+    
+    public MatchingBracketHighlighter(JTextComponent textComponent) {
+        this.textComponent = textComponent;
+    }
+    
+    /** Implements the ChangeListener interface so we can watch. */
+    public void stateChanged(ChangeEvent e) {
+        updateMatchingBracketHighlights();
+    }
+    
+    private void removeMatchingBracketHighlights() {
+        for (int i = 0; i < matchingBracketHighlights.length; ++i) {
+            if (matchingBracketHighlights[i] != null) {
+                textComponent.getHighlighter().removeHighlight(matchingBracketHighlights[i]);
+                matchingBracketHighlights[i] = null;
+            }
+        }
+    }
+    
+    private void updateMatchingBracketHighlights() {
+        removeMatchingBracketHighlights();
+        
+        try {
+            int offset = textComponent.getCaretPosition();
+            ECaret caret = (ECaret) textComponent.getCaret();
+            Highlighter highlighter = textComponent.getHighlighter();
+            char ch = caret.getCharAt(offset);
+            if (caret.isCloseBracket(ch)) {
+                matchingBracketHighlights[0] = highlighter.addHighlight(offset, offset + 1, MATCHING_BRACKET_PAINTER);
+            } else if (offset > 0) {
+                char previousChar = caret.getCharAt(offset - 1);
+                if (caret.isOpenBracket(previousChar)) {
+                    matchingBracketHighlights[0] = highlighter.addHighlight(offset - 1, offset, MATCHING_BRACKET_PAINTER);
+                }
+            }
+            if (matchingBracketHighlights[0] == null) {
+                return;
+            }
+            
+            int matchingBracketOffset = caret.findMatchingBracket(offset);
+            if (matchingBracketOffset != -1) {
+                int start = matchingBracketOffset;
+                int end = matchingBracketOffset;
+                if (caret.isCloseBracket(ch)) {
+                    --start;
+                } else {
+                    ++end;
+                }
+                matchingBracketHighlights[1] = highlighter.addHighlight(start, end, MATCHING_BRACKET_PAINTER);
+            }
+        } catch (BadLocationException ex) {
+            // Can't happen.
+            ex = ex;
+        }
+    }
+}
