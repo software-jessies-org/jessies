1d49d0cb19abc5d68d85f6642571006a72e98dcd
elliotth <elliotth@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-04-16 10:18:11 +0000

    src/e/edit/ETextArea.java: recognizes ASCII art as well as GNU-style indentation as a cue to use a fixed font for legibility. The one known limitation is with string literals containing multiple spaces; it's not obvious that the correct fix to this isn't to fix whatever problems are causing us to have such literals, a prominent example being the lack of a JLabel that renders tabs. (martin)

--- evergreen/trunk/src/e/edit/ETextArea.java
+++ evergreen/trunk/src/e/edit/ETextArea.java
@@ -148,29 +148,6 @@ public class ETextArea extends JTextArea {
     }
 
     /**
-     * Returns an appropriate font for this text area's content.
-     * 'Appropriate' basically means the user's configured font,
-     * unless the content seems to use a mixture of tabs and spaces
-     * for indentation, in which case, a fixed font is deemed
-     * appropriate regardless of the user's default preference.
-     */
-    public Font getAppropriateFontForContent() {
-        boolean fixed = Boolean.getBoolean("fixedFont.default");
-        String content = getText();
-
-        // If we've got a file with GNU-style lines where you have a mixture
-        // of tabs and spaces for indentation, we need to use a fixed font.
-        // But we need to exclude the case where we're looking at a JavaDoc
-        // comment where there's a space just to make the stars line up.
-        Pattern pattern = Pattern.compile("\\t [^*]", Pattern.MULTILINE);
-        if (pattern.matcher(content).find()) {
-            fixed = true;
-        }
-
-        return fixed ? getConfiguredFixedFont() : getConfiguredFont();
-    }
-    
-    /**
      * Sets an appropriate font for this text area's content.
      * Unless you're specifically setting the font to something
      * the user's asked for, this is the only way you should set
@@ -180,6 +157,62 @@ public class ETextArea extends JTextArea {
         setFont(getAppropriateFontForContent());
     }
 
+    /**
+     * Returns an appropriate font for this text area's content.
+     * 'Appropriate' basically means the user's configured font,
+     * unless the content seems to be such that it would be
+     * unreadable except with a fixed font.
+     */
+    public Font getAppropriateFontForContent() {
+        return shouldUseFixedFont() ? getConfiguredFixedFont() : getConfiguredFont();
+    }
+    
+    /**
+     * Tests whether we're likely to need a fixed font for the content
+     * to be legible. GNU-style indentation and ASCII art are two examples
+     * of the kind of thing that requires a fixed font.
+     */
+    private boolean shouldUseFixedFont() {
+        if (Boolean.getBoolean("fixedFont.default")) {
+            return true;
+        }
+        String content = getText();
+        if (hasMixedTabsAndSpaces(content)) {
+            return true;
+        }
+        if (hasMidLineAsciiArt(content)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Tests whether we've got a file with GNU-style lines where you have
+     * a mixture of tabs and spaces for indentation.
+     *
+     * We exclude the case where it's just a JavaDoc comment that uses a
+     * space just to make the stars line up, because we're using this to
+     * decide whether or not the file will be unreadable without using a
+     * fixed font, and JavaDoc looks fine either way.
+     */
+    private boolean hasMixedTabsAndSpaces(String content) {
+        Pattern pattern = Pattern.compile("\\t [^*]", Pattern.MULTILINE);
+        return pattern.matcher(content).find();
+    }
+
+    /**
+     * Tries to detect ASCII art heuristically. Multiple spaces which are
+     * not part of the indentation probably mean we're dealing with ASCII
+     * art. The numbers zero, one and two all exist in the world of computer
+     * science, but any more and we're dealing with art.
+     */
+    private boolean hasMidLineAsciiArt(String content) {
+        // "\\S\\s{3,}" doesn't work because \s matches newlines even
+        // in MULTILINE mode, so we use ' ' instead.
+        Pattern pattern = Pattern.compile("\\S {3,}", Pattern.MULTILINE);
+        return pattern.matcher(content).find();
+    }
+    
     public static Font getConfiguredFont() {
         return getConfiguredFont("font", "verdana", 12);
     }
