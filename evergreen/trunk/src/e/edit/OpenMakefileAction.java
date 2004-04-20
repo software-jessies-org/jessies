df8293fe86be2490fca91a0e1810ab429c78dafa
elliotth <elliotth@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-04-20 17:18:54 +0000

    src/e/edit/BuildAction.java, src/e/edit/EditMenuBar.java, src/e/edit/OpenMakefileAction.java: factor out the makefile-finding code in BuildAction so it's accessible from outside. Add a new action (on the Tools menu, with the other build-related actions) to open the makefile for the file you're editing.

---
+++ evergreen/trunk/src/e/edit/OpenMakefileAction.java
@@ -0,0 +1,22 @@
+package e.edit;
+
+import java.awt.event.*;
+
+public class OpenMakefileAction extends ETextAction {
+    public static final String ACTION_NAME = "Open Makefile";
+    
+    public OpenMakefileAction() {
+        super(ACTION_NAME);
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        ETextWindow window = getFocusedTextWindow();
+        if (window == null) {
+            return;
+        }
+        String makefileName = BuildAction.findMakefile(window.getContext());
+        if (makefileName != null) {
+            Edit.openFile(makefileName);
+        }
+    }
+}
