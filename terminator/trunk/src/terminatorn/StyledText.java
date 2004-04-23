b3700fb1595ef4c1820da59aa129c6792803a05d
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-04-23 21:15:36 +0000

    src/terminatorn/JTextBuffer.java, src/terminatorn/StyledText.java: despite my nasty hack in Edit, ECMA specifically says "yellow", not "orange". We now look similar enough to Mac OS X's Terminal.app that I'm starting to confuse the windows. (I've set my .Xdefaults to mimic my Terminal settings; as a future enhancement, we could read Terminal's settings if we're on Mac OS X.)

--- terminator/trunk/src/terminatorn/StyledText.java
+++ terminator/trunk/src/terminatorn/StyledText.java
@@ -11,7 +11,7 @@ public class StyledText {
 	public static final int BLACK = 0;
 	public static final int RED = 1;
 	public static final int GREEN = 2;
-	public static final int ORANGE = 3;
+	public static final int YELLOW = 3;
 	public static final int BLUE = 4;
 	public static final int MAGENTA = 5;
 	public static final int CYAN = 6;
