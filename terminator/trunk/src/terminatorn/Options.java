aa32ac1b6811bf8f323d272c76fe200ed4103fc4
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-04-24 09:07:23 +0000

    src/terminatorn/Options.java: documentation.

--- terminator/trunk/src/terminatorn/Options.java
+++ terminator/trunk/src/terminatorn/Options.java
@@ -5,6 +5,22 @@ import java.io.*;
 import java.util.*;
 import java.util.regex.*;
 
+/**
+ * Reads in settings from the file system and makes the conveniently
+ * available.
+ * 
+ * There's a grand tradition amongst Unix terminal emulators of pretending
+ * to be xterm, and we aren't any different. We go one step further by
+ * pretending to be an X11 application and reading .Xdefaults and .Xresources.
+ * Strictly, most X11 applications won't do this, but rxvt does, rather than
+ * using the bloated library routines.
+ * 
+ * We don't pass .Xdefaults and .Xresources through m4, as X11 does, but I've
+ * not seen that used since people had monochrome as well as color displays and
+ * needed different settings on each. We do understand X11's idiosyncratic
+ * comment style, and that should be enough for most resource files seen in
+ * the real world.
+ */
 public class Options {
 	private static final Options INSTANCE = new Options();
 	
@@ -14,6 +30,29 @@ public class Options {
 		return INSTANCE;
 	}
 	
+	/**
+	 * Returns a color, if explicitly configured by the user.
+	 * We only understand colors specified in the #rrggbb form,
+	 * and make no effort to find and parse rgb.txt (though we
+	 * could).
+	 * 
+	 * Color names supported by xterm (defaults in parentheses) include:
+	 * 
+	 *  background (white)
+	 *  foreground (black)
+	 *  cursorColor (black)
+	 *  highlightColor (reverse video)
+	 *  pointerColor
+	 *  pointerBackgroundColor
+	 * 
+	 * xterm also offers complete control over all the ECMA colors:
+	 * 
+	 *  color0 to color7 are the normal colors (black, red3, green3,
+	 *  yellow3, blue3, magenta3, cyan3, and gray90).
+	 * 
+	 *  color8 to color15 are the bold colors (gray30, red, green, yellow,
+	 *  blue, magenta, cyan, and white).
+	 */
 	public Color getColor(String name) {
 		String description = (String) options.get(name);
 		if (description == null || description.startsWith("#") == false) {
@@ -22,6 +61,10 @@ public class Options {
 		return Color.decode("0x" + description.substring(1));
 	}
 	
+	/**
+	 * Returns a suitable fixed font (ignoring all X11 configuration,
+	 * because we're unlikely to understand those font specifications).
+	 */
 	public Font getFont() {
 		boolean isMacOS = (System.getProperty("os.name").indexOf("Mac") != -1);
 		return Font.decode(isMacOS ? "Monaco" : "Monospaced");
