93e55b5ff856011d19aed6bb5465d935368ed520
elliotth <elliotth@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-04-28 09:33:17 +0000

    xv, src/e/tools/Xv.java: I'm sick of Gimp. The latest version takes even longer to start. And all I ever want is to look a .jpg file. So here's my two-minute reimplementation of "xv", which Debian won't let us have because it doesn't fit their definition of free. Even though you can have the source, and you don't have to pay to use it.

---
+++ salma-hayek/trunk/src/e/tools/Xv.java
@@ -0,0 +1,25 @@
+package e.tools;
+
+import javax.swing.*;
+import e.util.*;
+
+public class Xv extends JFrame {
+    
+    public Xv(String filename) {
+        super(filename);
+        Log.warn("Opening '" + filename + "'.");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(new JLabel(new ImageIcon(filename)));
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] arguments) {
+        Log.setApplicationName("Xv");
+        GuiUtilities.initLookAndFeel();
+        for (int i = 0; i < arguments.length; ++i) {
+            Xv xv = new Xv(arguments[i]);
+        }
+    }
+}
