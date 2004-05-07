7431459a621abb415413bd850bb5625ab19cac7c
elliotth <elliotth@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-05-07 18:50:27 +0000

    src/e/edit/JavaIndenter.java: fix the Java doc comment indentation so it doesn't mess up an already correctly-indented comment when you paste it (or cause it to be automatically re-indented for whatever other reason).

--- evergreen/trunk/src/e/edit/JavaIndenter.java
+++ evergreen/trunk/src/e/edit/JavaIndenter.java
@@ -76,7 +76,14 @@ public class JavaIndenter extends Indenter {
                 // a close of comment, we're not in a comment, and should
                 // do nothing.
             } else if (previousLine.startsWith("/*") || previousLine.startsWith("*")) {
-                indentation += " * ";
+                // We're in a doc comment.
+                if (activePartOfLine.startsWith("* ") || activePartOfLine.startsWith("*/")) {
+                    // We already have the JavaDoc ASCII art, and just need to
+                    // indent it one space.
+                    indentation += " ";
+                } else {
+                    indentation += " * ";
+                }
             }
         }
         
