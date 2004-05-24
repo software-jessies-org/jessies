8d5b977d4deae8001eae9566bbcad2cf01975c29
phil <phil@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-05-24 14:49:06 +0000

    Fixed bug where if LogWriter is asked to log an absolute-path command, it creates the .terminal-log/bin... directories.

--- terminator/trunk/src/terminator/LogWriter.java
+++ terminator/trunk/src/terminator/LogWriter.java
@@ -8,30 +8,36 @@ import java.util.*;
  * Logs terminal output to a file.
  */
 public class LogWriter {
-    private static DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ssZ");
-    
-    private FileWriter stream;
-    
-    public LogWriter(String prefix) throws IOException {
-        String timestamp = dateFormatter.format(new Date());
-        String directory = makeDirectory();
-        String filename =  prefix + '-' + timestamp + ".txt";
-        stream = new FileWriter(directory + filename);
-    }
-    
-    public void append(char ch) throws IOException {
-        stream.write(ch);
-        if (ch == '\n') {
-            stream.flush();
-        }
-    }
-    
-    private String makeDirectory() {
-        String directoryName = System.getProperty("user.home") + File.separator + ".terminal-logs" + File.separator;
-        File directory = new File(directoryName);
-        if (directory.exists() == false) {
-            directory.mkdir();
-        }
-        return directoryName;
-    }
+	private static DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ssZ");
+	
+	private FileWriter stream;
+	
+	public LogWriter(String prefix) throws IOException {
+		String timestamp = dateFormatter.format(new Date());
+		String directory = makeDirectory();
+		int lastSepIndex = prefix.lastIndexOf(File.separator);
+		if (lastSepIndex != -1) {
+			directory += prefix.substring(0, lastSepIndex + 1);
+			(new File(directory)).mkdirs();
+			prefix = prefix.substring(lastSepIndex + 1);
+		}
+		String filename =  prefix + '-' + timestamp + ".txt";
+		stream = new FileWriter(directory + filename);
+	}
+	
+	public void append(char ch) throws IOException {
+		stream.write(ch);
+		if (ch == '\n') {
+			stream.flush();
+		}
+	}
+	
+	private String makeDirectory() {
+		String directoryName = System.getProperty("user.home") + File.separator + ".terminal-logs" + File.separator;
+		File directory = new File(directoryName);
+		if (directory.exists() == false) {
+			directory.mkdir();
+		}
+		return directoryName;
+	}
 }
