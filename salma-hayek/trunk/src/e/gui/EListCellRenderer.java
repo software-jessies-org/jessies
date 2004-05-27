4578cc1552dfe03869e0439ca14d0e40cd93573f
elliotth <elliotth@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-05-27 09:46:58 +0000

    src/e/gui/EListCellRenderer.java: makes the alternate row coloring an option, so this is more suitable for use as a superclass. Adds a couple of fixes for JLabel problems, namely the fact that "" is zero-height, and '\t' doesn't render correctly.

--- salma-hayek/trunk/src/e/gui/EListCellRenderer.java
+++ salma-hayek/trunk/src/e/gui/EListCellRenderer.java
@@ -5,13 +5,31 @@ import javax.swing.*;
 
 public class EListCellRenderer extends DefaultListCellRenderer {
     private static final Color ALTERNATE_ROW_COLOR = new Color(0.92f, 0.95f, 0.99f);
+
+    private boolean alternateLineColor;
+
+    public EListCellRenderer(boolean alternateLineColor) {
+        this.alternateLineColor = alternateLineColor;
+    }
     
     public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
         super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+
+        // Work around a couple of JLabel problems.
+        String text = getText();
+        if (text.length() == 0) {
+            // Prevent blank lines from being squashed (mis-feature in JLabel).
+            setText(" ");
+        } else if (text.indexOf('\t') != -1) {
+            // Prevent tabs from being squashed (mis-feature in JLabel).
+            setText(text.replaceAll("\t", "    "));
+        }
         
-        if (!isSelected && index % 2 == 0) {
+        // Optionally use the line-printer paper trick of alternating row color.
+        if (alternateLineColor && !isSelected && index % 2 == 0) {
             setBackground(ALTERNATE_ROW_COLOR);
         }
+
         return this;
     }
 }
