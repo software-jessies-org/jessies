94ed64895be559393a1cfb34b98ae915111acdd6
elliotth <elliotth@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-05-28 08:41:38 +0000

    src/e/util/ProcessUtilities.java: switch to the more sophisticated version we've been using in the scm repository.

--- salma-hayek/trunk/src/e/util/ProcessUtilities.java
+++ salma-hayek/trunk/src/e/util/ProcessUtilities.java
@@ -4,22 +4,62 @@ import java.io.*;
 import java.util.*;
 
 public class ProcessUtilities {
-    /** Returns the lines output to standard output by 'command' when run. */
-    public static String[] backQuote(String[] command) {
+    /**
+     * Runs 'command'. Returns the command's status code.
+     * Lines written to standard output are appended to 'lines'.
+     * Lines written to standard error are appended to 'errors'.
+     * FIXME: should errors *we* detect go in 'lines', or in 'errors'?
+     */
+    public static int backQuote(File directory, String[] command, ArrayList lines, ArrayList errors) {
         ArrayList result = new ArrayList();
         try {
-            Process p = Runtime.getRuntime().exec(command);
+            Process p = Runtime.getRuntime().exec(command, null, directory);
             p.getOutputStream().close();
-            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            readLinesFromStream(lines, p.getInputStream());
+            readLinesFromStream(errors, p.getErrorStream());
+            return p.waitFor();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            lines.add(ex.getMessage());
+            return 1;
+        }
+    }
+
+    public static void spawn(final File directory, final String[] command) {
+        new Thread() {
+            public void run() {
+                try {
+                    Process p = Runtime.getRuntime().exec(command, null, directory);
+                    p.getOutputStream().close();
+                    p.waitFor();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    private static void readLinesFromStream(ArrayList result, InputStream stream) {
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(stream));
             String line;
             while ((line = in.readLine()) != null) {
                 result.add(line);
             }
+            in.close();
         } catch (Exception ex) {
             ex.printStackTrace();
             result.add(ex.getMessage());
         } finally {
-            return (String[]) result.toArray(new String[result.size()]);
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    result.add(ex.getMessage());
+                }
+            }
         }
     }
     
