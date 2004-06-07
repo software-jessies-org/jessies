9a88bba2810e0b7f2348b04ddce17afeb5721961
elliotth <elliotth@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-06-07 15:02:10 +0000

    src/e/gui/ETable.java: fix the background color-setting logic so we don't prevent selected rows from showing up as selected.

--- salma-hayek/trunk/src/e/gui/ETable.java
+++ salma-hayek/trunk/src/e/gui/ETable.java
@@ -14,8 +14,9 @@ public class ETable extends JTable {
      */
     public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
         Component c = super.prepareRenderer(renderer, row, column);
-        boolean useAlternateColor = (row % 2 == 0 && isCellSelected(row, column) == false);
-        c.setBackground(useAlternateColor ? GuiUtilities.ALTERNATE_ROW_COLOR : getBackground());
+        if (isCellSelected(row, column) == false) {
+            c.setBackground((row % 2 == 0) ? GuiUtilities.ALTERNATE_ROW_COLOR : getBackground());
+        }
         return c;
     }
 }
