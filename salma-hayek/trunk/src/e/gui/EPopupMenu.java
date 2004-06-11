1f67ae15534d26d4421018c2b16cf84ae7495bcf
elliotth <elliotth@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-06-11 21:33:40 +0000

    src/e/gui/EPopupMenu.java: at the cost of heinous duplication, support both real AWT PopupMenus and Swing JPopupMenus. On Linux, you want the latter; on Mac OS, the former. Why? Because Linux's native Motif junk is terrible, and the Swing replacement far superior, while on Mac OS, the native stuff is great, and Swing's replacement a pale imitation. It's not just that the bad one in each case looks bad: they feel wrong too, causing you to mess up when you're trying to use them. Unacceptable.

--- salma-hayek/trunk/src/e/gui/EPopupMenu.java
+++ salma-hayek/trunk/src/e/gui/EPopupMenu.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
+import e.util.*;
 
 public class EPopupMenu {
     private ArrayList menuItems = new ArrayList();
@@ -11,7 +12,7 @@ public class EPopupMenu {
     /** Creates a new popup menu. */
     public EPopupMenu() { }
     
-    /** Adds a group of EAction items to the end of the current list of items. */
+    /** Adds a group of Action items to the end of the current list of items. */
     public void add(Action[] items) {
         for (int i = 0; i < items.length; i++) {
             menuItems.add(items[i]);
@@ -25,14 +26,52 @@ public class EPopupMenu {
     
     /** Ensures that the menu is on the display & ready for action. */
     public void show(Component origin, int x, int y) {
-        JPopupMenu menu = createMenu(x, y);
-        //origin.add(menu);
-        menu.show(origin, x, y);
+        if (GuiUtilities.isMacOs()) {
+            // Mac OS' JPopupMenu is so bad we should
+            // use the AWT PopupMenu instead.
+            PopupMenu menu = createMenu(x, y);
+            origin.add(menu);
+            menu.show(origin, x, y);
+        } else {
+            JPopupMenu menu = createJMenu(x, y);
+            menu.show(origin, x, y);
+        }
     }
     
     /** Sets up the menu with items that act upon what's at (x,y). */
-    public JPopupMenu createMenu(final int x, final int y) {
+    public JPopupMenu createJMenu(final int x, final int y) {
         JPopupMenu menu = new JPopupMenu();
+        boolean lastWasSeparator = false;
+        for (int i = 0; i < menuItems.size(); i++) {
+            final Action action = (Action) menuItems.get(i);
+            if (action == null) {
+                if (lastWasSeparator == false) {
+                    menu.addSeparator();
+                }
+                lastWasSeparator = true;
+            } else {
+                menu.add(createJMenuItem(action, x, y));
+                lastWasSeparator = false;
+            }
+        }
+        return menu;
+    }
+    
+    /** Creates a JMenuItem from an Action and a location. */
+    public JMenuItem createJMenuItem(final Action action, final int x, final int y) {
+        JMenuItem menuItem = new JMenuItem((String) action.getValue(Action.NAME));
+        menuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+                action.actionPerformed(new ActionEvent(new Point(x, y), e.getID(), e.getActionCommand(), e.getModifiers()));
+            }
+        });
+        menuItem.setEnabled(action.isEnabled());
+        return menuItem;
+    }
+    
+    /** Sets up the menu with items that act upon what's at (x,y). */
+    public PopupMenu createMenu(final int x, final int y) {
+        PopupMenu menu = new PopupMenu();
         boolean lastWasSeparator = false;
         for (int i = 0; i < menuItems.size(); i++) {
             final Action action = (Action) menuItems.get(i);
@@ -49,9 +88,9 @@ public class EPopupMenu {
         return menu;
     }
     
-    /** Creates a MenuItem from an EAction and a location. */
-    public JMenuItem createMenuItem(final Action action, final int x, final int y) {
-        JMenuItem menuItem = new JMenuItem((String) action.getValue(Action.NAME));
+    /** Creates a MenuItem from an Action and a location. */
+    public MenuItem createMenuItem(final Action action, final int x, final int y) {
+        MenuItem menuItem = new MenuItem((String) action.getValue(Action.NAME));
         menuItem.addActionListener(new ActionListener() {
             public void actionPerformed(final ActionEvent e) {
                 action.actionPerformed(new ActionEvent(new Point(x, y), e.getID(), e.getActionCommand(), e.getModifiers()));
