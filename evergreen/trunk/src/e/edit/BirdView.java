1c4f160d42da7f24cfba96e5407c5a0a1283a91b
elliotth <elliotth@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-07-27 20:30:33 +0000

    src/e/edit/BirdView.java: reinstate Phil's version of the reflection hack to give the bird's eye view a size equivalent to the track of the neighboring scroll bar. Only fall back to the Mac OS hack (using measured pixel offsets) if we're actually on Mac OS. This should mean that the Java look and feel is better, Mac OS is the same as it was, and GTK+ is no worse than the Java LAF.

--- evergreen/trunk/src/e/edit/BirdView.java
+++ evergreen/trunk/src/e/edit/BirdView.java
@@ -4,14 +4,18 @@ import java.awt.*;
 import java.lang.reflect.*;
 import java.util.*;
 import javax.swing.*;
+import javax.swing.plaf.*;
+import javax.swing.plaf.basic.*;
+import e.util.*;
 
 /**
-A bird's-eye view of the document. Intended to sit alongside the vertical scrollbar.
-The Find dialog informs us of matches, and we display little red ticks corresponding
-to their locations.
-*/
+ * A bird's-eye view of the document. Intended to sit alongside the vertical scrollbar.
+ * The find code informs us of matches, and we display little marks corresponding
+ * to the matches' locations.
+ */
 public class BirdView extends JComponent {
     private ETextArea textArea;
+    private JScrollBar scrollBar;
 
     private Method method;
 
@@ -19,8 +23,9 @@ public class BirdView extends JComponent {
 
     public BirdView(ETextArea textArea, JScrollBar scrollBar) {
         this.textArea = textArea;
+        this.scrollBar = scrollBar;
         try {
-            method = javax.swing.plaf.basic.BasicScrollBarUI.class.getDeclaredMethod("getTrackBounds", new Class[] {});
+            method = BasicScrollBarUI.class.getDeclaredMethod("getTrackBounds", new Class[] {});
             method.setAccessible(true);
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -30,19 +35,33 @@ public class BirdView extends JComponent {
     public Dimension getPreferredSize() {
         return new Dimension(4, Integer.MAX_VALUE);
     }
+    
+    public Rectangle getUsableArea() {
+        Rectangle usableArea = new Rectangle(0, 0, getWidth() - 1, getHeight() - 1);
+        ScrollBarUI scrollUi = scrollBar.getUI();
+        if (scrollUi instanceof BasicScrollBarUI) {
+            BasicScrollBarUI basicUi = (BasicScrollBarUI) scrollUi;
+            try {
+                Rectangle trackArea = (Rectangle) method.invoke(basicUi, new Object[0]);
+                JScrollPane pane = (JScrollPane) SwingUtilities.getAncestorOfClass(JScrollPane.class, scrollBar);
+                usableArea.y = trackArea.y + pane.getInsets().top;
+                usableArea.height = trackArea.height;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } else if (GuiUtilities.isMacOs()) {
+            // These values were measured using Pixie. I don't know how to get them at run-time.
+            usableArea.y += 10;
+            usableArea.height -= 42;
+        }
+        return usableArea;
+    }
 
     public void paintComponent(Graphics g) {
         g.setColor(SystemColor.control); //UIManager.getColor("Scrollbar.track"));
         g.fillRect(0, 0, getWidth(), getHeight());
 
-        Rectangle usableArea = null;
-        usableArea = new Rectangle(0, 0, getWidth() - 1, getHeight() - 1);
-        usableArea.y += 10;
-        usableArea.height -= 42;
-        
-        if (usableArea == null) {
-            return;
-        }
+        Rectangle usableArea = getUsableArea();
 
         if (false) {
             /* When testing, it's nice to see where we think the rectangle is. */
@@ -51,7 +70,9 @@ public class BirdView extends JComponent {
         }
 
         int linesInDocument = textArea.getLineCount();
-        double scaleFactor = (double) usableArea.height / (double) linesInDocument;
+        // The '-1' in the following line is to force the last line of the file to be right at the bottom of
+        // the bird view.  Otherwise the marker for the final line seems to be too high in short files.
+        double scaleFactor = (double) usableArea.height / (double) (linesInDocument - 1);
 
         g.setColor(Color.BLACK);
         for (int i = 0; i < matchingLines.length(); i++) {
