9a865baf800b72fad63e591df8fcf5b410f50e9e
elliotth <elliotth@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-08-15 10:16:01 +0000

    src/e/edit/TagsPanel.java: remove a lot of code that belongs in TagsUpdater now.
    src/e/edit/TagsUpdater.java: gain code from TagsUpdater, and change it to
    move away from SwingWorker (which always seems over-complicated and rather
    awkward to use), and to re-use the same temporary file for the tags so that
    the MD5 digests are comparable: it turns out that Exuberant ctags writes the
    name of its output file into the output file! [The fix to my digest code,
    and the move away from SwingWorker came from Ed; I just moved the classes
    out of TagsPanel, and made them simpler in the process. The good news is he's
    bought a PowerBook, so we soon won't have to worry about regressions caused
    by him being out of date and us not reading patches carefully enough.]

--- evergreen/trunk/src/e/edit/TagsPanel.java
+++ evergreen/trunk/src/e/edit/TagsPanel.java
@@ -1,15 +1,11 @@
 package e.edit;
 
 import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
 import java.util.*;
 import javax.swing.*;
-import javax.swing.Timer;
 import javax.swing.tree.*;
 
 import e.gui.*;
-import e.util.*;
 
 public class TagsPanel extends JPanel {
     private JProgressBar progressBar = new JProgressBar();
@@ -67,26 +63,6 @@ public class TagsPanel extends JPanel {
     public void hideProgressBar() {
         setVisibleComponent(emptyPanel);
         progressBar.setIndeterminate(false);
-    }
-    
-    private static class BranchNode extends DefaultMutableTreeNode {
-        
-        public BranchNode(Object userObject) {
-            super(userObject);
-        }
-        
-        public void add(MutableTreeNode node) {
-            TagReader.Tag tag = (TagReader.Tag) ((DefaultMutableTreeNode) node).getUserObject();
-            insert(node, getInsertIndex(tag));
-        }
-        
-        private SortedSet kidsNames = new TreeSet();
-        
-        public int getInsertIndex(TagReader.Tag tag) {
-            String insertString = tag.getTypeSortIndex() + tag.identifier + kidsNames.size();
-            kidsNames.add(insertString);
-            return new ArrayList(kidsNames).indexOf(insertString);
-        }
     }
     
     public static class TagsTreeRenderer extends DefaultTreeCellRenderer {
@@ -147,109 +123,6 @@ public class TagsPanel extends JPanel {
                 setIcon(icon);
             }
             return this;
-        }
-    }
-    
-    public static class TagsScanner extends SwingWorker implements TagReader.TagListener {
-        private boolean tagsHaveNotChanged;
-        private String digest;
-        private long startTime;
-        private Timer progressTimer;
-        private TagsUpdater model;
-        
-        private DefaultMutableTreeNode root = new DefaultMutableTreeNode("root");
-        private DefaultTreeModel treeModel = new DefaultTreeModel(root);
-        private HashMap branches = new HashMap();
-        {
-            branches.put("", root);
-        }
-        
-        public TagsScanner(TagsUpdater model) {
-            this.model = model;
-            progressTimer = new Timer(500, new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    Edit.getTagsPanel().showProgressBar();
-                }
-            });
-            progressTimer.setRepeats(false);
-        }
-        
-        public void doScan() {
-            start();
-            startTime = System.currentTimeMillis();
-            progressTimer.start();
-        }
-        
-        public String getFilenameSuffix() {
-            String suffix = ".txt";
-            String filename = model.getTextWindow().getFilename();
-            int lastDot = filename.lastIndexOf('.');
-            if (lastDot != -1) {
-                suffix = filename.substring(lastDot);
-            }
-            return suffix;
-        }
-        
-        public void tagFound(TagReader.Tag tag) {
-            DefaultMutableTreeNode leaf = new DefaultMutableTreeNode(tag);
-            
-            if (tag.isContainerType()) {
-                leaf = new BranchNode(tag);
-                branches.put(tag.getClassQualifiedName(), leaf);
-            }
-            
-            DefaultMutableTreeNode branch = (DefaultMutableTreeNode) branches.get(tag.containingClass);
-            if (branch == null) {
-                branch = new BranchNode(tag.containingClass);
-                branches.put(tag.containingClass, branch);
-                root.add(branch);
-            }
-            
-            branch.add(leaf);
-        }
-        
-        public void taggingFailed(Exception ex) {
-            Edit.getCurrentWorkspace().reportError("", "There was an error reading the tags (" + ex.getMessage() + ")");
-        }
-        
-        public Object construct() {
-            try {
-                /*
-                 * It's important to use the same suffix as the original
-                 * file, because that's how ctags guesses the file's type,
-                 * and hence which parser to use.
-                 */
-                File temporaryFile = File.createTempFile("edit-", getFilenameSuffix());
-                temporaryFile.deleteOnExit();
-                model.getTextWindow().writeCopyTo(temporaryFile);
-                TagReader tagReader = new TagReader(temporaryFile, model.getTextWindow().getFileType(), this);
-                String newDigest = tagReader.getTagsDigest();
-                tagsHaveNotChanged = newDigest.equals(digest);
-                digest = newDigest;
-                temporaryFile.delete();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            return model;
-        }
-        
-        public void finished() {
-            get();
-            if (tagsHaveNotChanged) {
-                return;
-            }
-            
-            progressTimer.stop();
-            Edit.getTagsPanel().hideProgressBar();
-            if (model == null) {
-                Edit.getCurrentWorkspace().reportError("", "Couldn't make tags.");
-            } else {
-                model.setTreeModel(treeModel);
-            }
-            
-            long endTime = System.currentTimeMillis();
-            double duration = ((double) (endTime - startTime)) / 1000.0;
-            //Log.warn("Time taken reading tags: " + duration + "s");
         }
     }
 }
--- evergreen/trunk/src/e/edit/TagsUpdater.java
+++ evergreen/trunk/src/e/edit/TagsUpdater.java
@@ -2,8 +2,10 @@ package e.edit;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 import java.util.*;
 import javax.swing.*;
+import javax.swing.Timer;
 import javax.swing.event.*;
 import javax.swing.text.*;
 import javax.swing.tree.*;
@@ -14,9 +16,11 @@ public class TagsUpdater {
     private JPanel uiPanel;
     private ETextWindow textWindow;
     private boolean followCaretChanges;
+    private TagsScanner tagsScanner;
     
     public TagsUpdater(ETextWindow textWindow) {
         this.textWindow = textWindow;
+        this.tagsScanner = new TagsScanner();
         createUI();
         installListeners();
     }
@@ -108,7 +112,7 @@ public class TagsUpdater {
     }
     
     public void updateTags() {
-        new TagsPanel.TagsScanner(this).doScan();
+        new Thread(tagsScanner).start();
     }
     
     public void setTreeModel(TreeModel treeModel) {
@@ -160,6 +164,132 @@ public class TagsUpdater {
             if (followCaretChanges) {
                 tree.scrollPathToVisible(path);
             }
+        }
+    }
+    
+    public class TagsScanner implements Runnable, TagReader.TagListener {
+        private boolean tagsHaveNotChanged;
+        private String digest;
+        private long startTime;
+        private Timer progressTimer;
+        private File temporaryFile;
+        
+        private DefaultMutableTreeNode root;
+        private DefaultTreeModel treeModel;
+        private HashMap branches;
+        
+        public TagsScanner() {
+            progressTimer = new Timer(500, new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    Edit.getTagsPanel().showProgressBar();
+                }
+            });
+            progressTimer.setRepeats(false);
+        }
+        
+        public void run() {
+            root = new DefaultMutableTreeNode("root");
+            treeModel = new DefaultTreeModel(root);
+            branches = new HashMap();
+            branches.put("", root);
+            startTime = System.currentTimeMillis();
+            progressTimer.start();
+            scanTags();
+            finished();
+        }
+        
+        public String getFilenameSuffix() {
+            String suffix = ".txt";
+            String filename = getTextWindow().getFilename();
+            int lastDot = filename.lastIndexOf('.');
+            if (lastDot != -1) {
+                suffix = filename.substring(lastDot);
+            }
+            return suffix;
+        }
+        
+        public void tagFound(TagReader.Tag tag) {
+            DefaultMutableTreeNode leaf = new DefaultMutableTreeNode(tag);
+            
+            if (tag.isContainerType()) {
+                leaf = new BranchNode(tag);
+                branches.put(tag.getClassQualifiedName(), leaf);
+            }
+            
+            DefaultMutableTreeNode branch = (DefaultMutableTreeNode) branches.get(tag.containingClass);
+            if (branch == null) {
+                branch = new BranchNode(tag.containingClass);
+                branches.put(tag.containingClass, branch);
+                root.add(branch);
+            }
+            
+            branch.add(leaf);
+        }
+        
+        public void taggingFailed(Exception ex) {
+            Edit.getCurrentWorkspace().reportError("", "There was an error reading the tags (" + ex.getMessage() + ")");
+        }
+        
+        public void scanTags() {
+            try {
+                /*
+                 * It's important to use the same suffix as the original
+                 * file, because that's how ctags guesses the file's type,
+                 * and hence which parser to use.
+                 * Ctags writes the name of the file into its output, so use the
+                 * same file each time so that md5 hashes can be compared.
+                 */
+                if (temporaryFile == null) {
+                    temporaryFile = File.createTempFile("edit-", getFilenameSuffix());
+                    temporaryFile.deleteOnExit();
+                }
+                getTextWindow().writeCopyTo(temporaryFile);
+                TagReader tagReader = new TagReader(temporaryFile, getTextWindow().getFileType(), this);
+                String newDigest = tagReader.getTagsDigest();
+                tagsHaveNotChanged = newDigest.equals(digest);
+                digest = newDigest;
+            } catch (Exception ex) {
+                Edit.getCurrentWorkspace().reportError("Tags", "Couldn't make tags.");
+                ex.printStackTrace();
+            }
+        }
+        
+        public void finished() {
+            if (progressTimer.isRunning() == false) {
+                Edit.getTagsPanel().hideProgressBar();
+            }
+            progressTimer.stop();
+            if (tagsHaveNotChanged) {
+                return;
+            }
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    setTreeModel(treeModel);
+                }
+            });
+            
+            long endTime = System.currentTimeMillis();
+            double duration = ((double) (endTime - startTime)) / 1000.0;
+            //Log.warn("Time taken reading tags: " + duration + "s");
+        }
+    }
+    
+    private static class BranchNode extends DefaultMutableTreeNode {
+        public BranchNode(Object userObject) {
+            super(userObject);
+        }
+        
+        public void add(MutableTreeNode node) {
+            TagReader.Tag tag = (TagReader.Tag) ((DefaultMutableTreeNode) node).getUserObject();
+            insert(node, getInsertIndex(tag));
+        }
+        
+        private SortedSet kidsNames = new TreeSet();
+        
+        public int getInsertIndex(TagReader.Tag tag) {
+            String insertString = tag.getTypeSortIndex() + tag.identifier + kidsNames.size();
+            kidsNames.add(insertString);
+            return new ArrayList(kidsNames).indexOf(insertString);
         }
     }
 }
