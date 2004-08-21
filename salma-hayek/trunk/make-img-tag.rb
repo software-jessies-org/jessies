1537f24572bc64aab0a30a1ad21cc4c9096c9232
elliotth <elliotth@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-08-21 09:29:39 +0000

    make-img-tag.rb: little tool to output HTML IMG tags. Our repositories should have explicit bin/ directories; we can't keep just dumping executable stuff in their roots.

---
+++ salma-hayek/trunk/make-img-tag.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby -w
+if ARGV.length() == 0
+    print("Usage: #$0 <filename...>\n")
+    exit(1)
+end
+ARGV.each() {
+    |file|
+    `file #{file}` =~ /image data, (\d+) x (\d+), /
+    print("<img src=\"#{file}\" width=\"#$1\" height=\"#$2\">\n")
+}
