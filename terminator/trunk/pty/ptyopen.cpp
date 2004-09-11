dc95b09258809988d0c6965f3812d26ac060ab45
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-09-11 23:08:15 +0000

    pty/ptyopen.cpp: reinstate the code from before we switched to ptsname(3) for Mac OS, where -- on 10.2, at least -- there is no ptsname. (The new code isn't identical to the old because the interface has changed, and I took the opportunity to replace more C with C++.)

--- terminator/trunk/pty/ptyopen.cpp
+++ terminator/trunk/pty/ptyopen.cpp
@@ -1,5 +1,38 @@
 #include "pty.h"
 
+#ifdef __APPLE__
+
+int ptym_open(std::string& pts_name) {
+    for (char* ptr1 = "pqrstuvwxyzPQRST"; *ptr1 != 0; ++ptr1) {
+        for (char* ptr2 = "0123456789abcdef"; *ptr2 != 0; ++ptr2) {
+            pts_name = "/dev/pty";
+            pts_name.append(1, *ptr1);
+            pts_name.append(1, *ptr2);
+
+            /* try to open master */
+            int fdm = open(pts_name.c_str(), O_RDWR);
+            if (fdm < 0) {
+                if (errno == ENOENT) {  /* different from EIO */
+                    return -1;          /* out of pty devices */
+                } else {
+                    continue;           /* try next pty device */
+                }
+            }
+
+            /* Return name of slave. */
+            pts_name = "/dev/tty";
+            pts_name.append(1, *ptr1);
+            pts_name.append(1, *ptr2);
+
+            /* Return fd of master. */
+            return fdm;
+        }
+    }
+    return -1;        /* out of pty devices */
+}
+
+#else
+
 int ptym_open(std::string& pts_name) {
     int ptmx_fd = open("/dev/ptmx", O_RDWR);
     if (ptmx_fd < 0) {
@@ -22,30 +55,6 @@ int ptym_open(std::string& pts_name) {
     return ptmx_fd;
 }
 
-#if 0
-int ptym_open(char *pts_name) {
-    strcpy(pts_name, "/dev/ptyXY");
-      /* array index: 0123456789 (for references in following code) */
-    for (char* ptr1 = "pqrstuvwxyzPQRST"; *ptr1 != 0; ptr1++) {
-        pts_name[8] = *ptr1;
-        for (char* ptr2 = "0123456789abcdef"; *ptr2 != 0; ptr2++) {
-            pts_name[9] = *ptr2;
-
-            /* try to open master */
-            int fdm;
-            if ( (fdm = open(pts_name, O_RDWR)) < 0) {
-                if (errno == ENOENT)    /* different from EIO */
-                    return(-1);            /* out of pty devices */
-                else
-                    continue;            /* try next pty device */
-            }
-
-            pts_name[5] = 't';    /* change "pty" to "tty" */
-            return(fdm);        /* got it, return fd of master */
-        }
-    }
-    return(-1);        /* out of pty devices */
-}
 #endif
 
 int ptys_open(int fdm, const char* pts_name) {
