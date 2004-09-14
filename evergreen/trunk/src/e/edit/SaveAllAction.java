e5205d02880460ce72bf88226444cf3620ecf5fa
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-09-14 00:45:14 +0000

    src/e/edit/SaveAllAction.java: factors out the "Save All" code into a static method...
    src/e/edit/EditServer.java: ...so we can offer the functionality from
    EditServer for next time Linux decides it doesn't want to talk to a mouse
    that's been disconnected and re-connected. I'd add a "quit" command too,
    but I don't like the security problem it raises.

--- evergreen/trunk/src/e/edit/SaveAllAction.java
+++ evergreen/trunk/src/e/edit/SaveAllAction.java
@@ -11,11 +11,17 @@ public class SaveAllAction extends AbstractAction {
     }
     
     public void actionPerformed(ActionEvent e) {
+        saveAll(true);
+    }
+
+    public static void saveAll(boolean interactive) {
         Workspace[] workspaces = Edit.getWorkspaces();
         for (int i = 0; i < workspaces.length; ++i) {
             Workspace workspace = workspaces[i];
             if (workspace.saveAll() == false) {
-                Edit.showAlert("Save All", "Unable to save everything on workspace '" + workspace.getTitle() + "'.");
+                if (interactive) {
+                    Edit.showAlert("Save All", "Unable to save everything on workspace '" + workspace.getTitle() + "'.");
+                }
                 return;
             }
         }
