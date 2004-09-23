dcbc4d2d7324873c35a0eea8577e97aac87470bd
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-09-23 21:20:14 +0000

    src/e/gui/EPopupMenu.java: now Java 1.5 is almost here, it's probably safe to start insisting on Java 1.4; in particular, let's use the new JMenuItem constructor that takes an Action, rather than making our own JMenuItem from an Action. Can't believe this wasn't there from the start? Neither could I!

--- salma-hayek/trunk/src/e/gui/EPopupMenu.java
+++ salma-hayek/trunk/src/e/gui/EPopupMenu.java
@@ -60,23 +60,11 @@ public class EPopupMenu {
                 }
                 lastWasSeparator = true;
             } else {
-                menu.add(createJMenuItem(action, x, y));
+                menu.add(new JMenuItem(action));
                 lastWasSeparator = false;
             }
         }
         return menu;
-    }
-    
-    /** Creates a JMenuItem from an Action and a location. */
-    public JMenuItem createJMenuItem(final Action action, final int x, final int y) {
-        JMenuItem menuItem = new JMenuItem((String) action.getValue(Action.NAME));
-        menuItem.addActionListener(new ActionListener() {
-            public void actionPerformed(final ActionEvent e) {
-                action.actionPerformed(new ActionEvent(new Point(x, y), e.getID(), e.getActionCommand(), e.getModifiers()));
-            }
-        });
-        menuItem.setEnabled(action.isEnabled());
-        return menuItem;
     }
     
     /** Sets up the menu with items that act upon what's at (x,y). */
