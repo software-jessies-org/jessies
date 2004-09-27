f4124e48bad4420788d9eaed734559dfdce1e77d
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-09-27 01:53:41 +0000

    src/e/gui/ETable.java: paint stripes where there aren't any actual rows because there's not enough data in the model. Really only needed on Mac OS, to look more like native applications, but I think it's just a good idea generally. Millions of meters of line-printer paper can't be wrong.

--- salma-hayek/trunk/src/e/gui/ETable.java
+++ salma-hayek/trunk/src/e/gui/ETable.java
@@ -5,17 +5,70 @@ import javax.swing.*;
 import javax.swing.table.*;
 import e.util.*;
 
+/**
+ * A better-looking table than JTable. In particular, on Mac OS this looks
+ * more like a Cocoa table than the default Aqua LAF manages.
+ *
+ * @author Elliott Hughes
+ */
 public class ETable extends JTable {
     public ETable() {
     }
 
     /**
-     * Shades alternate rows.
+     * Paints empty rows too, after letting the UI delegate do
+     * its painting.
+     */
+    public void paint(Graphics g) {
+        super.paint(g);
+        paintEmptyRows(g);
+    }
+
+    /**
+     * Paints the backgrounds of the implied empty rows when the
+     * table model is insufficient to fill all the visible area
+     * available to us. We don't involve cell renderers, because
+     * we have no data.
+     */
+    protected void paintEmptyRows(Graphics g) {
+        final int rowCount = getRowCount();
+        final Rectangle clip = g.getClipBounds();
+        if (rowCount * rowHeight < clip.height) {
+            for (int i = rowCount; i <= clip.height/rowHeight; ++i) {
+                g.setColor(colorForRow(i));
+                g.fillRect(clip.x, i * rowHeight, clip.width, rowHeight);
+            }
+        }
+    }
+
+    /**
+     * Changes the behavior of a table in a JScrollPane to be more like
+     * the behavior of JList, which expands to fill the available space.
+     * JTable normally restricts its size to just what's needed by its
+     * model.
+     */
+    public boolean getScrollableTracksViewportHeight() {
+        if (getParent() instanceof JViewport) {
+            JViewport parent = (JViewport) getParent();
+            return (parent.getHeight() > getPreferredSize().height);
+        }
+        return false;
+    }
+
+    /**
+     * Returns the appropriate background color for the given row.
+     */
+    protected Color colorForRow(int row) {
+        return (row % 2 == 0) ? GuiUtilities.ALTERNATE_ROW_COLOR : getBackground();
+    }
+
+    /**
+     * Shades alternate rows in different colors.
      */
     public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
         Component c = super.prepareRenderer(renderer, row, column);
         if (isCellSelected(row, column) == false) {
-            c.setBackground((row % 2 == 0) ? GuiUtilities.ALTERNATE_ROW_COLOR : getBackground());
+            c.setBackground(colorForRow(row));
             c.setForeground(UIManager.getColor("Table.foreground"));
         } else {
             c.setBackground(UIManager.getColor("Table.selectionBackground"));
