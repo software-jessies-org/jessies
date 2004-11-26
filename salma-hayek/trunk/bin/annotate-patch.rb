228ecdcf8eab7509525bd60aa08c4a0704c74902
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-11-26 22:41:00 +0000

    bin/annotate-patch.rb: a new script to append scope (function, method, class) information to the end of the @@ lines introducing patch hunks. Like "diff -p" only using Exuberant ctags to work for languages other than C.

---
+++ salma-hayek/trunk/bin/annotate-patch.rb
@@ -0,0 +1,118 @@
+#!/usr/bin/ruby -w
+
+# NAME
+#   annotate-patch.rb - add scope names to patch hunks
+#
+# SYNOPSIS
+#   annotate-patch.rb patch-file
+#
+# DESCRIPTION
+#   Appends scope names to the end of lines in a patch that introduce hunks,
+#   in the style of "diff -p", only applying to any language that Exuberant
+#   ctags can parse, rather than just C.
+#
+# EXAMPLE
+#   Given this patch file, and starting in a directory containing src/tuple.h:
+# 
+example_jikes_patch = <<EOF
+Index: src/tuple.h
+===================================================================
+RCS file: /usr/cvs/jikes/jikes/src/tuple.h,v
+retrieving revision 1.19
+diff -u -r1.19 tuple.h
+--- src/tuple.h 11 Dec 2002 00:55:05 -0000      1.19
++++ src/tuple.h 26 Nov 2004 21:10:19 -0000
+@@ -90,8 +90,9 @@
+         // instead of having to perform a subtraction for each reference.
+         // See operator[] below. Finally, we update size.
+         //
+-        base[k] = (new T[Blksize()]) - size;
+-        size += Blksize();
++        unsigned block_size = Blksize();
++        base[k] = (new T[block_size]) - size;
++        size += block_size;
+     }
+ 
+ public:
+EOF
+# 
+# The output is rewritten so the hunk introduction looks like this (the two
+# guesses correspond to the best tag in the --- file and the +++ file; I
+# don't yet know whether this is going to be useful):
+# 
+example_result = <<EOF
+--- src/tuple.h 11 Dec 2002 00:55:05 -0000      1.19
++++ src/tuple.h 26 Nov 2004 21:10:19 -0000
+@@ -90,8 +90,9 @@ Jikes::Tuple.AllocateMoreSpace Jikes::Tuple.AllocateMoreSpace
+         // instead of having to perform a subtraction for each reference.
+EOF
+#
+# BUGS
+#   Exuberant ctags doesn't tell us the range of a tag, so a new top-level
+#   function in C, for example, will be described as being part of the
+#   previous top-level symbol.
+#
+#   It's not obvious that we care about the scope at the start of the context;
+#   it seems more likely that we're interested in the scope of the first line
+#   starting with a + or - in each hunk.
+#
+
+# -----------------------------------------------------------------------------
+
+if ARGV.length() != 1
+  print("usage: #{$0} patch-file\n")
+  exit(1)
+end
+
+def tags_for_file(filename)
+  tag_lines = `ctags -n -f - #{filename}`.split('\n')
+  tags = Hash.new()
+  tag_lines.each() {
+    |line|
+    if line =~ /^(\S*)\t\S*\t(\d+);"\t\S+(?:\t(\S*))?/
+      line_number = $2.to_i()
+      identifier = $1
+      if $3 =~ /(?:struct|class|enum|interface|namespace):(\S+)/
+        identifier = "#$1.#{identifier}"
+      end
+      tags[line_number] = identifier
+    end
+  }
+  return tags.sort()
+end
+
+def find_tag_for_line(tags, line)
+ sought_line_number = line.to_i()
+ name = "(unknown)"
+ tags.each() {
+  |tag|
+  line_number = tag[0]
+  if line_number.to_i() <= sought_line_number
+   name = tag[1]
+  end
+ }
+ return name
+end
+
+minus_file = "(unknown)"
+minus_tags = []
+plus_file = "(unknown)"
+plus_tags = []
+
+file = File.new(ARGV[0])
+lines = file.readlines()
+lines.each() {
+ |line|
+ if line =~ /^\+\+\+ (\S+)\s/
+  plus_file = $1
+  plus_tags = tags_for_file(plus_file)
+ elsif line =~ /^--- (\S+)\s/
+  minus_file = $1
+  minus_tags = tags_for_file(minus_file)
+ elsif line =~ /^@@ -(\d+),\d+ \+(\d+),\d+ @@/
+  plus_tag = find_tag_for_line(plus_tags, $2)
+  minus_tag = find_tag_for_line(minus_tags, $1)
+  line = "#{line.chomp()} #{minus_tag} #{plus_tag}\n"
+ end
+ print(line)
+}
