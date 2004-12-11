cf1eefbfd2aeef748222ce619dac1b96731dd1e8
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2004-12-11 06:35:07 +0000

    src/terminator/TerminalPaneMaster.java: src/terminator/view/JTerminalPane.java: remove terminalNameChanged from TerminalPaneMaster.

--- terminator/trunk/src/terminator/TerminalPaneMaster.java
+++ terminator/trunk/src/terminator/TerminalPaneMaster.java
@@ -2,21 +2,16 @@ package terminator;
 
 import java.awt.*;
 
-import terminator.view.*;
-
 /**
 A TerminalPaneMaster is the master of a JTerminalPane, and allows the JTerminalPane to request its master
 to do stuff, like opening new tabs, or whatever.
 
 @author Phil Norman
 */
-
 public interface TerminalPaneMaster {
 	public void openShellPane(boolean focusOnNewTab);
 	
 	public void openCommandPane(String command, boolean focusOnNewTab);
 	
 	public void setTerminalSize(Dimension size);
-	
-	public void terminalNameChanged(JTerminalPane terminal);
 }
