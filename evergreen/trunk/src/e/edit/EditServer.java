55babbbbaad384377318894881f4b492a27b34ed
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-01-28 06:54:44 +0000

    src/e/edit/EditServer.java: martind had a problem where an NFS server had gone away, and Edit had tried to access a file on that server and hung. Normally an Edit hang wouldn't be too serious a problem; you'd just connect to the EditServer and ask it to save everything and quit. Only it was the EditServer thread that was hung, because it was where the open request had come from. This patch refactors EditServer to do each job it's asked to do in a separate thread. It's been tested in the field for several weeks now, and hasn't caused any problems, so in it goes...

--- evergreen/trunk/src/e/edit/EditServer.java
+++ evergreen/trunk/src/e/edit/EditServer.java
@@ -20,61 +20,85 @@ public final class EditServer extends Thread {
     public void run() {
         for (;;) {
             try {
-                Socket client = socket.accept();
-                handleClient(client);
+                Thread thread = new Thread(new ClientHandler(socket.accept()));
+                thread.start();
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
     }
     
-    public void handleClient(Socket client) {
-        try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            PrintWriter out = new PrintWriter(new OutputStreamWriter(client.getOutputStream()));
-            handleCommand(in, out);
-            out.flush();
-            out.close();
-            in.close();
-            client.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
+    public class ClientHandler implements Runnable {
+        private Socket client;
+        
+        private BufferedReader in;
+        private PrintWriter out;
+        
+        public ClientHandler(Socket client) {
+            this.client = client;
         }
-    }
-    
-    public void handleCommand(final BufferedReader in, final PrintWriter out) throws IOException {
-        String line = in.readLine();
-        if (line == null || line.length() == 0) {
-            Log.warn("EditServer ignoring empty request");
-            return;
+        
+        public void run() {
+            handleClient();
         }
-        if (line.startsWith("open ")) {
-            String filename = line.substring("open ".length());
-            open(out, filename);
-        } else if (line.equals("remember-state")) {
-            edit.rememberState();
-        } else if (line.equals("save-all")) {
-            SaveAllAction.saveAll(false);
-        } else {
-            out.println("EditServer: didn't understand request \"" + line + "\".");
+        
+        private void handleClient() {
+            try {
+                this.in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                this.out = new PrintWriter(new OutputStreamWriter(client.getOutputStream()));
+                handleCommand();
+                out.flush();
+                out.close();
+                in.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+                closeClientSocket();
+            }
         }
-    }
-    
-    public void open(final PrintWriter out, final String filename) {
-        try {
-            SwingUtilities.invokeAndWait(new Runnable() {
-                public void run() {
-                    try {
-                        Edit.openFileNonInteractively(filename);
-                        Edit.getFrame().toFront();
-                        out.println("File '" + filename + "' opened OK.");
-                    } catch (Exception ex) {
-                        out.println(ex.getMessage());
+        
+        private void closeClientSocket() {
+            try {
+                client.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        
+        private void handleCommand() throws IOException {
+            String line = in.readLine();
+            if (line == null || line.length() == 0) {
+                Log.warn("EditServer ignoring empty request");
+                return;
+            }
+            if (line.startsWith("open ")) {
+                String filename = line.substring("open ".length());
+                handleOpen(filename);
+            } else if (line.equals("remember-state")) {
+                edit.rememberState();
+            } else if (line.equals("save-all")) {
+                SaveAllAction.saveAll(false);
+            } else {
+                out.println("EditServer: didn't understand request \"" + line + "\".");
+            }
+        }
+        
+        private void handleOpen(final String filename) {
+            try {
+                SwingUtilities.invokeAndWait(new Runnable() {
+                    public void run() {
+                        try {
+                            Edit.openFileNonInteractively(filename);
+                            Edit.getFrame().toFront();
+                            out.println("File '" + filename + "' opened OK.");
+                        } catch (Exception ex) {
+                            out.println(ex.getMessage());
+                        }
                     }
-                }
-            });
-        } catch (Exception ex) {
-            out.println(ex.getMessage());
+                });
+            } catch (Exception ex) {
+                out.println(ex.getMessage());
+            }
         }
     }
 }
