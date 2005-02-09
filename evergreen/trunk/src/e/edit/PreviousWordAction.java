22a6876639a47be4427b7d6e53cb98fafba9e770
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-02-09 06:25:57 +0000

    src/e/edit/NextWordAction.java: johnw pointed out the inconsistency between our next-word and previous-word actions. I claimed not to have noticed, but later realized that I'd often been annoyed by the fact that "sometimes" it was really slow to move word-at-a-time. It turns out that the "sometimes" was that the next-word action stopped at the start and end of each word, where the previous-word action stopped only at the start. I checked Apple's behavior, and they stop at the far side of words relative to the direction of travel. Windows and Java always stop at the start, regardless of the direction of travel. johnw argued for the Windows behavior on the grounds that it's reversible; I argued that the Apple behavior is more useful in practice because it lets you select a word from the keyboard, which is otherwise awkward. johnw seemed to agree. So: make next-word only stop at the ends of words.
    src/e/edit/PreviousWordAction.java: remove dead code.

--- evergreen/trunk/src/e/edit/PreviousWordAction.java
+++ evergreen/trunk/src/e/edit/PreviousWordAction.java
@@ -14,12 +14,6 @@ public class PreviousWordAction extends WordAction {
             return 0;
         }
         
-        // If we're selecting and at the start of whitespace, go the start of the whitespace.
-        // FIXME: this heuristic isn't right. What should the behavior be? For now, behave like Project Builder.
-        //if (select && isInWhitespace(text, offset - 1)) {
-            //return getWhitespaceStart(text, offset);
-        //}
-        
         // If we're at the start of a word, go to the start of the word before.
         if (isInWord(text, offset - 1) == false) {
             return getWordStart(text, getNonWordStart(text, offset - 1));
