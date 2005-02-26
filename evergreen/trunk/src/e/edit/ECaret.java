c2e781234235089b53ce409fbccebf643cab4d07
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-02-26 14:22:33 +0000

    src/e/edit/Brackets.java: src/e/edit/ECaret.java: move all the bracket-matching utility code out into a new Brackets class.
    src/e/edit/MatchingBracketHighlighter.java: track API change. This class now
    makes no mention of Edit-specific classes. It, Brackets, and
    DocumentCharSequence could move to salma-hayek if I didn't have a bad feeling
    that I had a lot of trouble fixing my repositories the last time I tried an
    inter-repository "svn mv".

--- evergreen/trunk/src/e/edit/ECaret.java
+++ evergreen/trunk/src/e/edit/ECaret.java
@@ -87,85 +87,19 @@ public class ECaret extends DefaultCaret {
         return c == ' ' || c == '\t';
     }
     
-    private static final String BRACKETS = "(<[{}]>)";
-    
-    private static final String PARTNERS = ")>]}{[<(";
-    
-    public static boolean isBracket(char ch) {
-        return BRACKETS.indexOf(ch) != -1;
-    }
-    
-    public boolean isOpenBracket(char ch) {
-        return BRACKETS.indexOf(ch) < PARTNERS.indexOf(ch);
-    }
-    
-    public boolean isCloseBracket(char ch) {
-        return isBracket(ch) && isOpenBracket(ch) == false;
-    }
-    
-    public static char getPartnerForBracket(char bracket) {
-        return PARTNERS.charAt(BRACKETS.indexOf(bracket));
-    }
-    
-    /* Some handy examples to try the bracket matching on.
-    
-        // if (hello > (int) )
-        // if (hello > (int))
-        // if ((int) x > 2)
-        // if ( (int) x > 2)
-    */
-    
-    /** Returns the offset of the matching bracket, or -1. */
-    public int findMatchingBracket(int offset) {
-        //Log.warn("findMatchingBracket(offset="+offset+")");
-        ETextArea textArea = (ETextArea) getComponent();
-        CharSequence chars = textArea.charSequence();
-        char bracket = chars.charAt(offset);
-        if (isCloseBracket(bracket)) {
-            return findMatchingBracket(chars, offset, false);
-        }
-        if (offset > 0 && isOpenBracket(chars.charAt(offset - 1))) {
-            return findMatchingBracket(chars, offset - 1, true);
-        }
-        return -1;
-    }
-    
-    /** Returns the offset of the matching bracket, scanning in the given direction, or -1. */
-    private static int findMatchingBracket(CharSequence chars, int offset, boolean scanForwards) {
-        //Log.warn("findMatchingBracket(offset="+offset+",scanForwards="+scanForwards+")");
-        char bracket = chars.charAt(offset);
-        if (isBracket(bracket) == false) {
-            return -1;
-        }
-        char partner = getPartnerForBracket(bracket);
-        int nesting = 1;
-        int step = scanForwards ? +1 : -1;
-        int stop = scanForwards ? chars.length() : 0;
-        for (offset += step; nesting != 0 && offset != stop; offset += step) {
-            char ch = chars.charAt(offset);
-            if (ch == bracket) {
-                nesting++;
-            } else if (ch == partner) {
-                nesting--;
-            }
-        }
-        if (offset != stop) {
-            // We actually want to stop just before the matching bracket.
-            offset += (scanForwards ? 1 : 2) * -step;
-        }
-        return offset;
-    }
-    
     /** Selects the word scanning out from the dot. */
     public void selectWordAtCursor() throws BadLocationException {
         JTextComponent text = getTextComponent();
+        ETextArea textArea = (ETextArea) text;
+        CharSequence chars = textArea.charSequence();
+        
         int offset = getDot();
         Element line = javax.swing.text.Utilities.getParagraphElement(text, offset);
         int lineStart = line.getStartOffset();
         int lineEnd = Math.min(line.getEndOffset(), text.getDocument().getLength());
         
         //FIXME: first try to match with a bracket.
-        int bracketOffset = findMatchingBracket(offset);
+        int bracketOffset = Brackets.findMatchingBracket(chars, offset);
         if (bracketOffset != -1) {
             moveDot(bracketOffset);
             return;
@@ -200,9 +134,8 @@ public class ECaret extends DefaultCaret {
         }
         
         // Select from the start to the end of the current word...
-        ETextArea textArea = (ETextArea) text;
-        setDot(WordAction.getWordStart(textArea.charSequence(), offset, textArea.getWordSelectionStopChars()));
-        moveDot(WordAction.getWordEnd(textArea.charSequence(), offset, textArea.getWordSelectionStopChars()));
+        setDot(WordAction.getWordStart(chars, offset, textArea.getWordSelectionStopChars()));
+        moveDot(WordAction.getWordEnd(chars, offset, textArea.getWordSelectionStopChars()));
     }
     
     /** Invokes appropriate selection methods for multiple presses. */
