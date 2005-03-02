8198827025e789f99a868fb8409c23b85a4b5805
enp <enp@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-03-02 18:43:49 +0000

    src/e/edit/CheckInChangesAction.java: Getting trigger-happy with my new repository access, make sure SCM_EDITOR is set to the new sensible value so that we can Edit File from checkintool's statuses list.

--- evergreen/trunk/src/e/edit/CheckInChangesAction.java
+++ evergreen/trunk/src/e/edit/CheckInChangesAction.java
@@ -5,7 +5,7 @@ package e.edit;
  */
 public class CheckInChangesAction extends ExternalToolAction {
     public CheckInChangesAction() {
-        super("Check in Changes...", "cd $EDIT_CURRENT_DIRECTORY ; SCM_EDITOR=e checkintool");
+        super("Check in Changes...", "cd $EDIT_CURRENT_DIRECTORY ; SCM_EDITOR=edit checkintool");
         setChecksEverythingSaved(true);
     }
 }
