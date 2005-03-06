cb25e0c0ec8858e79b97826b4e6e8939482406ff
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-03-06 06:55:08 +0000

    escaping.rb: I think we only want to embolden the names of files which are being checked-in.  Not files in quoted error messages like:
    /home/martind/software.jessies.org/terminator/bin/terminator: line 61: /root/.terminal-logs/.cached-icon: No such file or directory
    
    We're still going to embolden "cat" here:
    
    cat: /root/.terminal-logs/.terminator-server-port: No such file or directory
    
    Even after the auto-update of this file has taken effect.

--- salma-hayek/trunk/escaping.rb
+++ salma-hayek/trunk/escaping.rb
@@ -6,7 +6,7 @@ def escapeTextLineToHtml(line)
     $_ = line.chomp
     $_ = CGI.escapeHTML($_)
     # Embolden filenames:
-    $_.gsub!(/^(\S+): /, "<b>\\1</b>: ")
+    $_.gsub!(/^([^[:space:]\/]\S*): /, "<b>\\1</b>: ")
     # Preserve formatting:
     $_.gsub!(/^(\s+)/) {|spaces| "&nbsp;" * spaces.length()}
     # Make slight typographical improvements:
