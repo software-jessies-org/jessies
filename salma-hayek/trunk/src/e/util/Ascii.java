260181325925be4ea04dbf8472b88e80917d30de
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-03-06 17:24:59 +0000

    src/e/util/Ascii.java: collect the names of all the ASCII control characters together.

---
+++ salma-hayek/trunk/src/e/util/Ascii.java
@@ -0,0 +1,45 @@
+package e.util;
+
+/**
+ * Constants for the ASCII control characters.
+ */
+public class Ascii {
+    public static final char NUL = 0;
+    public static final char SOH = 1;
+    public static final char STX = 2;
+    public static final char ETX = 3;
+    public static final char EOT = 4;
+    public static final char ENQ = 5;
+    public static final char ACK = 6;
+    public static final char BEL = 7;
+    public static final char BS = 8;
+    public static final char HT = 9;
+    public static final char LF = 10;
+    public static final char VT = 11;
+    public static final char FF = 12;
+    public static final char CR = 13;
+    public static final char SO = 14;
+    public static final char SI = 15;
+    public static final char DLE = 16;
+    public static final char DC1 = 17;
+    public static final char DC2 = 18;
+    public static final char DC3 = 19;
+    public static final char DC4 = 20;
+    public static final char NAK = 21;
+    public static final char SYN = 22;
+    public static final char ETB = 23;
+    public static final char CAN = 24;
+    public static final char EM = 25;
+    public static final char SUB = 26;
+    public static final char ESC = 27;
+    public static final char FS = 28;
+    public static final char GS = 29;
+    public static final char RS = 30;
+    public static final char US = 31;
+    
+    public static final char DEL = 127;
+    
+    private Ascii() {
+        // No instances.
+    }
+}
