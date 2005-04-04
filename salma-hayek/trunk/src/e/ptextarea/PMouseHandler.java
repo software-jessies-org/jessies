9b707076ac929d94a8c9d067a7417607c8800eb4
pcn <pcn@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-04-04 21:36:24 +0000

    src/e/ptextarea/PMouseHandler.java: Added separate handling for single-, double- and triple-clicks and drags.  Single and triple are both implemented (they're easy), but double is yet to be done.

--- salma-hayek/trunk/src/e/ptextarea/PMouseHandler.java
+++ salma-hayek/trunk/src/e/ptextarea/PMouseHandler.java
@@ -5,9 +5,7 @@ import java.awt.event.*;
 
 public class PMouseHandler extends MouseAdapter implements MouseMotionListener {
     private PTextArea textArea;
-    private boolean isDraggingSelection = false;
-    private boolean dragHandlerWasCalled = false;
-    private int pressedLocation = -1;
+    private DragHandler dragHandler;
     
     public PMouseHandler(PTextArea textArea) {
         this.textArea = textArea;
@@ -17,11 +15,21 @@ public class PMouseHandler extends MouseAdapter implements MouseMotionListener {
         if (e.isPopupTrigger()) {
             return;
         }
-        textArea.requestFocus();
-        pressedLocation = getLocationOfMouse(e);
-        textArea.setCaretLocation(pressedLocation);
-        isDraggingSelection = true;
-        dragHandlerWasCalled = false;
+        int pressedLocation = getLocationOfMouse(e);
+        if (e.getClickCount() == 1) {
+            textArea.requestFocus();
+        }
+        dragHandler = getDragHandlerForClick(e);
+        dragHandler.makeInitialSelection(pressedLocation);
+    }
+    
+    private DragHandler getDragHandlerForClick(MouseEvent e) {
+        switch (e.getClickCount() % 3) {
+        case 0: return new TripleClickDragHandler();
+        case 1: return new SingleClickDragHandler();
+        case 2: return new DoubleClickDragHandler();
+        default: throw new RuntimeException("Logically impossible");
+        }
     }
     
     private int getLocationOfMouse(MouseEvent event) {
@@ -30,20 +38,93 @@ public class PMouseHandler extends MouseAdapter implements MouseMotionListener {
     }
     
     public void mouseReleased(MouseEvent event) {
-        isDraggingSelection = false;
-        if (dragHandlerWasCalled == false) {
-            textArea.clearSelection();
+        if (dragHandler != null) {
+            dragHandler.mouseReleased(event);
         }
+        dragHandler = null;
     }
     
     public void mouseDragged(MouseEvent event) {
-        if (isDraggingSelection) {
+        if (dragHandler != null) {
+            dragHandler.mouseDragged(event);
+        }
+    }
+    
+    public void mouseMoved(MouseEvent event) { }
+    
+    private interface DragHandler {
+        public void makeInitialSelection(int pressedLocation);
+        public void mouseDragged(MouseEvent event);
+        public void mouseReleased(MouseEvent event);
+    }
+    
+    private class SingleClickDragHandler implements DragHandler {
+        private boolean dragHandlerWasCalled = false;
+        private int pressedLocation;
+        
+        public void makeInitialSelection(int pressedLocation) {
+            this.pressedLocation = pressedLocation;
+            textArea.clearSelection();
+            textArea.setCaretLocation(pressedLocation);
+        }
+        
+        public void mouseDragged(MouseEvent event) {
             int location = getLocationOfMouse(event);
             textArea.select(Math.min(location, pressedLocation), Math.max(location, pressedLocation));
             textArea.setCaretLocation(location);
             dragHandlerWasCalled = true;
         }
+        
+        public void mouseReleased(MouseEvent event) {
+            if (dragHandlerWasCalled == false) {
+                textArea.clearSelection();
+            }
+        }
     }
     
-    public void mouseMoved(MouseEvent event) { }
+    private class DoubleClickDragHandler implements DragHandler {
+        private int pressedLocation;
+        
+        public void makeInitialSelection(int pressedLocation) {
+            this.pressedLocation = pressedLocation;
+        }
+        
+        public void mouseDragged(MouseEvent event) {
+        }
+        
+        public void mouseReleased(MouseEvent event) {
+        }
+    }
+    
+    private class TripleClickDragHandler implements DragHandler {
+        private int pressedLine;
+        
+        public void makeInitialSelection(int pressedLocation) {
+            this.pressedLine = textArea.getLineOfOffset(pressedLocation);
+            textArea.select(textArea.getLineStartOffset(pressedLine), getLineEndOffset(pressedLine));
+            textArea.setCaretLocation(getLineEndOffset(pressedLine));
+        }
+        
+        private int getLineEndOffset(int line) {
+            if (line == textArea.getLineCount() - 1) {
+                return textArea.getLineEndOffset(line);
+            } else {
+                return textArea.getLineStartOffset(line + 1);
+            }
+        }
+        
+        public void mouseDragged(MouseEvent event) {
+            int currentLine = textArea.getLineOfOffset(getLocationOfMouse(event));
+            int minLine = Math.min(currentLine, pressedLine);
+            int maxLine = Math.max(currentLine, pressedLine);
+            textArea.select(textArea.getLineStartOffset(minLine), getLineEndOffset(maxLine));
+            if (currentLine == maxLine) {
+                textArea.setCaretLocation(getLineEndOffset(currentLine));
+            } else {
+                textArea.setCaretLocation(textArea.getLineStartOffset(currentLine));
+            }
+        }
+        
+        public void mouseReleased(MouseEvent event) { }
+    }
 }
