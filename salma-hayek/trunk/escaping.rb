22d856f25fd990817d87bfddb2ba3273b10d051f
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-04-06 16:14:20 +0000

    escaping.rb: try harder to recognize quoted text.

--- salma-hayek/trunk/escaping.rb
+++ salma-hayek/trunk/escaping.rb
@@ -3,16 +3,25 @@
 require 'cgi'
 
 def escapeTextLineToHtml(line)
-    $_ = line.chomp
-    $_ = CGI.escapeHTML($_)
+    $_ = CGI.escapeHTML(line.chomp())
+    
     # Embolden filenames:
     $_.gsub!(/^([^[:space:]\/]\S*): /, "<b>\\1</b>: ")
-    # Preserve formatting:
-    $_.gsub!(/^(\s+)/) {|spaces| "&nbsp;" * spaces.length()}
+    
+    # Try to detect quoted text (either traditional Unix mail style or
+    # wiki-like leading whitespace):
+    $_.gsub!(/^(?:&gt;| ) (.*)$/) {
+        |text|
+        "<tt>  #{text.gsub(' ', '&nbsp;')}</tt>"
+    }
+    
     # Turn URLs into links:
     $_.gsub!(/(http:\/\/\S+)/, "<a href=\"\\1\">\\1</a>")
+    
     # Make slight typographical improvements:
     $_.gsub!(/ -- /, "&nbsp;&ndash; ")
     $_.gsub!(/ --- /, "&nbsp;&mdash; ")
+    
+    # Each input line gets a line-break in the HTML:
     return "#{$_}<br>\n"
 end
