23add1a63d329fd900b07239cc848447c5910c24
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-04-15 06:18:02 +0000

    src/e/ptextarea/PStyle.java: give each instance a name, and implement a decent toString.

--- salma-hayek/trunk/src/e/ptextarea/PStyle.java
+++ salma-hayek/trunk/src/e/ptextarea/PStyle.java
@@ -6,21 +6,27 @@ import java.awt.*;
  * An enum containing each style understood by PTextArea and its stylers.
  */
 public final class PStyle {
-    public static final PStyle NORMAL = new PStyle(Color.BLACK);
-    public static final PStyle STRING = new PStyle(Color.decode("#0000ff"));
-    public static final PStyle COMMENT = new PStyle(Color.decode("#227722"));
-    public static final PStyle KEYWORD = new PStyle(Color.decode("#770022"));
-    public static final PStyle ERROR = new PStyle(Color.RED);
-    public static final PStyle HYPERLINK = new PStyle(Color.BLUE);
+    public static final PStyle NORMAL = new PStyle("normal", Color.BLACK);
+    public static final PStyle STRING = new PStyle("string", Color.decode("#0000ff"));
+    public static final PStyle COMMENT = new PStyle("comment", Color.decode("#227722"));
+    public static final PStyle KEYWORD = new PStyle("keyword", Color.decode("#770022"));
+    public static final PStyle ERROR = new PStyle("error", Color.RED);
+    public static final PStyle HYPERLINK = new PStyle("hyperlink", Color.BLUE);
     
+    private String name;
     private Color color;
     
     public final Color getColor() {
         return color;
     }
     
-    private PStyle(Color color) {
+    public String toString() {
+        return "PStyle[" + name + ",color=" + color + "]";
+    }
+    
+    private PStyle(String name, Color color) {
         // This should be an enum come Java 1.5.
+        this.name = name;
         this.color = color;
     }
 }
