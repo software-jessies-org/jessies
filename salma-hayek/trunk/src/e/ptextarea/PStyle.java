77b3b4140ec9ba3442ad03b4d71dc5810b8b1e6f
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-04-19 16:14:09 +0000

    src/e/ptextarea/PStyle.java: use the UIManager's default text area foreground color rather than assuming black.
    TODO: job done.

--- salma-hayek/trunk/src/e/ptextarea/PStyle.java
+++ salma-hayek/trunk/src/e/ptextarea/PStyle.java
@@ -6,7 +6,7 @@ import java.awt.*;
  * An enum containing each style understood by PTextArea and its stylers.
  */
 public final class PStyle {
-    public static final PStyle NORMAL = new PStyle("normal", Color.BLACK);
+    public static final PStyle NORMAL = new PStyle("normal", javax.swing.UIManager.getColor("TextArea.foreground"));
     public static final PStyle STRING = new PStyle("string", Color.decode("#0000ff"));
     public static final PStyle COMMENT = new PStyle("comment", Color.decode("#227722"));
     public static final PStyle KEYWORD = new PStyle("keyword", Color.decode("#770022"));
