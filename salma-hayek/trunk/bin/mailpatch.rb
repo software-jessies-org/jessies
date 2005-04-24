0eb78c5347fe3b0b71383710bdf664583b8039c5
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-04-24 13:48:25 +0000

    bin/mailpatch.rb: LOGNAME seemingly isn't set when I run mailpatch.rb from the Edit context menu.  `whoami` does the trick, once I've forgotten and remembered the usual chomp.

--- salma-hayek/trunk/bin/mailpatch.rb
+++ salma-hayek/trunk/bin/mailpatch.rb
@@ -8,6 +8,9 @@ $: << script_path.dirname
 require 'patch-to-html-email.rb'
 
 from_address=ENV['LOGNAME']
+if from_address == ""
+  from_address=`whoami`.chomp
+end
 to_address=from_address
 reply_to_address=to_address # Use "nil" to reply to author.
 subject="patch"
