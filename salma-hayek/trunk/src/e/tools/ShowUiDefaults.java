24eb544949efc616d6868e502c7739a40e05012c
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-04-24 05:52:17 +0000

    src/e/tools/ShowUiDefaults.java: a hopefully useful new command-line tool.

---
+++ salma-hayek/trunk/src/e/tools/ShowUiDefaults.java
@@ -0,0 +1,26 @@
+package e.tools;
+
+import java.util.*;
+import javax.swing.*;
+
+/**
+ * Dumps the system's Swing UI defaults to standard output, for your grep(1)
+ * pleasure. I'm always wanting to see if there's a UI default I can get a
+ * useful default for my own code from, and it's always a pain to trawl
+ * through the source (or at least to find the right directory). This, if
+ * I can just remember its existence, should fix that.
+ * FIXME: should showing the values be an option? It makes the output harder
+ * to read, and isn't usually what I'm interested in.
+ */
+public class ShowUiDefaults {
+    private ShowUiDefaults() {
+    }
+    
+    public static void main(String[] args) {
+        UIDefaults defaults = UIManager.getLookAndFeelDefaults();
+        for (Enumeration e = defaults.keys() ; e.hasMoreElements() ;) {
+            String key = (String) e.nextElement();
+            System.out.println(key + "=" + defaults.get(key));
+        }
+    }
+}
