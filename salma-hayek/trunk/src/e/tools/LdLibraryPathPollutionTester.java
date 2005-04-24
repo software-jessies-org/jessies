a5c504561b8b6f7186ab6f151d18b3e36d744f25
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-04-24 09:12:32 +0000

    src/e/tools/LdLibraryPathPollutionTester.java: I finally worked out what I had been doing wrong here and submitted the LD_LIBRARY_PATH pollution bug to Sun (internal review id 424121). I'd assumed that std::cout in the child would be inherited from the parent whereas it's piped to the parent.  Even after solving that, there was the usual palaver with the regular expression having to match the whole input.  There was a link from the JavaDoc for PrintStream to PrintWriter but nothing from InputStream to what you'd want to wrap that in.  I had to find some BlueArc Java which used Pattern to see how you do it.
    It's all a lot more work than:
    
      my $input = `env`;
      if ($input =~ m/^LD_LIBRARY_PATH=(.*)$/) {
      	 print ("$1\n");
      }
    
    Yeah, so `env` could run out of memory and what do you do if you can't find env?  Those aren't good reasons for not making that possible.  Although I know how to read the whole input stream in a single line, with no equivalent of Process's pipe handling, this would have nastier in C++.

---
+++ salma-hayek/trunk/src/e/tools/LdLibraryPathPollutionTester.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.regex.*;
+
+public class LdLibraryPathPollutionTester {
+  public static void main(String[] arguments) {
+    try {
+      final Process process = Runtime.getRuntime().exec("env");
+      LineNumberReader input = new LineNumberReader(new InputStreamReader(process.getInputStream()));
+      Pattern pattern = Pattern.compile("^LD_LIBRARY_PATH=(.*)$");
+      String line;
+      while ((line = input.readLine()) != null) {
+        Matcher matcher = pattern.matcher(line);
+        if (matcher.matches()) {
+          System.out.println(matcher.group(1));
+        }
+      }
+    } catch (IOException exception) {
+      exception.printStackTrace();
+    }
+  }
+  
+  private LdLibraryPathPollutionTester () {
+  }
+}
