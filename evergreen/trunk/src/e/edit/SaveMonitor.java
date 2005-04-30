4c9c6c95600cce3a9f543f052cf4af259d39d396
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-04-30 00:49:30 +0000

    src/e/edit/SaveMonitor.java: a new class to notify interested parties that a file has just been saved.
    src/e/edit/ETextWindow.java: tell the SaveMonitor when a file's been saved.
    
    src/e/edit/FindFilesDialog.java: use the SaveMonitor's notifications to re-do
    the search.

---
+++ evergreen/trunk/src/e/edit/SaveMonitor.java
@@ -0,0 +1,48 @@
+package e.edit;
+
+import java.util.*;
+
+/**
+ * Used by the FindFilesDialog to know when results are potentially out of
+ * date.
+ */
+public class SaveMonitor {
+    private static final SaveMonitor INSTANCE = new SaveMonitor();
+    
+    public static SaveMonitor getInstance() {
+        return INSTANCE;
+    }
+    
+    private SaveMonitor() {
+    }
+    
+    private ArrayList saveListeners = new ArrayList();
+    
+    /**
+     * Adds a listener to be notified when any file is saved.
+     */
+    public synchronized void addSaveListener(SaveMonitor.Listener saveListener) {
+        saveListeners.add(saveListener);
+    }
+    
+    public synchronized void removeSaveListener(SaveMonitor.Listener saveListener) {
+        saveListeners.remove(saveListener);
+    }
+    
+    /**
+     * Informs all listeners that a file has been saved.
+     */
+    public synchronized void fireSaveListeners() {
+        for (int i = saveListeners.size() - 1; i >= 0; --i) {
+            SaveMonitor.Listener listener = (SaveMonitor.Listener) saveListeners.get(i);
+            listener.fileSaved();
+        }
+    }
+    
+    public interface Listener {
+        /**
+         * Invoked when a file is saved.
+         */
+        public void fileSaved();
+    }
+}
