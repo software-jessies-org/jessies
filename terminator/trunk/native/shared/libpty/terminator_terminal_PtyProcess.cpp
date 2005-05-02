68ffbdce20917af186a24bf0eca1ae98c4b31fa2
pcn <pcn@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-05-02 13:09:04 +0000

    native/Darwin/libpty/Darwin_stubs.c: We don't need this in its own file anymore, because...
    native/shared/libpty/terminator_terminal_PtyProcess.cpp: ...it's now included here, in a copy/paste job from Elliott's comment in ptyopen.cpp.
    
    native/Darwin/libpty/Makefile: Darwin's makefile now realises this.

--- terminator/trunk/native/shared/libpty/terminator_terminal_PtyProcess.cpp
+++ terminator/trunk/native/shared/libpty/terminator_terminal_PtyProcess.cpp
@@ -20,10 +20,28 @@
 #include <sstream>
 #include <string>
 
-/* Some systems don't have the new pty stuff defined. */
-extern char *ptsname(int fd);
-extern int grantpt(int fd);
-extern int unlockpt(int fd);
+#ifdef __APPLE__
+
+// Apple has helpfully commented these out in stdlib.h, so although the code
+// in this file should compile on all POSIX systems, we need these stubs on
+// Mac OS 10.3 where I notice there's also a posix_openpt(3) which sounds
+// like it would pretty interesting if it weren't also commented out:
+// http://www.opengroup.org/onlinepubs/009695399/functions/posix_openpt.html
+
+int grantpt(int) {
+    return -1;
+}
+
+char* ptsname(int) {
+    return 0;
+}
+
+int unlockpt(int) {
+    return -1;
+}
+
+#endif
+
 
 class IOException {
     std::string message;
