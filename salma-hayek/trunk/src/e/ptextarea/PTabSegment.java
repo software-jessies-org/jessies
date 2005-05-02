56ef75953a7d1e0e1b6fd0f9ba672006471eead4
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-05-02 21:26:01 +0000

    src/e/ptextarea/PTabSegment.java: remove the hard-coded tab width of 20 pixels, and use a FontMetrics-dependent width of 8 spaces. (The 8 is currently hard-coded, but fits traditional practice.) Also make it clear by renaming what units "MIN_TAB_WIDTH" is measured in. This patch makes stl_algobase.h readable, where it wasn't before.

--- salma-hayek/trunk/src/e/ptextarea/PTabSegment.java
+++ salma-hayek/trunk/src/e/ptextarea/PTabSegment.java
@@ -13,8 +13,9 @@ import java.awt.*;
 public class PTabSegment extends PAbstractSegment {
     public static final PTabSegment SINGLE_TAB = new PTabSegment(null, 0, 1);
     
-    private static final int MIN_TAB_WIDTH = 5;
-    private static final int TAB_WIDTH = 20;
+    private static final int MIN_TAB_WIDTH_IN_PIXELS = 5;
+    
+    private static final int SPACES_PER_TAB = 8;
     
     public PTabSegment(PTextArea textArea, int start, int end) {
         super(textArea, start, end, PStyle.NORMAL);
@@ -24,16 +25,22 @@ public class PTabSegment extends PAbstractSegment {
         return new PTabSegment(textArea, start + this.start, end + this.start);
     }
     
+    private int tabWidth(FontMetrics metrics) {
+        return (SPACES_PER_TAB * metrics.charWidth(' '));
+    }
+    
     public int getDisplayWidth(FontMetrics metrics, int startX) {
-        int x = startX + MIN_TAB_WIDTH + TAB_WIDTH * getLength();
-        x -= x % TAB_WIDTH;
+        final int tabWidth = tabWidth(metrics);
+        int x = startX + MIN_TAB_WIDTH_IN_PIXELS + tabWidth * getLength();
+        x -= x % tabWidth;
         return (x - startX);
     }
     
     public int getCharOffset(FontMetrics metrics, int startX, int x) {
+        final int tabWidth = tabWidth(metrics);
         for (int i = 0; i < getLength(); i++) {
-            int nextX = startX + MIN_TAB_WIDTH + TAB_WIDTH;
-            nextX -= nextX % TAB_WIDTH;
+            int nextX = startX + MIN_TAB_WIDTH_IN_PIXELS + tabWidth;
+            nextX -= nextX % tabWidth;
             if (x < nextX) {
                 if (x > (nextX + startX) / 2) {
                     return i + 1;
