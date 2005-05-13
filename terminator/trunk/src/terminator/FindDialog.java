ca2bd8820d4fd0e65259abfd70bb037a7892cf73
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-05-13 06:52:34 +0000

    src/terminator/FindDialog.java: track API change. I've left some code commented out here because I should come back and look at it, but I want to have things fixed before the 00:00 PST build starts.

--- terminator/trunk/src/terminator/FindDialog.java
+++ terminator/trunk/src/terminator/FindDialog.java
@@ -12,11 +12,12 @@ import terminator.view.highlight.*;
 public class FindDialog {
     private static final FindDialog INSTANCE = new FindDialog();
     
-    private FindField findField = new FindField();
+    private JTextField findField = new JTextField(40);
     private JLabel findStatus = new JLabel(" ");
     private JTextBuffer textToFindIn;
     
     private FindDialog() {
+        FormDialog.markAsMonitoredField(findField);
     }
     
     public static FindDialog getSharedInstance() {
@@ -31,22 +32,31 @@ public class FindDialog {
         FormPanel formPanel = new FormPanel();
         formPanel.addRow("Find:", findField);
         formPanel.setStatusBar(findStatus);
+        formPanel.setTypingTimeoutActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String regularExpression = findField.getText();
+                try {
+                    int matchCount = getFindHighlighter().setRegularExpression(textToFindIn, regularExpression);
+                    findStatus.setText("Matches: " + matchCount);
+                    findField.setForeground(UIManager.getColor("TextField.foreground"));
+                } catch (PatternSyntaxException ex) {
+                    findField.setForeground(Color.RED);
+                    findStatus.setText(ex.getDescription());
+                }
+            }
+        });
         FormDialog.showNonModal(frame, "Find", formPanel);
         
         findField.selectAll();
         findField.requestFocus();
         findStatus.setText(" ");
-        
-        findField.find();
     }
     
     private FindHighlighter getFindHighlighter() {
         return (FindHighlighter) textToFindIn.getHighlighterOfClass(FindHighlighter.class);
     }
     
-    private class FindField extends EMonitoredTextField {
-        public FindField() {
-            super(40);
+    /*
             addKeyListener(new KeyAdapter() {
                 public void keyTyped(KeyEvent e) {
                     if (textToFindIn != null && e.getKeyChar() == '\n') {
@@ -73,22 +83,5 @@ public class FindDialog {
                     }
                 }
             });
-        }
-        
-        public void timerExpired() {
-            find();
-        }
-        
-        public void find() {
-            String regularExpression = getText();
-            try {
-                int matchCount = getFindHighlighter().setRegularExpression(textToFindIn, regularExpression);
-                findStatus.setText("Matches: " + matchCount);
-                setForeground(UIManager.getColor("TextField.foreground"));
-            } catch (PatternSyntaxException ex) {
-                setForeground(Color.RED);
-                findStatus.setText(ex.getDescription());
-            }
-        }
-    }
+    */
 }
