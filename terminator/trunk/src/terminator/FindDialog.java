37a09a61a141cd02e1a1b5345413df4355de0c05
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-06-26 00:42:09 +0000

    src/terminator/FindDialog.java: fix the cancel behavior of the find dialog, so it's once again possible to cancel the current find. (The new implementation is nicer than the old, commented-out implementation that was recently broken and never fixed.)

--- terminator/trunk/src/terminator/FindDialog.java
+++ terminator/trunk/src/terminator/FindDialog.java
@@ -44,7 +44,13 @@ public class FindDialog {
                 }
             }
         });
-        FormDialog.showNonModal(frame, "Find", formPanel);
+        
+        FormDialog dialog = FormDialog.showNonModal(frame, "Find", formPanel);
+        dialog.setCancelRunnable(new Runnable() {
+            public void run() {
+                getFindHighlighter().forgetRegularExpression(textToFindIn);
+            }
+        });
         
         findField.selectAll();
         findField.requestFocus();
@@ -61,13 +67,6 @@ public class FindDialog {
                     if (textToFindIn != null && e.getKeyChar() == '\n') {
                         find();
                         e.consume();
-                        textToFindIn.requestFocus();
-                    }
-                }
-                
-                public void keyPressed(KeyEvent e) {
-                    if (textToFindIn != null && e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-                        getFindHighlighter().forgetRegularExpression(textToFindIn);
                         textToFindIn.requestFocus();
                     }
                 }
