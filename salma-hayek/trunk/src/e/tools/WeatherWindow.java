c1de63900f802562ef531603704a2e90efd12ec8
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-06-30 06:26:34 +0000

    src/e/tools/WeatherWindow.java: scale the BBC images down so they take up less of the display. Scrape the city name from the HTML, and use it as the window title. Add padding between the individual days' forecasts. Use different fonts for the day names and the temperatures.

--- salma-hayek/trunk/src/e/tools/WeatherWindow.java
+++ salma-hayek/trunk/src/e/tools/WeatherWindow.java
@@ -2,20 +2,27 @@ package e.tools;
 
 import e.util.*;
 import java.awt.*;
+import java.awt.geom.*;
+import java.awt.image.*;
 import java.net.*;
 import java.io.*;
 import java.util.*;
 import java.util.regex.*;
+import javax.imageio.*;
 import javax.swing.*;
 
 public class WeatherWindow extends JFrame {
+    private static final Color BACKGROUND_COLOR = new Color(0.3647f, 0.6941f, 0.8863f);
+    private static final Font DAY_FONT = new Font("SansSerif", Font.BOLD, 12);
+    private static final Font TEMPERATURE_FONT = new Font("SansSerif", Font.PLAIN, 10);
+    
     public static void main(String[] args) {
         new WeatherWindow().setVisible(true);
     }
     
     public WeatherWindow() {
         super("Weather");
-        setBackground(new Color(0.3647f, 0.6941f, 0.8863f));
+        setBackground(BACKGROUND_COLOR);
         setContentPane(makeUi());
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         pack();
@@ -44,16 +51,19 @@ public class WeatherWindow extends JFrame {
         
         ArrayList<DayForecast> forecast = new ArrayList<DayForecast>();
         
+        Pattern cityNamePattern = Pattern.compile("^\\s+<title>BBC.* Forecast in .* for (\\S+),");
         Pattern dayNamePattern = Pattern.compile("class=\"weatherday\".*strong>(\\S+)<br");
         Pattern imageUrlPattern = Pattern.compile("img src=\"(\\S+\\/fiveday_sym\\/\\d+\\S+)\"");
         Pattern dayTemperaturePattern = Pattern.compile("class=\"temptxt\"><strong>(\\d+)<.*Day ");
         Pattern nightTemperaturePattern = Pattern.compile("class=\"temptxt\">.*<strong>(\\d+)<.*Night ");
-        Matcher matcher;
         String dayName = null;
         String imageUrl = null;
         int dayTemperatureC = 0;
+        Matcher matcher;
         for (String line : content.split("\n")) {
-            if ((matcher = dayNamePattern.matcher(line)).find()) {
+            if ((matcher = cityNamePattern.matcher(line)).find()) {
+                setTitle(matcher.group(1) + " 5 Day Forecast");
+            } else if ((matcher = dayNamePattern.matcher(line)).find()) {
                 dayName = matcher.group(1);
             } else if ((matcher = imageUrlPattern.matcher(line)).find()) {
                 imageUrl = "http://www.bbc.co.uk" + matcher.group(1);
@@ -84,7 +94,7 @@ public class WeatherWindow extends JFrame {
         }
         */
         
-        JPanel result = new JPanel(new FlowLayout());
+        JPanel result = new JPanel(new FlowLayout(FlowLayout.CENTER, 25, 8));
         for (DayForecast day : forecast) {
             result.add(day);
         }
@@ -101,8 +111,10 @@ public class WeatherWindow extends JFrame {
             super(new BorderLayout());
             this.day = day;
             try {
-                this.icon = new ImageIcon(new URL(imageUrl));
-            } catch (java.io.IOException ex) {
+                BufferedImage image = ImageIO.read(new URL(imageUrl));
+                BufferedImage scaledImage = new AffineTransformOp(AffineTransform.getScaleInstance(0.5, 0.5), AffineTransformOp.TYPE_BILINEAR).filter(image, null);
+                this.icon = new ImageIcon(scaledImage);
+            } catch (IOException ex) {
                 ex.printStackTrace();
             }
             this.dayTemperatureC = dayTemperatureC;
@@ -110,18 +122,17 @@ public class WeatherWindow extends JFrame {
             initUi();
         }
         
-        private JLabel makeLabel(String text) {
+        private JLabel makeLabel(String text, Font font) {
             JLabel label = new JLabel(text, SwingConstants.CENTER);
-            //label.setBackground(new Color(0.3647f, 0.6941f, 0.8863f));
-            //label.setOpaque(true);
+            label.setFont(font);
             return label;
         }
         
         private void initUi() {
-            add(makeLabel(day), BorderLayout.NORTH);
+            add(makeLabel(day, DAY_FONT), BorderLayout.NORTH);
             add(new JLabel(icon), BorderLayout.CENTER);
             final String degreesC = "\u00b0C";
-            add(makeLabel(dayTemperatureC + degreesC + " / " + nightTemperatureC + degreesC), BorderLayout.SOUTH);
+            add(makeLabel(dayTemperatureC + degreesC + " / " + nightTemperatureC + degreesC, TEMPERATURE_FONT), BorderLayout.SOUTH);
         }
         
         public String toString() {
