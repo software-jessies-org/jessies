56c671e4d6906e7f940107b3588554d83227624a
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-06-30 16:37:46 +0000

    src/e/edit/EditServer.java: add a new "open-and-block" verb that waits for the window to be closed before closing the connection.
    bin/edit: 
    bin/edit-and-block: add a "--block" option to the "edit" script, and add a new "edit-and-block" script for use with the $EDITOR environment variable. This lets you use Edit to edit your crontab. (There may be other applications, but I don't have any. "bk resolve", though visually confusing, waits for you to give it the "C" command anyway. And I haven't used "cvs commit", the other place I've wanted this functionality, since a couple of hours in to writing SCM.)

--- evergreen/trunk/src/e/edit/EditServer.java
+++ evergreen/trunk/src/e/edit/EditServer.java
@@ -1,5 +1,6 @@
 package e.edit;
 
+import java.awt.event.*;
 import java.io.*;
 import javax.swing.*;
 
@@ -11,9 +12,12 @@ public final class EditServer extends InAppServer {
     }
     
     public boolean handleCommand(String line, PrintWriter out) {
-        if (line.startsWith("open ")) {
+        if (line.startsWith("open-and-block ")) {
+            String filename = line.substring("open-and-block ".length());
+            handleOpen(filename, true, out);
+        } else if (line.startsWith("open ")) {
             String filename = line.substring("open ".length());
-            handleOpen(filename, out);
+            handleOpen(filename, false, out);
         } else if (line.equals("remember-state")) {
             Edit.getInstance().rememberState();
         } else if (line.equals("save-all")) {
@@ -24,21 +28,61 @@ public final class EditServer extends InAppServer {
         return true;
     }
     
-    private void handleOpen(final String filename, final PrintWriter out) {
-        try {
-            SwingUtilities.invokeAndWait(new Runnable() {
-                public void run() {
-                    try {
-                        Edit.openFileNonInteractively(filename);
-                        Edit.getFrame().toFront();
-                        out.println("File '" + filename + "' opened OK.");
-                    } catch (Exception ex) {
-                        out.println(ex.getMessage());
+    private class Opener implements Runnable {
+        private String filename;
+        private boolean shouldBlock;
+        private PrintWriter out;
+        private EWindow window;
+        
+        public Opener(final String filename, final PrintWriter out) {
+            this.filename = filename;
+            this.out = out;
+        }
+        
+        public EWindow open() {
+            try {
+                SwingUtilities.invokeAndWait(this);
+            } catch (Exception ex) {
+                out.println(ex.getMessage());
+            }
+            return window;
+        }
+        
+        public void run() {
+            try {
+                this.window = Edit.openFileNonInteractively(filename);
+                Edit.getFrame().toFront();
+                out.println("File '" + filename + "' opened OK.");
+            } catch (Exception ex) {
+                out.println(ex.getMessage());
+            }
+        }
+    }
+    
+    private void handleOpen(final String filename, final boolean shouldBlock, final PrintWriter out) {
+        Opener opener = new Opener(filename, out);
+        final EWindow window = opener.open();
+        if (shouldBlock) {
+            final Object lock = new Object();
+            // FIXME: is this really the easiest way to watch for the component being removed from the hierarchy?
+            window.addHierarchyListener(new HierarchyListener() {
+                public void hierarchyChanged(HierarchyEvent e) {
+                    if ((e.getChangeFlags() & HierarchyEvent.SHOWING_CHANGED) != 0 && window.isShowing() == false) {
+                        System.out.println(window + " closed");
+                        synchronized (lock) {
+                            lock.notify();
+                        }
                     }
                 }
             });
-        } catch (Exception ex) {
-            out.println(ex.getMessage());
+            try {
+                synchronized (lock) {
+                    lock.wait();
+                }
+            } catch (Exception ex) {
+                out.println(ex.getMessage());
+            }
+            out.println("Closed " + filename);
         }
     }
 }
