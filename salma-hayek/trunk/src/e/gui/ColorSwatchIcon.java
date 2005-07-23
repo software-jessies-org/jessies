042d6a65874b430b094f010c4bac7e9a4dad2596
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-07-23 20:10:06 +0000

    src/e/gui/ColorSwatchIcon.java: support null, which doesn't draw anything.

--- salma-hayek/trunk/src/e/gui/ColorSwatchIcon.java
+++ salma-hayek/trunk/src/e/gui/ColorSwatchIcon.java
@@ -5,18 +5,26 @@ import java.awt.*;
 /**
  * A rectangular color swatch useful for giving a preview of a color.
  */
-public class ColorSwatchIcon extends DrawnIcon {
+public final class ColorSwatchIcon extends DrawnIcon {
     private Color color;
     private Color borderColor;
     
     public ColorSwatchIcon(Color color, Dimension size) {
         super(size);
+        setColor(color);
+    }
+    
+    public void setColor(Color color) {
         this.color = color;
-        this.borderColor = color.darker();
+        this.borderColor = (color != null) ? color.darker() : null;
     }
     
     @Override
     public void paintIcon(Component c, Graphics g, int x, int y) {
+        if (color == null) {
+            return;
+        }
+        
         g.setColor(color);
         g.fillRect(x, y, getIconWidth(), getIconHeight());
         g.setColor(borderColor);
