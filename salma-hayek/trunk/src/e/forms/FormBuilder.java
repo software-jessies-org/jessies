166a2b0e728e98614a2f339e066a81fc09850273
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-08-04 04:56:47 +0000

    src/e/forms/FormBuilder.java: too tired to code, I can still write some documentation for a very important class.

--- salma-hayek/trunk/src/e/forms/FormBuilder.java
+++ salma-hayek/trunk/src/e/forms/FormBuilder.java
@@ -2,27 +2,57 @@ package e.forms;
 
 import java.awt.*;
 
+/**
+ * Creates and shows form dialogs. A form dialog is a simple collection of
+ * input fields, as you'd see in most general dialogs, properties dialogs,
+ * and preference dialogs. A field can be a complex component such as a text
+ * area, table, or whatever.
+ * 
+ * There's automatic support for "as-you-type" operation applied to the form's
+ * text fields. See FormPanel and FormDialog for more customization details.
+ */
 public class FormBuilder {
     FormPanel formPanel;
     FormDialog formDialog;
     
+    /**
+     * Gets ready to show a form with a given name as a child of the
+     * given Frame.
+     */
     public FormBuilder(Frame parent, String title) {
         this.formPanel = new FormPanel();
         this.formDialog = new FormDialog(parent, title, formPanel);
     }
     
+    /**
+     * Returns the underlying FormDialog; only needed for advanced
+     * customization.
+     */
     public FormDialog getFormDialog() {
         return formDialog;
     }
     
+    /**
+     * Returns the underlying FormPanel; use this to add content to your
+     * form. Keep hold of the components you give to the FormPanel so that
+     * you can read values back from them when the dialog has been accepted.
+     */
     public FormPanel getFormPanel() {
         return formPanel;
     }
     
+    /**
+     * Shows the dialog with the given verb on the "OK" button, and returns
+     * true if that button (rather than "Cancel") was chosen.
+     */
     public boolean show(String actionLabel) {
         return formDialog.show(actionLabel);
     }
     
+    /**
+     * Shows the dialog non-modally, with just a "Close" button instead of
+     * an action button and a "Cancel" button.
+     */
     public void showNonModal() {
         formDialog.showNonModal();
     }
