de6d18520803a1546a34079401c42b37707e33b5
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-09-11 17:41:46 +0000

    native/Darwin/BringProcessToFront/BringProcessToFront.mm: new tool to benefit Edit.

---
+++ salma-hayek/trunk/native/Darwin/BringProcessToFront/BringProcessToFront.mm
@@ -0,0 +1,30 @@
+#include <Carbon/Carbon.h>
+#include <iostream>
+
+/*
+ * On Mac OS, there's a strong notion of application. One consequence is that
+ * toFront doesn't work on an application that isn't the one the user's
+ * interacting with. Normally this is fine, if not preferable, but it's awkward
+ * for a Java program that wants to be brought to the front via indirect user
+ * request. For example, without this, it's impossible for the "edit" script
+ * to bring Edit to the front on Mac OS.
+ * 
+ * (This file is Objective C++ simply so that the make rules add
+ * "-framework Cocoa", which will drag in Carbon for us.)
+ */
+
+static void bringProcessToFront(pid_t pid) {
+    ProcessSerialNumber psn;
+    OSStatus status = GetProcessForPID(pid, &psn);
+    if (status == noErr) {
+        status = SetFrontProcess(&psn);
+    }
+    if (status != noErr) {
+        std::cout << "pid " << pid << " = " << status << std::endl;
+    }
+}
+
+int main(int, char*[]) {
+    bringProcessToFront(getppid());
+    return 0;
+}
