3ef1166ac63ef9706b3053b03c7d2b912bb816a8
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-10-11 05:39:49 +0000

    src/e/tools/Xv.java: improve our behavior when given large images.

--- salma-hayek/trunk/src/e/tools/Xv.java
+++ salma-hayek/trunk/src/e/tools/Xv.java
@@ -1,21 +1,34 @@
 package e.tools;
 
-import javax.swing.*;
 import e.util.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+import javax.swing.*;
 
 public class Xv extends JFrame {
     
-    public Xv(String filename) {
+    public Xv(String filename) throws IOException {
         super(filename);
-        Log.warn("Opening '" + filename + "'.");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setContentPane(new JScrollPane(new JLabel(new ImageIcon(filename))));
-        pack();
-        setLocationRelativeTo(null);
+        
+        BufferedImage image = ImageIO.read(new File(filename));
+        Log.warn("Opened '" + filename + "' (" + image.getWidth() + "x" + image.getHeight() + ").");
+        
+        setContentPane(new JScrollPane(new JLabel(new ImageIcon(image))));
+        
+        Dimension displaySize = getGraphicsConfiguration().getBounds().getSize();
+        if (image.getWidth() > displaySize.width || image.getHeight() > displaySize.height) {
+            setSize(displaySize.width / 2, displaySize.height / 2);
+        } else {
+            pack();
+        }
+        setLocationByPlatform(true);
         setVisible(true);
     }
 
-    public static void main(String[] arguments) {
+    public static void main(String[] arguments) throws IOException {
         Log.setApplicationName("Xv");
         GuiUtilities.initLookAndFeel();
         for (String argument : arguments) {
