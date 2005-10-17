e43ff900355b3e8c6a110618e0934450cd3359f2
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-10-17 14:03:36 +0000

    bin/get-jessies-svn-user.rb: A token effort at producing a more useful additional error message to add to that produced by Subversion when this is run from /usr/local/bin or wherever.
    I'm not planning on editing make-new-project.rb to use this, partly because I don't want to test it.  Elliott might like to.

--- salma-hayek/trunk/bin/get-jessies-svn-user.rb
+++ salma-hayek/trunk/bin/get-jessies-svn-user.rb
@@ -10,5 +10,8 @@ salma_hayek = Pathname.new("#{__FILE__}/..").realpath().dirname()
   |line|
   if line.match(/svn\+ssh:\/\/(\w+)@/)
     puts($1)
+    exit(0)
   end
 }
+$stderr.puts("the copy of get-jessies-svn-user.rb which you run needs to be in a checked-out Subversion work area")
+$stderr.puts("this copy was #{__FILE__}")
