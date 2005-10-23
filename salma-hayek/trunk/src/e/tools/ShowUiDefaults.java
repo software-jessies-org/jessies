a16363821c8420a8dec432a5a5baaeaa25ec0d9d
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-10-23 23:37:10 +0000

    src/e/tools/ShowUiDefaults.java: I've long had a local modification to set the system look and feel, and decided that it was about time this program showed the defaults for all LAFs. Also, since you always want to pipe the output through sort, we may as well produce sorted output ourselves.

--- salma-hayek/trunk/src/e/tools/ShowUiDefaults.java
+++ salma-hayek/trunk/src/e/tools/ShowUiDefaults.java
@@ -7,20 +7,44 @@ import javax.swing.*;
  * Dumps the system's Swing UI defaults to standard output, for your grep(1)
  * pleasure. I'm always wanting to see if there's a UI default I can get a
  * useful default for my own code from, and it's always a pain to trawl
- * through the source (or at least to find the right directory). This, if
- * I can just remember its existence, should fix that.
- * FIXME: should showing the values be an option? It makes the output harder
- * to read, and isn't usually what I'm interested in.
+ * through the source (or at least to find the right directory).
+ * 
+ * In addition, we show the UI defaults for all installed LAFs, to make it
+ * easier to check that a default you want to use exists in the other LAFs,
+ * or to make it easier to compare values.
  */
 public class ShowUiDefaults {
     private ShowUiDefaults() {
     }
     
-    public static void main(String[] args) {
+    private static void showUiDefaults() {
+        UIManager.LookAndFeelInfo[] lafs = UIManager.getInstalledLookAndFeels();
+        for (UIManager.LookAndFeelInfo laf : lafs) {
+            showUiDefaultsForLaf(laf);
+        }
+    }
+    
+    private static void showUiDefaultsForLaf(UIManager.LookAndFeelInfo laf) {
+        try {
+            UIManager.setLookAndFeel(laf.getClassName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return;
+        }
+        
+        ArrayList<String> list = new ArrayList<String>();
         UIDefaults defaults = UIManager.getLookAndFeelDefaults();
         for (Enumeration e = defaults.keys(); e.hasMoreElements();) {
             Object key = e.nextElement();
-            System.out.println(key + "=" + defaults.get(key));
+            list.add(laf.getName() + ":" + key + "=" + defaults.get(key));
         }
+        Collections.sort(list, String.CASE_INSENSITIVE_ORDER);
+        for (String line : list) {
+            System.out.println(line);
+        }
+    }
+    
+    public static void main(String[] args) {
+        showUiDefaults();
     }
 }
