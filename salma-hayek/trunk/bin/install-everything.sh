3fdd934978beec5bbd6817553743ee4f4e7d8b82
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-10-24 00:39:30 +0000

    bin/install-everything.sh: $JAVA_HOME is dead.

--- salma-hayek/trunk/bin/install-everything.sh
+++ salma-hayek/trunk/bin/install-everything.sh
@@ -18,18 +18,18 @@ fi
 
 # Install Java in /usr/local, and put links to java and javac in /usr/local/bin.
 java_installer=/net/mirror/mirror-link/java/jdk-1_5_0_03-linux-i586.bin
-export JAVA_HOME=/usr/local/jdk1.5.0_03
-if sudo -u devadmin test -f $java_installer && ! test -d $JAVA_HOME ; then
+java_home=/usr/local/jdk1.5.0_03
+if sudo -u devadmin test -f $java_installer && ! test -d $java_home ; then
     sudo -u devadmin cp $java_installer /tmp/jdk &&
     /tmp/jdk ||
     die "installing Java"
 fi
-if test -d $JAVA_HOME ; then
-    link_in_usr_local_bin $JAVA_HOME/bin/java
-    link_in_usr_local_bin $JAVA_HOME/bin/javac
+if test -d $java_home ; then
+    link_in_usr_local_bin $java_home/bin/java
+    link_in_usr_local_bin $java_home/bin/javac
     for BROWSER in mozilla mozilla-firefox; do
         if test -d /usr/lib/$BROWSER/plugins; then
-            ln -s $JAVA_HOME/jre/plugin/i386/ns7/libjavaplugin_oji.so /usr/lib/$BROWSER/plugins
+            ln -s $java_home/jre/plugin/i386/ns7/libjavaplugin_oji.so /usr/lib/$BROWSER/plugins
         fi
     done
 fi
