c21857274ddfed089215d02ddcb46e57902fb179
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-10-24 00:27:24 +0000

    src/e/ptextarea/PBashTextStyler.java: a new styler for bash(1) scripts.

---
+++ salma-hayek/trunk/src/e/ptextarea/PBashTextStyler.java
@@ -0,0 +1,94 @@
+package e.ptextarea;
+
+import java.util.*;
+
+public class PBashTextStyler extends PCLikeTextStyler {
+    private static final String[] KEYWORDS = new String[] {
+        // "Compound Commands", bash man page.
+        "case",
+        "do",
+        "done",
+        "elif",
+        "else",
+        "esac",
+        "for",
+        "fi",
+        "function",
+        "if",
+        "in",
+        "select",
+        "then",
+        "until",
+        "while",
+        // "SHELL BUILTIN COMMANDS", bash man page.
+        "source",
+        "alias",
+        "bg",
+        "bind",
+        "break",
+        "builtin",
+        "cd",
+        "caller",
+        "command",
+        "compgen",
+        "complete",
+        "continue",
+        "declare",
+        "typeset",
+        "dirs",
+        "disown",
+        "echo",
+        "enable",
+        "eval",
+        "exec",
+        "exit",
+        "export",
+        "fc",
+        "fg",
+        "getopts",
+        "hash",
+        "help",
+        "history",
+        "jobs",
+        "kill",
+        "let",
+        "local",
+        "logout",
+        "popd",
+        "printf",
+        "pushd",
+        "pwd",
+        "read",
+        "readonly",
+        "return",
+        "set",
+        "shift",
+        "shopt",
+        "suspend",
+        "test",
+        "times",
+        "trap",
+        "type",
+        "ulimit",
+        "umask",
+        "unalias",
+        "unset",
+        "wait"
+    };
+    
+    public PBashTextStyler(PTextArea textArea) {
+        super(textArea);
+    }
+    
+    public boolean supportShellComments() {
+        return true;
+    }
+    
+    public boolean supportDoubleSlashComments() {
+        return false;
+    }
+    
+    public void addKeywordsTo(Collection<String> collection) {
+        collection.addAll(Arrays.asList(KEYWORDS));
+    }
+}
