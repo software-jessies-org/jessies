5aeeab031cee194a9b31a6cdfaacf7cbd8f7680a
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-11-03 04:31:29 +0000

    bin/invoke-java.rb: scmtool requires scm/classes to be on the classpath, which means that we need to cope with project_root not being salma_hayek.  It also uses MRJ141Stubs.jar and passes extra arguments to e/util/Launcher, as does Edit.  Add a comment hinting that I'm not able to test this on Mac OS X.
    bin/fatbits: Cope with the extra_arguments list.

--- salma-hayek/trunk/bin/invoke-java.rb
+++ salma-hayek/trunk/bin/invoke-java.rb
@@ -3,8 +3,10 @@
 require 'fileutils.rb'
 require 'pathname.rb'
 
-def invoke_java(dock_name, dock_icon, class_name)
-  salma_hayek = Pathname.new("#{__FILE__}/..").realpath().dirname()
+def invoke_java(dock_name, dock_icon, class_name, extra_arguments)
+  project_root = Pathname.new("#{$0}/..").realpath().dirname()
+  salma_hayek = Pathname.new("#{project_root}/../salma-hayek").realpath()
+  
   # Cope with symbolic links to this script.
   require "#{salma_hayek}/bin/target-os.rb"
   
@@ -13,12 +15,19 @@ def invoke_java(dock_name, dock_icon, class_name)
     heap_size="100m"
   end
   
-  args = [ "java", "-Xmx#{heap_size}", "-cp", "#{salma_hayek}/classes" ]
+  class_path = [ "#{project_root}/classes", "#{salma_hayek}/classes" ].uniq()
+  if target_os() != "Darwin"
+    class_path << "#{salma_hayek}/MRJ141Stubs.jar"
+  end
+  
+  args = [ "java", "-Xmx#{heap_size}", "-cp", class_path.join(":") ]
   if target_os() == "Darwin"
     args << "-Xdock:name=#{dock_name}"
+    # Does it matter that we set this to the empty string where previously we left the icon argument off?
     args << "-Xdock:icon=#{dock_icon}"
   end
   args << class_name
+  args.concat(extra_arguments)
   args.concat(ARGV)
   return system(*args)
 end
