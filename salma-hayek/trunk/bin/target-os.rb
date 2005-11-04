04c6d5c50c29390316021f5e09ae94815d83fbe1
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-11-04 20:10:35 +0000

    bin/target-os.rb: cache the result of uname(1) so callers can invoke target_os() as often as they like but only pay once.

--- salma-hayek/trunk/bin/target-os.rb
+++ salma-hayek/trunk/bin/target-os.rb
@@ -1,7 +1,22 @@
 #!/usr/bin/ruby -w
 
+require "singleton.rb"
+
+class OsExaminer
+    include Singleton
+    
+    def initialize
+        # Cache the result of invoking uname(1) and mangling its output.
+        @os_name = `uname`.chomp().sub(/CYGWIN.*/, "Cygwin")
+    end
+    
+    def os_name
+        return @os_name
+    end
+end
+
 def target_os()
-    return `uname`.chomp().sub(/CYGWIN.*/, "Cygwin")
+    return OsExaminer.instance().os_name()
 end
 
 if __FILE__ == $0
