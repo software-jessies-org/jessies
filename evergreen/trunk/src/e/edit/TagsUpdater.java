513f22dfb02dec12137c5bfaafbe902c063c41b6
enp <enp@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-11-05 15:32:22 +0000

    src/e/edit/TagsUpdater.java: Revert inadvertent change.

--- evergreen/trunk/src/e/edit/TagsUpdater.java
+++ evergreen/trunk/src/e/edit/TagsUpdater.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.util.*;
-import java.util.concurrent.*;
 import javax.swing.*;
 import javax.swing.Timer;
 import javax.swing.tree.*;
