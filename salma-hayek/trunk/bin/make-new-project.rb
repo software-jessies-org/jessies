bc1b40d3a7e2c63cfc03a291bb037294643b0a63
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-11-06 21:45:21 +0000

    bin/make-new-project.rb: discussion on the mailing list got us to the conclusion that the attempt to work around ownership problems in this script was insufficient, and that the right fix is the one now included as a comment in this script.

--- salma-hayek/trunk/bin/make-new-project.rb
+++ salma-hayek/trunk/bin/make-new-project.rb
@@ -1,25 +1,34 @@
 #!/bin/bash
-# usage: make-new-project.rb <name>
 
-# It's important that you make a new project as the general repository owner
-# so that file permissions are correct.
-# mad alleges that other users' umask will be wrong, and (presumably) that
-# the chmod in this script is insufficient to fix the problem.
+# It's important that your server's svn(1) be a wrapper like this:
+# 
+# $ cat `type -p svn`
+# #!/bin/sh
+# umask 002
+# /usr/bin/svn.orig "$@"
+# $
+#
+# It's not possible to work around ownership/permissions problems in this
+# script, because each check-in risks messing things up unless the umask is
+# always set appropriately. Subversion itself does nothing to help you with
+# this.
 
-# It's also important that you check out again as your individual owner
-# before committing actual changes to the repository.
+if [ $# != 2 ]; then
+    echo "Usage: $0 <username> <project-name>"
+    exit 1
+fi
 
-# Hopefully we'll have moved to Mercurial or something before we next need to
-# worry about this.
-svn_host=software@jessies.org
-
-# ----------------------------------------------------------------------------
+# Hard-coding @jessies.org might seem like a mistake, but it's just to make
+# it clear that the rest of this script still makes assumptions about
+# directory layout and the like that you will need to change if you change
+# this line.
+svn_user_and_host=$1@jessies.org
+project_name=$2
 
 projects_dir=~/Projects
 
-name=$1
-if [ -d $projects_dir/$name ]; then
-    echo "Project '$name' already exists!"
+if [ -d $projects_dir/$project_name ]; then
+    echo "Project '$project_name' already exists!"
     exit 1
 fi
 if [ ! -f $projects_dir/edit/COPYING ]; then
@@ -27,39 +36,39 @@ if [ ! -f $projects_dir/edit/COPYING ]; then
     exit 1
 fi
 
-echo "Creating new project '$name'..."
+echo "Creating new project '$project_name'..."
 
 mkdir -p /tmp/$$
 cd /tmp/$$
 
 echo "Creating directories..."
-mkdir -p $name
-mkdir -p $name/src
+mkdir -p $project_name
+mkdir -p $project_name/src
 
 echo "Creating Makefile..."
-cat > $name/Makefile <<EOF
+cat > $project_name/Makefile <<EOF
 include ../salma-hayek/universal.make
 EOF
 
 echo "Adding GPL..."
-cp $projects_dir/edit/COPYING $name/COPYING
+cp $projects_dir/edit/COPYING $project_name/COPYING
 
 echo "Creating a new Subversion repository..."
-ssh $svn_host svnadmin create /home/software/svnroot/$name
-ssh $svn_host chmod -R g+w /home/software/svnroot/$name/db
-ssh $svn_host ln -s /home/software/checked-out/salma-hayek/post-commit /home/software/svnroot/$name/hooks
+ssh $svn_user_and_host svnadmin create /home/software/svnroot/$project_name
+ssh $svn_user_and_host chmod -R g+w /home/software/svnroot/$project_name/db
+ssh $svn_user_and_host ln -s /home/software/checked-out/salma-hayek/post-commit /home/software/svnroot/$project_name/hooks
 echo "Making the initial import..."
-svn import $name svn+ssh://$svn_host/home/software/svnroot/$name -m "New project, $name."
+svn import $project_name svn+ssh://$svn_user_and_host/home/software/svnroot/$project_name -m "New project, $project_name."
 echo "Checking back out..."
-svn co svn+ssh://$svn_host/home/software/svnroot/$name $projects_dir/$name
-cd $projects_dir/$name
+svn co svn+ssh://$svn_user_and_host/home/software/svnroot/$project_name $projects_dir/$project_name
+cd $projects_dir/$project_name
 
 echo "Telling Subversion to ignore generated files..."
 svn propset svn:ignore "ChangeLog
 ChangeLog.html
 classes
 .generated
-$name.jar" .
+$project_name.jar" .
 echo "Getting Subversion to commit that change..."
 svn update
 echo "Prompting you to manually commit that change..."
