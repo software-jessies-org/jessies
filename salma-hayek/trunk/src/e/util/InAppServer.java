9519c57349fb065c4940708f275a8d66a5ae7bb2
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-11-08 05:05:40 +0000

    src/e/util/InAppServer.java: I'd been meaning to do something about the ugly command-parsing for ages, and Ed's finally given me the necessary push by implementing something better in his debugger. This is a generalization of that code.

--- salma-hayek/trunk/src/e/util/InAppServer.java
+++ salma-hayek/trunk/src/e/util/InAppServer.java
@@ -1,29 +1,86 @@
 package e.util;
 
 import java.io.*;
+import java.lang.reflect.*;
 import java.net.*;
+import java.util.*;
 
 /**
  * If your application wants to provide a simple server that users (or scripts)
- * can connect to and make requests, you might find this handy. Simply override
- * handleCommand to do some work, and instantiate! Edit uses this so you can
- * open files from the shell, and Terminator uses it so that successive
- * invocations don't need to start a new VM.
+ * can connect to and make requests, you might find this handy. Simply supply
+ * an interface listing available commands, and provide an object that
+ * implements that interface to do the work, and you're off!
+ * 
+ * Edit uses this so you can open files from the shell, and Terminator uses it
+ * so that successive invocations don't need to start a new VM.
  */
-public abstract class InAppServer {
-    /**
-     * Override this and return true if you were able to interpret the
-     * command, false otherwise.
-     */
-    public abstract boolean handleCommand(String line, PrintWriter out);
+public final class InAppServer {
+    private String fullName;
+    private Class exportedInterface;
+    private Object handler;
     
-    public InAppServer(String name, String portFilename) {
-        String fullName = name + "Server";
+    public InAppServer(String name, String portFilename, Class exportedInterface, Object handler) {
+        this.fullName = name + "Server";
+        this.exportedInterface = exportedInterface;
+        // FIXME: better type-safety?
+        this.handler = handler;
+        
         try {
             new Thread(new ConnectionAccepter(FileUtilities.fileFromString(portFilename)), fullName).start();
         } catch (Throwable th) {
             Log.warn("Couldn't start " + fullName, th);
         }
+    }
+    
+    public boolean handleCommand(String line, PrintWriter out) {
+        System.err.println(line);
+        String[] split = line.split("[\t ]");
+        String commandName = split[0];
+        
+        try {
+            Method[] methods = exportedInterface.getMethods();
+            for (Method method : methods) {
+                if (method.getName().equals(commandName)) {
+                    return invokeMethod(line, out, method, split);
+                }
+            }
+            throw new NoSuchMethodException();
+        } catch (NoSuchMethodException nsmex) {
+            out.println(fullName + ": didn't understand request \"" + line + "\".");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            String s = fullName + ": request denied \"" + line + "\" (" + ex.toString() + ").";
+            out.println(s);
+        } finally {
+            out.flush();
+            out.close();
+        }
+        return false;
+    }
+    
+    private boolean invokeMethod(String line, PrintWriter out, Method method, String[] fields) throws IllegalAccessException, InvocationTargetException {
+        ArrayList<Object> methodArguments = new ArrayList<Object>();
+        
+        Class[] parameterTypes = method.getParameterTypes();
+        if (parameterTypes.length == 2 && parameterTypes[0] == PrintWriter.class && parameterTypes[1] == String.class) {
+            // FIXME: there must be a better way to say "I want to parse the line myself."
+            methodArguments.add(out);
+            methodArguments.add(line);
+        } else {
+            int nextField = 1;
+            for (Class parameterType : parameterTypes) {
+                if (parameterType == PrintWriter.class) {
+                    methodArguments.add(out);
+                } else if (parameterType == String.class) {
+                    methodArguments.add(fields[nextField++]);
+                }
+                // FIXME: support other common types. "int" seems a likely first candidate.
+            }
+        }
+        
+        Object result = method.invoke(handler, methodArguments.toArray());
+        out.println((result == null) ? "OK" : result.toString());
+        return true;
     }
     
     private class ConnectionAccepter implements Runnable {
