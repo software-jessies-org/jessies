b05c25c9541a253d70a8a944d208bc16929eda5d
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-11-27 22:27:32 +0000

    bin/nightly-build.rb: put my home nightly-build script in the repository, rather than have slightly different variants on each of my machines.

---
+++ salma-hayek/trunk/bin/nightly-build.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby -w
+
+# ----------------------------------------------------------------------------
+# Find Subversion projects.
+# ----------------------------------------------------------------------------
+svn_projects = []
+Dir.glob("#{ENV['HOME']}/Projects/*/.svn").each() {
+  |svn_directory|
+  svn_directory =~ /^(.*\/)\.svn$/
+  svn_projects << $1
+}
+svn_projects.uniq!()
+
+# ----------------------------------------------------------------------------
+# Of the jessies.org projects, salma-hayek must come first. Performance anxiety.
+# ----------------------------------------------------------------------------
+salma_hayek = svn_projects.find() {
+  |item|
+  item.include?("/salma-hayek/")
+}
+svn_projects.delete(salma_hayek)
+svn_projects.insert(0, salma_hayek)
+
+# ----------------------------------------------------------------------------
+# Update and build the Subversion projects.
+# ----------------------------------------------------------------------------
+svn_projects.each() {
+  |svn_project|
+  svn_project =~ /.*\/([^\/]+)\/$/
+  project_name = $1
+  print("-- Updating and Building \"#{project_name}\"\n")
+  system("source ~/.bashrc ; cd #{svn_project} ; svn update && make")
+}
