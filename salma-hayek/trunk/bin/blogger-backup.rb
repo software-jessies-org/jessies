f71769d9b4daa8ddfffadb684a0536ada2ef2907
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-12-06 07:21:03 +0000

    bin/blogger-backup.rb: reduce the amount of uninteresting output, and show more information about what we actually did.

--- salma-hayek/trunk/bin/blogger-backup.rb
+++ salma-hayek/trunk/bin/blogger-backup.rb
@@ -9,13 +9,14 @@ blog_name=ARGV[0]
 blog_uri="http://#{blog_name}.blogspot.com/"
 
 print("Downloading #{blog_uri}...\n")
-blog_content=`curl #{blog_uri}`
+blog_content=`curl --silent --show-error #{blog_uri}`
 
 blog_content.split("\n").each() {
   |line|
   if line =~ /"(#{blog_uri}(\d+_\d+_\d+_\S+_archive\.html))"/
     next_archive_uri=$1
     next_filename=$2
-    system("curl -o #{next_filename} #{next_archive_uri}")
+    print("Downloading #{next_archive_uri}...\n")
+    system("curl --silent --show-error -o #{next_filename} #{next_archive_uri}")
   end
 }
