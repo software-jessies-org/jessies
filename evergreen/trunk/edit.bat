53bcf88bd0f2bae27b00d959dcef574c3dcb2f0e
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2005-12-19 01:56:00 +0000

    edit.bat: delete rather than pretend to maintain this batch file; if we really want to support Win32, we want a proper WiX installer.

--- evergreen/trunk/edit.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set EDIT_HOME=u:\Projects\edit
-java -Xmx1g -cp %EDIT_HOME%\classes;%EDIT_HOME%\..\salma-hayek\classes;%EDIT_HOME%\..\salma-hayek\MRJ141Stubs.jar -DpreferencesDirectory=%EDIT_HOME%\preferences e.edit.Edit %* > %EDIT_HOME%\edit.log
