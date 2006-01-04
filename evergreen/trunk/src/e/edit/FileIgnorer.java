fd2067a4dd50821d1467054ae5b441ef6b0d20d0
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-01-04 22:31:13 +0000

    bin/echo-local-non-source-directory-pattern: src/e/edit/FileIgnorer.java: only run the "echo-local-non-source-directory-pattern" script if it exists, and remove the broken default one from Edit's bin directory. It was causing me to miss the site-local script.

--- evergreen/trunk/src/e/edit/FileIgnorer.java
+++ evergreen/trunk/src/e/edit/FileIgnorer.java
@@ -6,48 +6,50 @@ import java.util.regex.*;
 import e.util.*;
 
 public class FileIgnorer {
-  /** Extensions of files that shouldn't be indexed. */
-  private String[] ignoredExtensions;
-  
-  /** Names of directories that shouldn't be entered when indexing. */
-  private Pattern uninterestingDirectoryNames;
-  
-  public FileIgnorer(String rootDirectoryPath) {
-    File rootDirectory = FileUtilities.fileFromString(rootDirectoryPath);
-    ignoredExtensions = FileUtilities.getArrayOfPathElements(Parameters.getParameter("files.uninterestingExtensions", ""));
-    uninterestingDirectoryNames = Pattern.compile(getUninterestingDirectoryPattern(rootDirectory));
-  }
-  
-  public boolean isIgnored(File file) {
-    if (file.isHidden() || file.getName().startsWith(".") || file.getName().endsWith("~")) {
-      return true;
-    }
-    if (file.isDirectory()) {
-      return isIgnoredDirectory(file);
-    }
-    return FileUtilities.nameEndsWithOneOf(file, ignoredExtensions);
-  }
-  
-  private static String getUninterestingDirectoryPattern(File rootDirectory) {
-    // Add the default ignored directory patterns.
-    ArrayList<String> patterns = new ArrayList<String>();
-    patterns.add("\\.deps");
-    patterns.add("\\.svn");
-    patterns.add("BitKeeper");
-    patterns.add("CVS");
-    patterns.add("SCCS");
+    /** Extensions of files that shouldn't be indexed. */
+    private String[] ignoredExtensions;
     
-    // This is not a per-workspace script, it's a site-local script.
-    final String scriptName = "echo-local-non-source-directory-pattern";
-    String[] command = new String[] { scriptName };
-    ArrayList<String> errors = new ArrayList<String>();
-    ProcessUtilities.backQuote(rootDirectory, command, patterns, errors);
+    /** Names of directories that shouldn't be entered when indexing. */
+    private Pattern uninterestingDirectoryNames;
     
-    // Make a regular expression.
-    return StringUtilities.join(patterns, "|");
-  }
-  
-  public boolean isIgnoredDirectory(File directory) {
-    return uninterestingDirectoryNames.matcher(directory.getName()).matches();
-  }
+    public FileIgnorer(String rootDirectoryPath) {
+        File rootDirectory = FileUtilities.fileFromString(rootDirectoryPath);
+        ignoredExtensions = FileUtilities.getArrayOfPathElements(Parameters.getParameter("files.uninterestingExtensions", ""));
+        uninterestingDirectoryNames = Pattern.compile(getUninterestingDirectoryPattern(rootDirectory));
+    }
+    
+    public boolean isIgnored(File file) {
+        if (file.isHidden() || file.getName().startsWith(".") || file.getName().endsWith("~")) {
+            return true;
+        }
+        if (file.isDirectory()) {
+            return isIgnoredDirectory(file);
+        }
+        return FileUtilities.nameEndsWithOneOf(file, ignoredExtensions);
+    }
+    
+    private static String getUninterestingDirectoryPattern(File rootDirectory) {
+        // Add the default ignored directory patterns.
+        ArrayList<String> patterns = new ArrayList<String>();
+        patterns.add("\\.deps");
+        patterns.add("\\.svn");
+        patterns.add("BitKeeper");
+        patterns.add("CVS");
+        patterns.add("SCCS");
+        
+        // Run the site-local script if we find it.
+        final String scriptName = "echo-local-non-source-directory-pattern";
+        if (FileUtilities.findOnPath(scriptName) != null) {
+            String[] command = new String[] { scriptName };
+            ArrayList<String> errors = new ArrayList<String>();
+            ProcessUtilities.backQuote(rootDirectory, command, patterns, errors);
+        }
+        
+        // Make a regular expression.
+        return StringUtilities.join(patterns, "|");
+    }
+    
+    public boolean isIgnoredDirectory(File directory) {
+        return uninterestingDirectoryNames.matcher(directory.getName()).matches();
+    }
 }
