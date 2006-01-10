a3281bb1e1a6cea417b14dd4ea503d773687e801
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-01-10 21:36:22 +0000

    bin/terminator: src/terminator/TerminatorServer.java: the previous revision was only secure through obscurity. If you knew to send "newTerminal " followed by NUL-separated arguments, you could still run anything you liked. This revision removes that functionality because it's not what this feature was intended to provide and is insecure.

--- terminator/trunk/src/terminator/TerminatorServer.java
+++ terminator/trunk/src/terminator/TerminatorServer.java
@@ -2,12 +2,17 @@ package terminator;
 
 import java.io.*;
 
+/**
+ * Improves the performance of opening a new shell from the command line or
+ * from applications such as the GNOME panel. This should not be extended to
+ * execute arbitrary commands without going to appropriate lengths to secure
+ * and authenticate the communication.
+ */
 public class TerminatorServer {
-    public void newTerminal(PrintWriter out, String line) {
+    public void newShell(PrintWriter out, String line) {
         try {
-            String tail = line.substring("newTerminal ".length());
-            String[] arguments = (tail.length() > 0) ? tail.split("\u0000") : new String[0];
-            Terminator.getSharedInstance().parseCommandLine(arguments, out, out);
+            // We don't accept any arguments over the network, because that could easily be a security hole.
+            Terminator.getSharedInstance().parseCommandLine(new String[0], out, out);
         } catch (Exception ex) {
             ex.printStackTrace(out);
         }
