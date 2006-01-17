91b0f3bba00eb277ae79c9a512f066888ade199d
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-01-17 00:54:47 +0000

    src/terminator/InfoDialog.java: chrisa couldn't tell that the title field was editable, and it's easy to see why when the uneditable fields were almost indistinguishable on every platform but Win32.

--- terminator/trunk/src/terminator/InfoDialog.java
+++ terminator/trunk/src/terminator/InfoDialog.java
@@ -12,27 +12,31 @@ public class InfoDialog {
     
     private ETextField title;
     private ETextField dimensions;
-    private ETextField logFilename;
     private ETextField processes;
+    private ETextField logFilename;
     private JCheckBox suspendLogging;
     private JTerminalPane terminal;
     
     private InfoDialog() {
         this.title = new TitleField();
         this.dimensions = new UneditableField();
-        this.logFilename = new UneditableField();
         this.processes = new UneditableField();
+        this.logFilename = new UneditableField();
         this.suspendLogging = makeSuspendLoggingCheckBox();
     }
     
     private static class UneditableField extends ETextField {
         public UneditableField() {
+            // Text fields with setEditable(false) don't look very different on any platform but Win32.
+            // Win32 is the only platform that clearly distinguishes between all the combinations of editable and enabled.
+            // It's sadly unclear that those responsible for the other platforms even understand the distinction.
+            // Although Cocoa makes a overly-subtle visual distinction, Apple's Java doesn't reproduce it.
+            // As a work-around, we use a trick various Mac OS programs use: make the uneditable text fields look like labels.
+            // You lose the visual clue that you can select and copy the text, but that's less important than obscuring the visual clue on editable fields that they're editable.
+            // FIXME: a text area would retain the selection behavior but add wrapping.
+            setBorder(null);
+            setOpaque(false);
             setEditable(false);
-            addMouseListener(new MouseAdapter() {
-                public void mouseClicked(MouseEvent e) {
-                    selectAll();
-                }
-            });
         }
     }
     
@@ -77,9 +81,9 @@ public class InfoDialog {
         FormPanel formPanel = form.getFormPanel();
         formPanel.addRow("Title:", title);
         formPanel.addRow("Dimensions:", dimensions);
+        formPanel.addRow("Processes:", processes);
         formPanel.addRow("Log Filename:", logFilename);
         formPanel.addRow("", suspendLogging);
-        formPanel.addRow("Processes:", processes);
         form.getFormDialog().setRememberBounds(false);
         form.showNonModal();
     }
