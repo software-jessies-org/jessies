f3e40fd4a39ac6354fce879e856a0885c3fad735
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-01-17 03:29:48 +0000

    src/terminator/terminal/PtyProcess.java: on operating systems other than Mac OS, fall back to calling lsof(1) in the absence of the BSD sysctl we use. Also make a couple of unnecessarily-public native methods private.
    TODO: part of a job done, and an interesting note.

--- terminator/trunk/src/terminator/terminal/PtyProcess.java
+++ terminator/trunk/src/terminator/terminal/PtyProcess.java
@@ -4,6 +4,7 @@ import e.gui.*;
 import e.util.*;
 import java.awt.Dimension;
 import java.io.*;
+import java.util.*;
 import java.util.concurrent.*;
 
 public class PtyProcess {
@@ -157,6 +158,15 @@ public class PtyProcess {
     }
     
     public String listProcessesUsingTty() {
+        if (GuiUtilities.isMacOs()) {
+            return listProcessesUsingTtyOnMacOs();
+        } else if (FileUtilities.findOnPath("lsof") != null) {
+            return listProcessesUsingTtyWithLsof();
+        }
+        return "";
+    }
+    
+    private String listProcessesUsingTtyOnMacOs() {
         try {
             return nativeListProcessesUsingTty();
         } catch (IOException ex) {
@@ -164,12 +174,45 @@ public class PtyProcess {
         }
     }
     
+    private String listProcessesUsingTtyWithLsof() {
+        // Linux doesn't support the sysctl(3) parameters we use on Mac OS.
+        // lsof(1) takes 350ms on my Linux box right now, which is poor but just about acceptable (and 10x faster than lsof(1) on Mac OS).
+        // This Java post-processing takes effectively no time, so there isn't obviously anything we can do.
+        // Something like this is 10x faster still, but won't work for setuid processes:
+        // ls -l /proc/*/fd/2 | perl -ne 'm|/proc/(\d+)/fd/2 -> /dev/pts/27| && system "ps --no-heading -o ucmd $1"'
+        // If we used something like that, but extracted the process name from /proc/<pid>/stat, we would work on Cygwin too.
+        // There's no obvious C interface we could from our JNI.
+        
+        // So, for now, call lsof(1).
+        String[] command = new String[] { "lsof", "-w", "-Fc", slavePtyName };
+        ArrayList<String> lines = new ArrayList<String>();
+        ArrayList<String> errors = new ArrayList<String>();
+        int status = ProcessUtilities.backQuote(null, command, lines, errors);
+        if (status != 0 || errors.isEmpty() == false || (lines.size() % 2) != 0) {
+            return "";
+        }
+        
+        // And post-process its output, which looks like this:
+        // $ lsof -w -Fc /dev/pts/27
+        // p11974
+        // cbash
+        // p11991
+        // cvim
+        ArrayList<String> processes = new ArrayList<String>();
+        for (int i = 0; i < lines.size(); i += 2) {
+            String pidLine = lines.get(i);
+            String nameLine = lines.get(i + 1);
+            processes.add(nameLine.substring(1) + "(" + pidLine.substring(1) + ")");
+        }
+        return StringUtilities.join(processes, ", ");
+    }
+    
     private native void nativeStartProcess(String[] command, FileDescriptor descriptor) throws IOException;
     private native void nativeWaitFor() throws IOException;
     public native void destroy() throws IOException;
     
-    public native int nativeRead(byte[] destination, int arrayOffset, int desiredLength) throws IOException;
-    public native void nativeWrite(int source) throws IOException;
+    private native int nativeRead(byte[] destination, int arrayOffset, int desiredLength) throws IOException;
+    private native void nativeWrite(int source) throws IOException;
     
     public native void sendResizeNotification(Dimension sizeInChars, Dimension sizeInPixels) throws IOException;
     
