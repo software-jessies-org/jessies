8eaeebb9654b4150315e5698c66c611f858e9976
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-01-27 06:29:16 +0000

    bin/file-list-to-wxi.rb: Do the foreseen extra Ruby twiddling to output one component per directory.  This:
    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/msi/setup/defining_installer_components.asp
    
    ... says "try to keep the total number of components low to improve performance" and other sources suggest this is the more conventional structure.  The real reason is that we may need to retain a GUID per component across builds and directories are longer-lived than files, which would mean less annoyingly pointless repository churn if we have to check them in.  I'm betting we can just invent them afresh each time as long as we don't mind each upgrade re-installing files which haven't changed (which, of course, we couldn't give a monkey's about - "computer, move some bits about").  But I'd rather not tempt us to make some risky hack if the right answer does turn out to be making them persistent, so I wanted to make that a less unpleasant option first.

--- salma-hayek/trunk/bin/file-list-to-wxi.rb
+++ salma-hayek/trunk/bin/file-list-to-wxi.rb
@@ -4,23 +4,38 @@ if ARGV[0] == "--diskId"
 else
   diskIdTag = ""
 end
-puts("<Include>")
-directoryNumber = 0
-fileNumber = 0
+directoryPathToFileNames = Hash.new() {
+  |hash, directoryPath|
+  hash[directoryPath] = []
+}
 $stdin.each() {
   |filePath|
   filePath.chomp!()
   pathComponents = filePath.split("/")
   fileName = pathComponents.pop()
+  directoryPath = pathComponents.join("/")
+  directoryPathToFileNames[directoryPath].push(fileName)
+}
+puts("<Include>")
+directoryNumber = 0
+fileNumber = 0
+directoryPathToFileNames.each_pair() {
+  |directoryPath, fileNames|
+  pathComponents = directoryPath.split("/")
   pathComponents.each() {
     |directoryName|
     puts("<Directory Id='directory#{directoryNumber}' Name='name#{directoryNumber}' LongName='#{directoryName}'>")
     directoryNumber += 1
   }
-  puts("<Component Id='component#{fileNumber}'>")
-  puts("<File Id='file#{fileNumber}' Name='name#{fileNumber}' LongName='#{fileName}' #{diskIdTag} src='#{filePath}' />")
+  puts("<Component Id='component#{directoryNumber}'>")
+  fileNames.each() {
+    |fileName|
+    filePathComponents = pathComponents + [fileName]
+    filePath = filePathComponents.join("/")
+    puts("<File Id='file#{fileNumber}' Name='name#{fileNumber}' LongName='#{fileName}' #{diskIdTag} src='#{filePath}' />")
+    fileNumber += 1
+  }
   puts("</Component>")
-  fileNumber += 1
   pathComponents.reverse().each() {
     |directoryName|
     puts("</Directory>")
