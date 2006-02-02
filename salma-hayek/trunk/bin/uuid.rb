12f7e7e8aab0dc829ed36c523422e8eeb97d43b0
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-02-02 02:00:16 +0000

    bin/uuid.rb: this needs to be called from Ruby, not make, so it's better if it can be used as a library.

--- salma-hayek/trunk/bin/uuid.rb
+++ salma-hayek/trunk/bin/uuid.rb
@@ -1,26 +1,32 @@
 #!/usr/bin/ruby -w
 
-require "Win32API"
-
-# Contrary to what the MS documentation says, UuidCreateSequential fills
-# a buffer you give it.
-uuid = "\0" * 16
-uuid_create = Win32API.new("rpcrt4.dll", "UuidCreateSequential", ['p'], 'i')
-rc = uuid_create.Call(uuid)
-if rc != 0
-  $stderr.puts("UuidCreateSequential returned #{rc}.")
-  exit(1)
+def uuid()
+  require "Win32API"
+  
+  # Contrary to what the MS documentation says, UuidCreateSequential fills
+  # a buffer you give it.
+  uuid = "\0" * 16
+  uuid_create = Win32API.new("rpcrt4.dll", "UuidCreateSequential", ['p'], 'i')
+  rc = uuid_create.Call(uuid)
+  if rc != 0
+    raise "UuidCreateSequential returned #{rc}."
+  end
+  
+  # Output the UUID in the RFC4122 format.
+  result = ""
+  i = 0
+  uuid.each_byte() {
+    |byte|
+    result << (sprintf("%02x", byte))
+    i += 1
+    if [4, 6, 8, 10].include?(i)
+      result << "-"
+    end
+  }
+  return result
 end
 
-# Output the UUID in the RFC4122 format.
-i = 0
-uuid.each_byte() {
-  |byte|
-  printf("%02x", byte)
-  i += 1
-  if [4, 6, 8, 10].include?(i)
-    print("-")
-  end
-}
-print("\n")
-exit(0)
+if __FILE__ == $0
+  puts(uuid())
+  exit(0)
+end
