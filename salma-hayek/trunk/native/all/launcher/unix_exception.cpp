292d405cdbf29e7f4a712c7102981564938950a8
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-02-08 03:54:57 +0000

    native/all/launcher/unix_exception.cpp: native/Headers/unix_exception.h: bring unix_exception back inline, and turn it into Terminator's superior implementation (including the translation of errno into a string). Also record the value of errno at the time we're constructed, and make it available for future examination.

--- salma-hayek/trunk/native/all/launcher/unix_exception.cpp
+++
@@ -1,7 +0,0 @@
-#include "unix_exception.h"
-
-#include "toString.h"
-
-unix_exception::unix_exception(const std::string& message)
-: std::runtime_error(message + (errno ? ": (" + toString(errno) + ")" : "")) {
-}
