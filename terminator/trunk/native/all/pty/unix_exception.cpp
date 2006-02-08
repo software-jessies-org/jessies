be2fe855856dfcee1b00d4c04ecdcee39785befa
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-02-08 03:57:31 +0000

    native/all/pty/errnoToString.cpp: native/all/pty/errnoToString.h: native/all/pty/unix_exception.cpp: lose our (superior) implementation of unix_exception (and the associated errnoToString) to salma-hayek.

--- terminator/trunk/native/all/pty/unix_exception.cpp
+++
@@ -1,7 +0,0 @@
-#include "unix_exception.h"
-
-#include "errnoToString.h"
-
-unix_exception::unix_exception(const std::string& message)
-: std::runtime_error(message + (errno ? ": (" + errnoToString() + ")" : "")) {
-}
