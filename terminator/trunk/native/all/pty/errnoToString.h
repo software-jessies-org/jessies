7d3e98ecd9e21aed6579318a8a218a9c85cda773
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-02-08 03:57:31 +0000

    native/all/pty/errnoToString.cpp: native/all/pty/errnoToString.h: native/all/pty/unix_exception.cpp: lose our (superior) implementation of unix_exception (and the associated errnoToString) to salma-hayek.

--- terminator/trunk/native/all/pty/errnoToString.h
+++
@@ -1,9 +0,0 @@
-#ifndef ERRNO_TO_STRING_H_included
-#define ERRNO_TO_STRING_H_included
-
-#include <string>
-
-/** Converts the current value of 'errno' to a string. */
-std::string errnoToString();
-
-#endif
