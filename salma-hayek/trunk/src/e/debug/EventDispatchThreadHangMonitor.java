7e25d611ce07bd7105fd2af7389aac47655e48bc
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-03-20 03:41:20 +0000

    src/e/debug/EventDispatchThreadHangMonitor.java: a major rewrite in light of problems brought up by Alexander Potochkin. In particular, we need to cope with nested invocations of dispatchEvent (he provided two examples of how this can happen, only one of which involved modal dialogs). We also need to cope with multiple EDTs (as mentioned in the last revision), and nested dispatch makes it unwise to elide parts of the stack trace in as simplistic a fashion as we were doing.

--- salma-hayek/trunk/src/e/debug/EventDispatchThreadHangMonitor.java
+++ salma-hayek/trunk/src/e/debug/EventDispatchThreadHangMonitor.java
@@ -6,6 +6,9 @@ import java.awt.event.*;
 import java.io.*;
 import java.lang.management.*;
 import java.util.*;
+import java.util.Timer;
+
+import javax.swing.*;
 
 /**
  * Monitors the AWT event dispatch thread for events that take longer than
@@ -34,25 +37,88 @@ public final class EventDispatchThreadHangMonitor extends EventQueue {
     // called on the event dispatch thread, like font loading).
     private static final long UNREASONABLE_DISPATCH_DURATION_MS = 1000;
     
-    // Used as the value of startedLastEventDispatchAt when we're not in
-    // the middle of event dispatch.
-    private static final long NO_CURRENT_EVENT = 0;
-    
-    // When we started dispatching the current event, in milliseconds.
-    private long startedLastEventDispatchAt = NO_CURRENT_EVENT;
-    
-    // Have we already dumped a stack trace for the current event dispatch?
-    private boolean reportedHang = false;
+    // Help distinguish multiple hangs in the log, and match start and end too.
+    // Only access this via getNewHangNumber.
+    private static int hangCount = 0;
     
     // Prevents us complaining about hangs during start-up, which are probably
     // the JVM vendor's fault.
     private boolean haveShownSomeComponent = false;
     
-    // Help distinguish multiple hangs in the log, and match start and end too.
-    private int hangCount = 0;
+    // The currently outstanding event dispatches. The implementation of
+    // modal dialogs is a common cause for multiple outstanding dispatches.
+    private LinkedList<DispatchInfo> dispatches = new LinkedList<DispatchInfo>();
     
-    // The event dispatch thread, for the purpose of getting stack traces.
-    private Thread eventDispatchThread = null;
+    private static class DispatchInfo {
+        // The last-dumped hung stack trace for this dispatch.
+        private StackTraceElement[] lastReportedStack;
+        // If so; what was the identifying hang number?
+        private int hangNumber;
+        
+        // The EDT for this dispatch (for the purpose of getting stack traces).
+        // I don't know of any API for getting the event dispatch thread,
+        // but we can assume that it's the current thread if we're in the
+        // middle of dispatching an AWT event...
+        // Alexander Potochkin points out that we can't cache this because
+        // the EDT can die and be replaced by a new EDT if there's an uncaught
+        // exception.        
+        private final Thread eventDispatchThread = Thread.currentThread();
+        
+        // The time in milliseconds at which we noted this dispatch.
+        private final long startTimeMillis = System.currentTimeMillis();
+        
+        public DispatchInfo() {
+            // All initialization is done by the field initializers.
+        }
+        
+        public void checkForHang() {
+            if (timeSoFar() > UNREASONABLE_DISPATCH_DURATION_MS) {
+                reportHang();
+            }
+        }
+        
+        private void reportHang() {
+            StackTraceElement[] currentStack = eventDispatchThread.getStackTrace();
+            if (stacksEqual(lastReportedStack, currentStack)) {
+                // Don't keep reporting the same hang every time the timer goes off.
+                return;
+            }
+            
+            hangNumber = getNewHangNumber();
+            String stackTrace = stackTraceToString(currentStack);
+            lastReportedStack = currentStack;
+            Log.warn("(hang #" + hangNumber + ") event dispatch thread stuck processing event for " +  timeSoFar() + " ms:" + stackTrace);
+            checkForDeadlock();
+        }
+        
+        private static boolean stacksEqual(StackTraceElement[] a, StackTraceElement[] b) {
+            if (a == null) {
+                return false;
+            }
+            if (a.length != b.length) {
+                return false;
+            }
+            for (int i = 0; i < a.length; ++i) {
+                if (a[i].equals(b[i]) == false) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        /**
+         * Returns how long this dispatch has been going on (in milliseconds).
+         */
+        private long timeSoFar() {
+            return (System.currentTimeMillis() - startTimeMillis);
+        }
+        
+        public void dispose() {
+            if (lastReportedStack != null) {
+                Log.warn("(hang #" + hangNumber + ") event dispatch thread unstuck after " + timeSoFar() + " ms.");
+            }
+        }
+    }
     
     private EventDispatchThreadHangMonitor() {
         initTimer();
@@ -71,73 +137,19 @@ public final class EventDispatchThreadHangMonitor extends EventQueue {
     private class HangChecker extends TimerTask {
         @Override
         public void run() {
-            // Synchronize on the outer class, because that's where all
-            // the state lives.
-            synchronized (INSTANCE) {
-                checkForHang();
-            }
-        }
-        
-        private void checkForHang() {
-            if (startedLastEventDispatchAt == NO_CURRENT_EVENT) {
-                // We don't destroy the timer when there's nothing happening
-                // because it would mean a lot more work on every single AWT
-                // event that gets dispatched.
-                return;
-            }
-            if (haveShownSomeComponent && timeSoFar() > UNREASONABLE_DISPATCH_DURATION_MS) {
-                reportHang();
-            }
-        }
-        
-        private void reportHang() {
-            if (reportedHang) {
-                // Don't keep reporting the same hang every 100 ms.
-                return;
-            }
-            
-            reportedHang = true;
-            String stackTrace = stackTraceToString(eventDispatchThread.getStackTrace());
-            Log.warn("(hang #" + ++hangCount + ") event dispatch thread stuck processing event for " +  timeSoFar() + " ms:" + stackTrace);
-            checkForDeadlock();
-        }
-        
-        private void checkForDeadlock() {
-            ThreadMXBean threadBean = ManagementFactory.getThreadMXBean();
-            long[] threadIds = threadBean.findMonitorDeadlockedThreads();
-            if (threadIds == null) {
-                return;
-            }
-            Log.warn("deadlock detected involving the following threads:");
-            ThreadInfo[] threadInfos = threadBean.getThreadInfo(threadIds, Integer.MAX_VALUE);
-            for (ThreadInfo info : threadInfos) {
-                Log.warn("Thread #" + info.getThreadId() + " " + info.getThreadName() + " (" + info.getThreadState() + ") waiting on " + info.getLockName() + " held by " + info.getLockOwnerName() + stackTraceToString(info.getStackTrace()));
-            }
-        }
-        
-        private String stackTraceToString(StackTraceElement[] stackTrace) {
-            StringBuilder result = new StringBuilder();
-            // We know that it's not interesting to show any code above where
-            // we get involved in event dispatch, so we stop printing the stack
-            // trace when we get as far back as our code.
-            final String ourEventQueueClassName = EventDispatchThreadHangMonitor.class.getName();
-            for (StackTraceElement stackTraceElement : stackTrace) {
-                if (stackTraceElement.getClassName().equals(ourEventQueueClassName)) {
-                    break;
+            synchronized (dispatches) {
+                if (dispatches.isEmpty() || haveShownSomeComponent == false) {
+                    // Nothing to do.
+                    // We don't destroy the timer when there's nothing happening
+                    // because it would mean a lot more work on every single AWT
+                    // event that gets dispatched.
+                    return;
                 }
-                result.append("\n    " + stackTraceElement);
+                for (DispatchInfo dispatch : dispatches) {
+                    dispatch.checkForHang();
+                }
             }
-            return result.toString();
         }
-    }
-    
-    /**
-     * Returns how long we've been processing the current event (in
-     * milliseconds).
-     */
-    private long timeSoFar() {
-        long currentTime = System.currentTimeMillis();
-        return (currentTime - startedLastEventDispatchAt);
     }
     
     /**
@@ -161,29 +173,143 @@ public final class EventDispatchThreadHangMonitor extends EventQueue {
         }
     }
     
-    /**
-     * Stores the time at which we started processing the current event.
-     */
-    private synchronized void preDispatchEvent() {
-        // I don't know of any API for getting the event dispatch thread,
-        // but we can assume that it's the current thread if we're in the
-        // middle of dispatching an AWT event...
-        // Alexander Potochkin points out that we can't cache this because
-        // the EDT can die and be replaced by a new EDT if there's an uncaught
-        // exception.
-        eventDispatchThread = Thread.currentThread();
-        reportedHang = false;
-        startedLastEventDispatchAt = System.currentTimeMillis();
+    private void debug(String which) {
+        if (false) {
+            for (int i = dispatches.size(); i >= 0; --i) {
+                System.out.print(' ');
+            }
+            System.out.println(which);
+        }
     }
     
     /**
-     * Reports the end of any ongoing hang, and notes that we're no longer
-     * processing an event.
+     * Starts tracking a dispatch.
+     */
+    private synchronized void preDispatchEvent() {
+        debug("pre");
+        synchronized (dispatches) {
+            dispatches.addLast(new DispatchInfo());
+        }
+    }
+    
+    /**
+     * Stops tracking a dispatch.
      */
     private synchronized void postDispatchEvent() {
-        if (reportedHang) {
-            Log.warn("(hang #" + hangCount + ") event dispatch thread unstuck after " + timeSoFar() + " ms.");
+        synchronized (dispatches) {
+            DispatchInfo dispatchInfo = dispatches.removeLast();
+            dispatchInfo.dispose();
         }
-        startedLastEventDispatchAt = NO_CURRENT_EVENT;
+        debug("post");
+    }
+    
+    private static void checkForDeadlock() {
+        ThreadMXBean threadBean = ManagementFactory.getThreadMXBean();
+        long[] threadIds = threadBean.findMonitorDeadlockedThreads();
+        if (threadIds == null) {
+            return;
+        }
+        Log.warn("deadlock detected involving the following threads:");
+        ThreadInfo[] threadInfos = threadBean.getThreadInfo(threadIds, Integer.MAX_VALUE);
+        for (ThreadInfo info : threadInfos) {
+            Log.warn("Thread #" + info.getThreadId() + " " + info.getThreadName() + " (" + info.getThreadState() + ") waiting on " + info.getLockName() + " held by " + info.getLockOwnerName() + stackTraceToString(info.getStackTrace()));
+        }
+    }
+    
+    private static String stackTraceToString(StackTraceElement[] stackTrace) {
+        StringBuilder result = new StringBuilder();
+        // We used to avoid showing any code above where this class gets
+        // involved in event dispatch, but that hides potentially useful
+        // information when dealing with modal dialogs. Maybe we should
+        // reinstate that, but search from the other end of the stack?
+        for (StackTraceElement stackTraceElement : stackTrace) {
+            result.append("\n    " + stackTraceElement);
+        }
+        return result.toString();
+    }
+    
+    private synchronized static int getNewHangNumber() {
+        return ++hangCount;
+    }
+    
+    public static void main(String[] args) {
+        initMonitoring();
+        Tests.main(args);
+    }
+    
+    private static class Tests {
+        public static void main(final String[] args) {
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    for (String arg : args) {
+                        final JFrame frame = new JFrame();
+                        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        frame.setLocationRelativeTo(null);
+                        if (arg.equals("focus")) {
+                            runFocusTest(frame);
+                        } else if (arg.equals("modal")) {
+                            runModalTest(frame);
+                        } else {
+                            System.err.println("unknown regression test '" + arg + "'");
+                            System.exit(1);
+                        }
+                        frame.pack();
+                        frame.setVisible(true);
+                    }
+                }
+            });
+        }
+        
+        // Alexander Potochkin supplied this demonstration of nested calls to
+        // dispatchEvent caused by SequencedEvent.
+        private static void runFocusTest(final JFrame frame) {
+            final JDialog dialog = new JDialog(frame, "Non-Modal Dialog");
+            dialog.add(new JLabel("Close me!"));
+            dialog.pack();
+            dialog.setLocationRelativeTo(frame);
+            dialog.addWindowFocusListener(new WindowAdapter() {
+                public void windowGainedFocus(WindowEvent e) {
+                    System.out.println("FocusTest.windowGainedFocus");
+                    // If you don't cope with nested calls to dispatchEvent, you won't detect this.
+                    // See java.awt.SequencedEvent for an example.
+                    sleep(2500);
+                }
+            });
+            JButton button = new JButton("Show Non-Modal Dialog");
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    dialog.setVisible(true);
+                }
+            });
+            frame.add(button);
+        }
+        
+        // Alexander Potochkin supplied this demonstration of the problems
+        // of dealing with modal dialogs.
+        private static void runModalTest(final JFrame frame) {
+            JButton button = new JButton("Show Modal Dialog");
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    sleep(2500); // This is easy.
+                    JDialog dialog = new JDialog(frame, "Modal dialog", true);
+                    dialog.add(new JLabel("Close me!"));
+                    dialog.pack();
+                    dialog.setLocation(frame.getX() - 100,  frame.getY());
+                    dialog.setVisible(true);
+                    sleep(2500); // If you don't distinguish different stack traces, you won't report this.
+                }
+            });
+            frame.add(button);
+        }
+        
+        private static void sleep(long ms) {
+            try {
+                System.out.println("Sleeping for " + ms + " ms on " + Thread.currentThread() + "...");
+                Thread.sleep(ms);
+                System.out.println("Finished sleeping...");
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
