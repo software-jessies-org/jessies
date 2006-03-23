bcffe47cc5af452e6c89faba71ebd6bcc65c6c26
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-03-23 07:34:30 +0000

    src/e/ptextarea/PPythonTextStyler.java: simple Python coloring.

---
+++ salma-hayek/trunk/src/e/ptextarea/PPythonTextStyler.java
@@ -0,0 +1,66 @@
+package e.ptextarea;
+
+import java.util.*;
+
+public class PPythonTextStyler extends PAbstractLanguageStyler {
+    private static final String[] KEYWORDS = new String[] {
+        // python -c 'import keyword ; print keyword.kwlist'
+        "and",
+        "assert",
+        "break",
+        "class",
+        "continue",
+        "def",
+        "del",
+        "elif",
+        "else",
+        "except",
+        "exec",
+        "finally",
+        "for",
+        "from",
+        "global",
+        "if",
+        "import",
+        "in",
+        "is",
+        "lambda",
+        "not",
+        "or",
+        "pass",
+        "print",
+        "raise",
+        "return",
+        "try",
+        "while",
+        "yield",
+    };
+    
+    public PPythonTextStyler(PTextArea textArea) {
+        super(textArea);
+    }
+    
+    @Override
+    protected boolean supportShellComments() {
+        return true;
+    }
+    
+    @Override
+    protected boolean supportDoubleSlashComments() {
+        return false;
+    }
+    
+    @Override
+    protected boolean supportSlashStarComments() {
+        return false;
+    }
+    
+    @Override
+    protected boolean isQuote(char ch) {
+        return (ch == '\'' || ch == '\"' || ch == '`');
+    }
+    
+    public void addKeywordsTo(Collection<String> collection) {
+        collection.addAll(Arrays.asList(KEYWORDS));
+    }
+}
