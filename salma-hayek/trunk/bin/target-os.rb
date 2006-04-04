b120353121dbe9cf74674c8a50f4a001fc03abad
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-04-04 17:47:49 +0000

    bin/target-os.rb: cope with Ruby on CentOS, which reports its target OS as "Linux-gnu" rather than just "Linux". I wonder if we shouldn't just call the real uname(1) if we decide we're not on Cygwin?

--- salma-hayek/trunk/bin/target-os.rb
+++ salma-hayek/trunk/bin/target-os.rb
@@ -9,10 +9,14 @@ class OsExaminer
         require "rbconfig.rb"
         # Avoid calling the console-subsystem uname(1) program on Cygwin.
         @os_name = Config::CONFIG["target_os"]
-        # Remove the version number from darwin8.0.
-        @os_name.sub!(/[\d.]+/, "")
         # Conform to uname conventions.
         @os_name.capitalize!()
+        
+        # Remove version numbers. Our motivating example was "darwin8.0" on Mac OS.
+        @os_name.sub!(/[\d.]+/, "")
+        # Chris Starling reports that Ruby on CentOS says "Linux-gnu" rather than just "Linux" (which is what we see on other Linuxes).
+        @os_name.sub!("Linux-gnu", "Linux")
+        # Ruby [blastwave or built from source or both?] on Solaris says "Solaris" rather than "SunOS" (which is what uname(1) would say).
         @os_name.sub!("Solaris", "SunOS")
     end
     
