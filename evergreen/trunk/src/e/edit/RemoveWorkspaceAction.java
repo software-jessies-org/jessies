6a07a101d48ad86985a246cee841ce7af52ebad9
pcn <pcn@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-04-12 22:28:24 +0000

    src/e/edit/Edit.java: src/e/edit/RemoveWorkspaceAction.java: src/e/edit/RescanWorkspaceAction.java: If I right-click on one of the items in the tabbed pane, I expect the 'rescan' and 'remove workspace' actions to act on the tab I right-clicked on, not the 'current' workspace.  Therefore changed to work that way.

--- evergreen/trunk/src/e/edit/RemoveWorkspaceAction.java
+++ evergreen/trunk/src/e/edit/RemoveWorkspaceAction.java
@@ -5,12 +5,23 @@ import javax.swing.*;
 
 public class RemoveWorkspaceAction extends AbstractAction {
     public static final String ACTION_NAME = "Remove Workspace";
+    private int workspaceIndex = -1;
     
     public RemoveWorkspaceAction() {
         super(ACTION_NAME);
     }
     
+    public RemoveWorkspaceAction(int workspaceIndex) {
+        super(ACTION_NAME);
+        this.workspaceIndex = workspaceIndex;
+    }
+    
     public void actionPerformed(ActionEvent e) {
-        Edit.getInstance().removeCurrentWorkspace();
+        Edit edit = Edit.getInstance();
+        if (workspaceIndex == -1) {
+            edit.removeCurrentWorkspace();
+        } else {
+            edit.removeWorkspace(workspaceIndex);
+        }
     }
 }
--- evergreen/trunk/src/e/edit/RescanWorkspaceAction.java
+++ evergreen/trunk/src/e/edit/RescanWorkspaceAction.java
@@ -5,12 +5,24 @@ import javax.swing.*;
 
 public class RescanWorkspaceAction extends AbstractAction {
     public static final String ACTION_NAME = "Rescan Files";
+    private int workspaceIndex = -1;
     
     public RescanWorkspaceAction() {
         super(ACTION_NAME);
     }
     
+    public RescanWorkspaceAction(int workspaceIndex) {
+        super(ACTION_NAME);
+        this.workspaceIndex = workspaceIndex;
+    }
+    
     public void actionPerformed(ActionEvent e) {
-        Edit.getInstance().getCurrentWorkspace().updateFileList(null);
+        System.err.println("Event source: " + e.getSource());
+        Edit edit = Edit.getInstance();
+        if (workspaceIndex == -1) {
+            edit.getCurrentWorkspace().updateFileList(null);
+        } else {
+            edit.getWorkspaces()[workspaceIndex].updateFileList(null);
+        }
     }
 }
