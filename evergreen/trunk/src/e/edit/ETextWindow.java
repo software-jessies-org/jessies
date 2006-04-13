da1da315fce0050eaa9ffb64c72d799977d764a8
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-04-13 01:06:28 +0000

    src/e/edit/ETextWindow.java: Open the newly Saved As file.  This will now cause a workspace rescan, fixing the Open Quickly index so it contains the new file.  When I was mulling over this in the morning, I was afraid that I'd start editing the wrong copy.  But I'd probably do that anyway.  As if I ever go up the screen as far as the File menu.

--- evergreen/trunk/src/e/edit/ETextWindow.java
+++ evergreen/trunk/src/e/edit/ETextWindow.java
@@ -813,6 +813,8 @@ public class ETextWindow extends EWindow implements PTextListener {
                 }
             }
             writeToFile(newFile);
+            Edit edit = Edit.getInstance();
+            edit.openFile(newFile.getAbsolutePath());
             return true;
         } catch (Exception ex) {
             Edit.getInstance().showAlert("Couldn't save file \"" + newFilename + "\"", ex.getMessage());
