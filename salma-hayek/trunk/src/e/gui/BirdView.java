b21b3c33d8bcf80fabd41caa619fd5c5517331fd
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-04-16 21:00:48 +0000

    src/e/gui/BirdView.java: it's worth commenting the seemingly strange preferred height.

--- salma-hayek/trunk/src/e/gui/BirdView.java
+++ salma-hayek/trunk/src/e/gui/BirdView.java
@@ -88,6 +88,8 @@ public class BirdView extends JComponent {
     }
 
     public Dimension getPreferredSize() {
+        // We care about our width, but want to take our height from our corresponding text component.
+        // This assumes we'll be layed out by the same layout manager, but it seems like that must always be the case anyway.
         return new Dimension(4, 0);
     }
     
