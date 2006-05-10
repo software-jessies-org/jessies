f75bc16af989b69c9317f09f8cfc4351dba7f5b8
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-05-10 06:31:33 +0000

    bin/nightly-build.rb: We should build distributed builds from clean.  Facilitate that by allowing "nightly-build.rb clean native-dist".

--- salma-hayek/trunk/bin/nightly-build.rb
+++ salma-hayek/trunk/bin/nightly-build.rb
@@ -1,21 +1,9 @@
 #!/usr/bin/ruby -w
 
 # ----------------------------------------------------------------------------
-# Initialize defaults.
-# ----------------------------------------------------------------------------
-target = ""
-
-# ----------------------------------------------------------------------------
 # Parse command line.
 # ----------------------------------------------------------------------------
-if ARGV.length() == 1
-  target = ARGV.shift()
-end
-
-if ARGV.length() != 0
-  $stderr.puts("usage: #$0 <target>")
-  exit(1)
-end
+targets = ARGV
 
 # ----------------------------------------------------------------------------
 # Find Subversion projects.
@@ -44,7 +32,7 @@ svn_projects.each() {
   svn_project =~ /.*\/([^\/]+)\/$/
   project_name = $1
   print("-- Updating and Building \"#{project_name}\"\n")
-  system("source ~/.bashrc ; cd #{svn_project} ; svn status ; svn diff ; svn update && make #{target}")
+  system("source ~/.bashrc ; cd #{svn_project} ; svn status ; svn diff ; svn update && make #{targets.join(" ")}")
   if $? != 0
     failed_builds << project_name
   end
