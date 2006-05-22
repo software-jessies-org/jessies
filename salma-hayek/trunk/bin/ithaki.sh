cfac6554345aad06ce5d7f652e02f80e451a0361
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-05-22 22:27:33 +0000

    bin/ithaki.sh: apt-ftparchive appears to be the replacement for dpkg-scanpackages.  It bleats less, which is good in my book.  It also will generate a Release file, which we need in order to sign, having generated a key with no passphrase with gpg --gen-key, in order that it's then possible to add the key to apt-key's trust database in order that, finally, the flaming thing will stop asking whether we really want to install an unauthenticated package.
    This is no use at all.  Previously, you only had to install the packages once, confirming the authentication bypass, in order that they'd then upgrade without prompting.  Now you have to do something obscure with apt-key, that no-one would guess and that's more difficult to explain than "say yes when it asks whether you want to install unauthenticated packages".
    
    Worse, if the user agreed to add our key to their trust database, they'd be agreeing that, any time they upgrade, we could replace any package on their system, simply by providing a version that seems to be newer.

--- salma-hayek/trunk/bin/ithaki.sh
+++ salma-hayek/trunk/bin/ithaki.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
-# Add this to /etc/apt/sources.list on every machine:
+
+# Add this to /etc/apt/sources.list:
+# deb http://software.jessies.org/debian/ ./
+# And do this if you want to avoid the authentication prompt on first installation:
+# wget -O - http://software.jessies.org/debian/software.jessies.org.gpg | sudo apt-key add -
+
+# BlueArc's local copy of the archive:
+DEB_DIRECTORY=/u/u154/software.jessies.org/
 # deb http://deb/software.jessies.org/ ./
+# sudo apt-key add - < $DEB_DIRECTORY/software.jessies.org.gpg
+#
 # "deb" is a DNS alias for a machine in the local domain running apache on which the following command has been run:
 # sudo ln -s $DEB_DIRECTORY /var/www/software.jessies.org
 
 # Run the latest version of the nightly build script, rather than the version from yesterday.
 # This has the advantage that the update won't overwrite the running script - which Ruby doesn't like.
-~/software.jessies.org/work/salma-hayek/bin/nightly-build.rb clean native-dist
-DEB_DIRECTORY=/u/u154/software.jessies.org/
+~martind/software.jessies.org/work/salma-hayek/bin/nightly-build.rb clean native-dist
 mkdir -p $DEB_DIRECTORY
 cd $DEB_DIRECTORY
-find ~/software.jessies.org/nightlies/ -name "*.deb" | xargs cp --target-directory=.
-dpkg-scanpackages . /dev/null | gzip -9 > Packages.gz
-scp Packages.gz software@jessies.org:~/downloads/debian/Packages.gz
+find ~martind/software.jessies.org/nightlies/ -name "*.deb" | xargs cp --target-directory=.
+# If we don't have a Packages file (as well as Packages.gz) we get:
+# Failed to fetch http://deb/software.jessies.org/./Release  Unable to find expected entry  Packages in Meta-index file (malformed Release file?)
+apt-ftparchive packages . > Packages
+gzip -9 < Packages > Packages.gz
+apt-ftparchive release . > Release
+# This has to run as martind@bluearc.com to sign with the right key.
+gpg -sba - - < Release > Release.gpg
+scp Packages Packages.gz Release Release.gpg software@jessies.org:~/downloads/debian/
