028e39170c5bfdab0dabe18360b958a5b6ec4928
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-05-22 06:03:35 +0000

    lib/data/man-summary.txt: remove the list of man pages present on one particular machine at one particular time...
    src/e/edit/ManPageResearcher.java: ...in favor of scanning the machine we're actually running on. This only takes 0.5s on my dual G5.

--- evergreen/trunk/src/e/edit/ManPageResearcher.java
+++ evergreen/trunk/src/e/edit/ManPageResearcher.java
@@ -1,13 +1,13 @@
 package e.edit;
 
+import java.io.*;
 import java.util.*;
+import java.util.regex.*;
 import e.ptextarea.*;
 import e.util.*;
 
 /**
-
-Offers man page entries corresponding to selected words.
-
+ * Offers man page entries corresponding to selected words.
  */
 public class ManPageResearcher implements WorkspaceResearcher {
     /** Keep the names of all the known man pages. */
@@ -33,37 +33,59 @@ public class ManPageResearcher implements WorkspaceResearcher {
      */
     private static void init() {
         final long startTime = System.currentTimeMillis();
-        knownManPages = new HashSet<String>();
         
-        /*
-         * Should really do something equivalent to this:
-         * 
-         * locate man/man | grep -v "^/Previous Systems/" | grep "/man[23]$" |  tr '\n' '\0' | xargs -0 find | grep -Ev "\.3(pm|ssl|tcl)$" | perl -ne 'if (m/\/([^\/.]*)\..*$/) { print "$1\n"; }' | sort -u
-         * 
-         * What to do on systems without locate(1), or where the database hasn't been generated?
-         * 
-         * For now, we'll assume that we have a file listing all the interesting man pages.
-         * This has the advantage that you can remove any entries that annoy you.
-         */
-
-        String filename = Evergreen.getInstance().getResourceFilename("man-summary.txt");
-        String[] manPages;
-        if (FileUtilities.exists(filename)) {
-            manPages = StringUtilities.readLinesFromFile(filename);
-        } else {
-            manPages = new String[0];
-        }
+        knownManPages = new HashSet<String>();
         uniqueIdentifiers = new TreeSet<String>();
-        for (String manPage : manPages) {
-            knownManPages.add(manPage);
-            uniqueIdentifiers.add(manPage);
+        
+        HashSet<String> sections = new HashSet<String>();
+        
+        Pattern manPagePattern = Pattern.compile("^(.*)\\.([23][a-z]*)(\\.gz)?$");
+        for (File manPath : findManPageDirectories()) {
+            String[] manPages = manPath.list();
+            if (manPages == null) manPages = new String[0];
+            for (String manPage : manPages) {
+                Matcher matcher = manPagePattern.matcher(manPage);
+                if (matcher.matches()) {
+                    String stub = matcher.group(1);
+                    String section = matcher.group(2);
+                    if (section.endsWith("pm") || section.endsWith("tcl") || section.endsWith("ssl")) {
+                        continue;
+                    }
+                    sections.add(section);
+                    // FIXME: keep track which section each page came from.
+                    knownManPages.add(stub);
+                    uniqueIdentifiers.add(stub);
+                } else {
+                    Log.warn("unexpected man page \"" + manPage + "\"");
+                }
+            }
         }
+        
+        System.out.println(sections);
         
         // FIXME: this turns "posix_openpt" into two words, so the spelling checker will accept "openpt" alone, rather than just in the identifier "posix_openpt" as intended. Maybe we should check blessed identifiers as a whole before we try break them into words, and then supply the spelling checker with the unique identifiers we bless, rather than just the list of words? (At the same time, passing all the words works well for Java source.)
         uniqueWords = JavaResearcher.extractUniqueWords(uniqueIdentifiers.iterator());
         
         final long duration = System.currentTimeMillis() - startTime;
         Log.warn("Learned of " + knownManPages.size() + " man pages in " + duration + "ms.");
+    }
+    
+    private static List<File> findManPageDirectories() {
+        ArrayList<File> manPaths = new ArrayList<File>();
+        Pattern manPathPattern = Pattern.compile("^(?:MANDATORY_)?MANPATH\\s+(.*)$");
+        String[] configurationPossibilities = new String[] { "/usr/share/misc/man.conf", "/etc/manpath.config" };
+        for (String configurationPossibility : configurationPossibilities) {
+            if (FileUtilities.exists(configurationPossibility)) {
+                for (String line : StringUtilities.readLinesFromFile(configurationPossibility)) {
+                    Matcher matcher = manPathPattern.matcher(line);
+                    if (matcher.matches()) {
+                        manPaths.add(new File(matcher.group(1), "man2"));
+                        manPaths.add(new File(matcher.group(1), "man3"));
+                    }
+                }
+            }
+        }
+        return manPaths;
     }
     
     public void addManPageWordsTo(Set<String> set) {
