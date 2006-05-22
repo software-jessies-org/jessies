ce6ffb0a0813815bc38e44646f5d622117e95aba
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-05-22 17:03:43 +0000

    bin/install-everything.sh: Stuart had a hard-coded /usr/local/bin path in his crontab.  I've changed it.  I cleaned up my duezer installation, so I might notice install-everything.sh disappearing from my path in future.

--- salma-hayek/trunk/bin/install-everything.sh
+++ salma-hayek/trunk/bin/install-everything.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Use this as the command in your crontab:
+# echo install-everything.sh | sudo bash --login
+
 # apt-get's output is routinely splattered with incomprehensible errors.
 apt-get update &> /dev/null
 
@@ -9,3 +12,12 @@ apt-get -y install org.jessies.{evergreen,scm,terminator}
 # FIXME: This belongs in terminator.deb.
 update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/terminator 50
 update-alternatives --auto x-terminal-emulator
+
+# Hard-wiring the existing path to the script in our users' crontabs would only lead to worse problems.
+ln -f -s /usr/share/software.jessies.org/terminator/Resources/salma-hayek/bin/install-everything.sh /usr/local/bin/install-everything.sh
+
+# This will clean up our old installations.
+# I expect we want to run it manually rather than every night from here.
+exit 0
+rm -rf /usr/local/www.jessies.org/
+find /usr/local -type l | xargs ls -l | perl -ne 'm@(/usr/local/.*) -> /usr/local/www.jessies.org/@ && print("$1\n")' | xargs rm
