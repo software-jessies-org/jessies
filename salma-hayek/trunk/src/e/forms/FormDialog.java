54a0a2da44485111de51b661e24ed183d278d0c3
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-06-18 23:47:01 +0000

    src/e/forms/FormDialog.java: remove debugging output.

--- salma-hayek/trunk/src/e/forms/FormDialog.java
+++ salma-hayek/trunk/src/e/forms/FormDialog.java
@@ -356,7 +356,6 @@ public class FormDialog {
         dialog.dispose();
         
         // We want to restore focus to whichever component had it before we were shown, but we mustn't pass on focus until later because we may be here in response to a KeyEvent (such as the user hitting Return to accept the dialog), and it would be wrong for the original focus owner to process that event.
-        Thread.dumpStack();
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 dialogFocusRedirector.restoreFocus();
