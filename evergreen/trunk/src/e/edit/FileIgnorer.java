3f786ca9e4845613d0415f2b9ed20fb1df8b103b
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-06-18 04:57:52 +0000

    src/e/edit/FileIgnorer.java: we shouldn't index files in BitKeeper's RESYNC/ directories.

--- evergreen/trunk/src/e/edit/FileIgnorer.java
+++ evergreen/trunk/src/e/edit/FileIgnorer.java
@@ -46,7 +46,7 @@ public class FileIgnorer {
         patterns.add("\\.bzr");
         patterns.add("\\.hg");
         patterns.add("\\.svn");
-        patterns.add("BitKeeper");
+        patterns.add("BitKeeper"); patterns.add("RESYNC");
         patterns.add("CVS");
         patterns.add("SCCS");
         
