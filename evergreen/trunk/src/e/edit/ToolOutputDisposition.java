d522c5cd196367369e9a4635e3f3290cfe2ecff3
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-07-18 05:42:19 +0000

    src/e/edit/ToolOutputDisposition.java: add a new output disposition for putting the output on the clipboard.
    src/e/edit/ShellCommand.java: implement the CLIPBOARD and DIALOG output dispositions. There's no convenient way to request these yet, so I temporarily modified my ExternalToolsParser to use them.

--- evergreen/trunk/src/e/edit/ToolOutputDisposition.java
+++ evergreen/trunk/src/e/edit/ToolOutputDisposition.java
@@ -4,6 +4,7 @@ package e.edit;
  * Represents the different things that can happen to the output of a ShellCommand.
  */
 public enum ToolOutputDisposition {
+    CLIPBOARD,
     CREATE_NEW_DOCUMENT,
     DIALOG,
     DISCARD,
