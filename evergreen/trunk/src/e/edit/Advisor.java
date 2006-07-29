1e4523fe172bbac5f3bf3ffc891b5a497fa88394
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-07-29 06:05:04 +0000

    src/e/edit/Advisor.java: look up (and format) documentation in the background, and show some basic progress information.

--- evergreen/trunk/src/e/edit/Advisor.java
+++ evergreen/trunk/src/e/edit/Advisor.java
@@ -1,14 +1,15 @@
 package e.edit;
 
+import e.gui.*;
+import e.ptextarea.*;
+import e.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import java.util.concurrent.*;
 import javax.swing.*;
 import javax.swing.Timer;
-import e.ptextarea.*;
-import e.gui.*;
-import e.util.*;
+import org.jdesktop.swingworker.SwingWorker;
 
 public class Advisor extends JPanel {
     private static Advisor instance;
@@ -66,6 +67,45 @@ public class Advisor extends JPanel {
         research(getLookupString());
     }
     
+    private class ResearchRunner extends SwingWorker<String, Object> {
+        private String searchTerm;
+        private ETextWindow textWindow;
+        
+        private ResearchRunner(String searchTerm) {
+            this.searchTerm = searchTerm;
+            this.textWindow = ETextAction.getFocusedTextWindow();
+            advicePane.setText("Searching for documentation on \"" + searchTerm + "\"...");
+        }
+        
+        @Override
+        protected String doInBackground() {
+            StringBuilder newText = new StringBuilder();
+            for (WorkspaceResearcher researcher : researchers) {
+                if (textWindow == null || researcher.isSuitable(textWindow)) {
+                    String result = researcher.research(searchTerm);
+                    if (result != null && result.length() > 0) {
+                        newText.append(result);
+                    }
+                }
+            }
+            
+            if (newText.length() == 0) {
+                newText.append("No documentation found for \"" + searchTerm + "\".");
+            }
+            
+            return newText.toString();
+        }
+        
+        @Override
+        public void done() {
+            try {
+                showDocumentation(get());
+            } catch (Exception ex) {
+                Log.warn("ResearchRunner failed", ex);
+            }
+        }
+    }
+    
     public synchronized void showDocumentation(String content) {
         if (content.startsWith("<html>") == false) {
             content = "<html><head><title></title></head><body bgcolor=#FFFFFF>" + content + "</body></html>";
@@ -102,22 +142,7 @@ public class Advisor extends JPanel {
     }
     
     public void research(String text) {
-        ETextWindow textWindow = ETextAction.getFocusedTextWindow();
-        StringBuilder newText = new StringBuilder();
-        for (WorkspaceResearcher researcher : researchers) {
-            if (textWindow == null || researcher.isSuitable(textWindow)) {
-                String result = researcher.research(text);
-                if (result != null && result.length() > 0) {
-                    newText.append(result);
-                }
-            }
-        }
-        
-        if (newText.length() == 0) {
-            newText.append("No documentation found for \"" + text + "\".");
-        }
-        
-        showDocumentation(newText.toString());
+        new ResearchRunner(text).execute();
     }
     
     private void addResearcher(WorkspaceResearcher researcher) {
