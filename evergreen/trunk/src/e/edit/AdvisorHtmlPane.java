1046854b821cc008efad0b537a2efe2ca98e67ed
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-08-15 06:40:50 +0000

    src/e/edit/AdvisorHtmlPane.java: add basic find functionality to the HTML component in the help browser.

--- evergreen/trunk/src/e/edit/AdvisorHtmlPane.java
+++ evergreen/trunk/src/e/edit/AdvisorHtmlPane.java
@@ -1,12 +1,18 @@
 package e.edit;
 
 import e.gui.*;
+import e.util.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.util.regex.*;
 import javax.swing.*;
 import javax.swing.event.*;
+import javax.swing.text.*;
 import javax.swing.text.html.*;
 
 public class AdvisorHtmlPane extends JComponent implements HyperlinkListener {
+    private static final DefaultHighlighter.DefaultHighlightPainter FIND_HIGHLIGHT_PAINTER = new DefaultHighlighter.DefaultHighlightPainter(e.ptextarea.PFind.MATCH_COLOR);
+    
     private JTextPane textPane;
     private EStatusBar statusBar;
     
@@ -24,6 +30,8 @@ public class AdvisorHtmlPane extends JComponent implements HyperlinkListener {
         textPane.setDragEnabled(false);
         textPane.setEditable(false);
         textPane.addHyperlinkListener(this);
+        
+        ComponentUtilities.initKeyBinding(textPane, new FindAction(textPane));
         
         HTMLEditorKit editorKit = (HTMLEditorKit) textPane.getEditorKit();
         StyleSheet styleSheet = editorKit.getStyleSheet();
@@ -58,6 +66,72 @@ public class AdvisorHtmlPane extends JComponent implements HyperlinkListener {
             } else {
                 Advisor.getInstance().linkClicked(e.getDescription());
             }
+        }
+    }
+    
+    public static class FindAction extends AbstractAction {
+        private JTextPane textPane;
+        private JTextField findField = new JTextField(40);
+        
+        public FindAction(JTextPane textPane) {
+            super("Find...");
+            this.textPane = textPane;
+            putValue(ACCELERATOR_KEY, e.util.GuiUtilities.makeKeyStroke("F", false));
+        }
+        
+        public void actionPerformed(ActionEvent e) {
+            String selection = textPane.getSelectedText();
+            if (selection != null && selection.length() > 0) {
+                findField.setText(StringUtilities.regularExpressionFromLiteral(selection));
+            }
+            
+            AbstractFindDialog findDialog = new AbstractFindDialog() {
+                public int updateFindResults(String regularExpression) {
+                    return findAllMatches(regularExpression);
+                }
+                
+                public void clearFindResults() {
+                    findAllMatches(null);
+                }
+            };
+            findDialog.showFindDialog(textPane, findField);
+        }
+        
+        public int findAllMatches(String regularExpression) {
+            Highlighter highlighter = textPane.getHighlighter();
+            
+            // Remove any existing find highlights.
+            Highlighter.Highlight[] highlights = highlighter.getHighlights();
+            for (Highlighter.Highlight highlight : highlights) {
+                if (highlight.getPainter() == FIND_HIGHLIGHT_PAINTER) {
+                    highlighter.removeHighlight(highlight);
+                }
+            }
+            
+            if (regularExpression == null || regularExpression.length() == 0) {
+                return 0;
+            }
+            
+            String content = textPane.getText();
+            if (content == null) {
+                return 0;
+            }
+            
+            Pattern pattern = PatternUtilities.smartCaseCompile(regularExpression);
+            int matchCount = 0;
+            HTMLDocument document = (HTMLDocument) textPane.getDocument();
+            for (HTMLDocument.Iterator it = document.getIterator(HTML.Tag.CONTENT); it.isValid(); it.next()) {
+                try {
+                    String fragment = document.getText(it.getStartOffset(), it.getEndOffset() - it.getStartOffset());
+                    Matcher matcher = pattern.matcher(fragment);
+                    while (matcher.find()) {
+                        highlighter.addHighlight(it.getStartOffset() + matcher.start(), it.getStartOffset() + matcher.end(), FIND_HIGHLIGHT_PAINTER);
+                        ++matchCount;
+                    }
+                } catch (BadLocationException ex) {
+                }
+            }
+            return matchCount;
         }
     }
 }
