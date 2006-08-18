2e1e5bd6ac431056505b459f5bedaeca882e8ee7
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-08-18 06:11:38 +0000

    src/e/edit/ETextWindow.java: pull the file-type guessing code out...
    src/e/edit/FileType.java: ...into the new FileType class.

--- evergreen/trunk/src/e/edit/ETextWindow.java
+++ evergreen/trunk/src/e/edit/ETextWindow.java
@@ -1,5 +1,9 @@
 package e.edit;
 
+import e.forms.*;
+import e.gui.*;
+import e.ptextarea.*;
+import e.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
@@ -9,10 +13,6 @@ import java.util.regex.*;
 import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.Timer;
-import e.forms.*;
-import e.gui.*;
-import e.ptextarea.*;
-import e.util.*;
 
 /**
  * A text-editing component.
@@ -247,59 +247,6 @@ public class ETextWindow extends EWindow implements PTextListener {
         textBecameDirty();
     }
     
-    /**
-     * Tests whether the 'content' looks like a Unix shell script. If
-     * 'interpreter' is a shell, you should probably prepend "/" to avoid
-     * false positives; if 'interpreter' is a scripting language, you should
-     * probably avoid doing so because it's still relatively common practice
-     * to use the env(1) hack. We could perhaps automate this by matching
-     * either ("/" + interpreter) or ("/env\\s[^\\n]*" + interpreter).
-     */
-    private static boolean isInterpretedContent(CharSequence content, String interpreter) {
-        return Pattern.compile("^#![^\\n]*" + interpreter).matcher(content).find();
-    }
-    
-    /** Tests whether the 'content' looks like a Unix script, of the Ruby variety. */
-    private static boolean isRubyContent(CharSequence content) {
-        return isInterpretedContent(content, "ruby");
-    }
-    
-    /** Tests whether the 'content' looks like a Unix script, of the Perl variety. */
-    private static boolean isPerlContent(CharSequence content) {
-        return isInterpretedContent(content, "perl");
-    }
-    
-    /** Tests whether the 'content' looks like a Unix script, of the Python variety. */
-    private static boolean isPythonContent(CharSequence content) {
-        return isInterpretedContent(content, "python");
-    }
-    
-    private static boolean isBashContent(CharSequence content) {
-        return isInterpretedContent(content, "/(bash|sh)");
-    }
-    
-    /**
-     * Tests whether the 'content' looks like a C++ file.
-     *
-     * A standard C++ header file (such as <string>) might not have any extension,
-     * though it's likely in that case start with #ifndef.
-     *
-     * GNU headers tend to have an emacs mode hint, so let's obey those too (I think
-     * emacs scans the whole file, but GNU headers seem to use the first line).
-     */
-    private static boolean isCPlusPlusContent(CharSequence content) {
-        // FIXME: this method has an over-simplified idea of emacs mode strings, based on the STL header file usage.
-        // Here are two examples. The former shows that we could use a similar check for Ruby; the latter that we ought to recognize another form, and that we can also potentially find the tab string this way.
-        // hydrogen:/usr/lib/ruby/1.8$ grep -n -- '-\*-' *
-        // getoptlong.rb:1:#                                                         -*- Ruby -*-
-        // yaml.rb:1:# -*- mode: ruby; ruby-indent-level: 4; tab-width: 4 -*- vim: sw=4 ts=4
-        // We should also probably recognize plain C (since our C_PLUS_PLUS means C/C++/Objective-C/Objective-C++):
-        // powerpc-darwin8.0/dl.h:1:/* -*- C -*-
-        // FIXME: emacs mode strings should be handled separately, and override content-based file type determination.
-        // FIXME: gEdit's "modelines" plug-in http://cvs.gnome.org/viewcvs/gedit/plugins/modelines/ details emacs(1), kate(1), and vim(1) mode lines.
-        return Pattern.compile("(#ifndef|" + StringUtilities.regularExpressionFromLiteral("-*- C++ -*-") + ")").matcher(content).find();
-    }
-    
     private void highlightMergeConflicts() {
         // FIXME: this works for BitKeeper, but is it right for Subversion too?
         final String MERGE_CONFLICT_REGULAR_EXPRESSION = "(<{7} .*|>{7})";
@@ -375,7 +322,7 @@ public class ETextWindow extends EWindow implements PTextListener {
             lastModifiedTime = file.lastModified();
             text.getTextBuffer().readFromFile(file);
             
-            configureForGuessedFileType();
+            reconfigureForGuessedFileType();
             updateWatermarkAndTitleBar();
             highlightMergeConflicts();
             text.getTextBuffer().getUndoBuffer().resetUndoBuffer();
@@ -388,66 +335,9 @@ public class ETextWindow extends EWindow implements PTextListener {
         }
     }
     
-    /**
-     * There are three main ways to guess a file's type: by content, by name,
-     * or by emacs mode string. The initFileType methods implement this, though
-     * there's no uniform checking for emacs mode strings (the perlrun man page,
-     * for example, mentions "#!/bin/sh -- # -*- perl -*- -p" but we wouldn't
-     * currently recognize such a script as a Perl script, even though we would
-     * recognize a C++ files by its emacs mode string).
-     */
-    private void initFileType() {
-        // See if we can infer the type by name first and fall back to guessing
-        // from the content. If you don't do it this way round, you get fooled
-        // by files (such as this one) that contain things that look like
-        // suggestive content. It's hard to see that there's ever any excuse
-        // for having the wrong filename extension.
-        initFileTypeByName();
-        if (fileType == FileType.PLAIN_TEXT) {
-            initFileTypeByContent();
-        }
-    }
-    private void initFileTypeByContent() {
-        CharSequence content  = text.getTextBuffer();
-        if (isRubyContent(content)) {
-            fileType = FileType.RUBY;
-        } else if (isBashContent(content)) {
-            fileType = FileType.BASH;
-        } else if (isCPlusPlusContent(content)) {
-            fileType = FileType.C_PLUS_PLUS;
-        } else if (isPerlContent(content)) {
-            fileType = FileType.PERL;
-        } else if (isPythonContent(content)) {
-            fileType = FileType.PYTHON;
-        } else {
-            fileType = FileType.PLAIN_TEXT;
-        }
-    }
-    private void initFileTypeByName() {
-        if (filename.endsWith(".java")) {
-            fileType = FileType.JAVA;
-        } else if (filename.endsWith(".cpp") || filename.endsWith(".hpp") || filename.endsWith(".c") || filename.endsWith(".h") || filename.endsWith(".m") || filename.endsWith(".mm") || filename.endsWith(".hh") || filename.endsWith(".cc") || filename.endsWith(".strings")) {
-            fileType = FileType.C_PLUS_PLUS;
-        } else if (filename.endsWith(".pl") || filename.endsWith(".pm")) {
-            fileType = FileType.PERL;
-        } else if (filename.endsWith(".py")) {
-            fileType = FileType.PYTHON;
-        } else if (filename.endsWith(".rb")) {
-            fileType = FileType.RUBY;
-        } else if (filename.endsWith(".sh") || filename.endsWith("bash.bashrc") || filename.endsWith("bash.logout") || filename.endsWith(".bash_profile") || filename.endsWith(".bashrc") || filename.endsWith(".bash_logout")) {
-            fileType = FileType.BASH;
-        } else if (filename.endsWith("Makefile") || filename.endsWith("GNUmakefile") || filename.endsWith("makefile") || filename.endsWith(".make")) {
-            fileType = FileType.MAKE;
-        } else if (filename.endsWith(".vhd")) {
-            fileType = FileType.VHDL;
-        } else {
-            fileType = FileType.PLAIN_TEXT;
-        }
-    }
-    
-    private void configureForGuessedFileType() {
+    private void reconfigureForGuessedFileType() {
         FileType originalFileType = fileType;
-        initFileType();
+        fileType = FileType.guessFileType(filename, text.getTextBuffer());
         if (fileType == FileType.JAVA) {
             text.setIndenter(new PJavaIndenter(text));
             text.setTextStyler(new PJavaTextStyler(text));
@@ -799,7 +689,7 @@ public class ETextWindow extends EWindow implements PTextListener {
             editor.showStatus("Saved " + filename);
             backupFile.delete();
             this.lastModifiedTime = file.lastModified();
-            configureForGuessedFileType();
+            reconfigureForGuessedFileType();
             updateWatermarkAndTitleBar();
             tagsUpdater.updateTags();
             SaveMonitor.getInstance().fireSaveListeners();
