3bc654674d0160f59d7caef54cab4c34439a8183
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-08-24 05:10:06 +0000

    src/e/edit/EvergreenLaunchable.java: open files given as arguments using invokeLater so they'll be opened after all the remembered files instead of before.

--- evergreen/trunk/src/e/edit/EvergreenLaunchable.java
+++ evergreen/trunk/src/e/edit/EvergreenLaunchable.java
@@ -11,9 +11,16 @@ public class EvergreenLaunchable implements Launchable {
     }
     
     public void startGui() {
-        Evergreen editor = Evergreen.getInstance();
-        for (String argument : arguments) {
-            editor.openFile(argument);
+        // Start up the editor.
+        final Evergreen editor = Evergreen.getInstance();
+        
+        // Open any files given as arguments after everything else has been set up to increase their chances of being on a visible workspace.
+        for (final String argument : arguments) {
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    editor.openFile(argument);
+                }
+            });
         }
     }
 }
