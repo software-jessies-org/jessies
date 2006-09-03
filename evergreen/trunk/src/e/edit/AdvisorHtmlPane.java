b1d7a9516a1842817a5985ed80ce08a30eefefbc
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-09-03 17:38:16 +0000

    src/e/edit/AdvisorHtmlPane.java: implement web browser keyboard navigation with home, end, space, shift-space, page up, page down, up arrow, and down arrow.

--- evergreen/trunk/src/e/edit/AdvisorHtmlPane.java
+++ evergreen/trunk/src/e/edit/AdvisorHtmlPane.java
@@ -3,6 +3,7 @@ package e.edit;
 import e.gui.*;
 import e.util.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.net.*;
 import javax.swing.*;
 import javax.swing.event.*;
@@ -27,7 +28,8 @@ public class AdvisorHtmlPane extends JComponent implements HyperlinkListener {
         textPane.setDragEnabled(false);
         textPane.setEditable(false);
         textPane.addHyperlinkListener(this);
-        JTextComponentUtilities.addFindFunctionalityTo(textPane);
+        
+        initKeyBindings(textPane);
         
         HTMLEditorKit editorKit = (HTMLEditorKit) textPane.getEditorKit();
         StyleSheet styleSheet = editorKit.getStyleSheet();
@@ -43,6 +45,43 @@ public class AdvisorHtmlPane extends JComponent implements HyperlinkListener {
         String pre = "pre { font-family: \"" + preFont.getFamily() + "\", monospace; font-size: " + preFont.getSize() + "pt; background-color: #eeeeff; border-style: solid; border-width: thin; border-color: #bbbbff; padding: 5px 5px 5px 5px; }";
         styleSheet.addRule(pre);
         //System.err.println(pre);
+    }
+    
+    private static class ScrollAction extends AbstractAction {
+        private JComponent c;
+        private boolean byBlock;
+        private int direction;
+        
+        public ScrollAction(KeyStroke keyStroke, JComponent c, boolean byBlock, int direction) {
+            this.c = c;
+            this.byBlock = byBlock;
+            this.direction = direction;
+            putValue(Action.NAME, (byBlock ? "page" : "line") + (direction < 0 ? "Up" : "Down") + "Action");
+            putValue(Action.ACCELERATOR_KEY, keyStroke);
+        }
+        
+        public void actionPerformed(ActionEvent e) {
+            // You can't -- as far as I know -- specify a KeyStroke that insists on a modifier key *not* being down. "No modifiers" really means "any".
+            // So rather than use a KeyStroke with the shift modifier and a KeyStroke without (which would mean both would fire when shift was down), we just use one KeyStroke and check for shift here.
+            boolean shiftDown = ((e.getModifiers() & ActionEvent.SHIFT_MASK) != 0);
+            ComponentUtilities.scroll(c, byBlock, (shiftDown ? -direction : direction));
+        }
+    }
+    
+    private static void initKeyBindings(JTextPane textPane) {
+        // Add C-F, C-D, and C-G.
+        JTextComponentUtilities.addFindFunctionalityTo(textPane);
+        
+        // Add home and end, which are provided by DefaultEditorKit, but don't seem to be set up by default.
+        textPane.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_HOME, 0, false), DefaultEditorKit.beginAction);
+        textPane.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_END, 0, false), DefaultEditorKit.endAction);
+        
+        // Make space and shift-space synonyms for page down and page up, like in real web browsers.
+        ComponentUtilities.initKeyBinding(textPane, new ScrollAction(KeyStroke.getKeyStroke(' '), textPane, true, 1));
+        
+        // Connect the up and down arrow keys to the scroll bar.
+        ComponentUtilities.initKeyBinding(textPane, new ScrollAction(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0, false), textPane, false, 1));
+        ComponentUtilities.initKeyBinding(textPane, new ScrollAction(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0, false), textPane, false, -1));
     }
     
     public void setText(String text) {
