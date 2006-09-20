b7a2ff8deadc57f5af84a120b44dce9904240b8f
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-09-20 05:09:03 +0000

    src/e/util/Bag.java: avoiding auto-boxing is slightly faster. Using HashMap instead of TreeMap would be slightly slower. Keeping track of the commonest item as we go would also be slightly slower.

--- salma-hayek/trunk/src/e/util/Bag.java
+++ salma-hayek/trunk/src/e/util/Bag.java
@@ -3,15 +3,26 @@ package e.util;
 import java.util.*;
 
 public class Bag<V> {
-    private static final Integer ONE = Integer.valueOf(1);
-    private Map<V, Integer> objectToCountMap = new TreeMap<V, Integer>();
+    private static class IntBox {
+        public int value = 1;
+        
+        public String toString() {
+            return Integer.toString(value);
+        }
+    }
+    
+    private Map<V, IntBox> objectToCountMap = new TreeMap<V, IntBox>();
     
     public Bag() {
     }
     
     public void add(V key) {
-        Integer count = objectToCountMap.get(key);
-        objectToCountMap.put(key, (count == null ? ONE : (count.intValue() + 1)));
+        IntBox count = objectToCountMap.get(key);
+        if (count == null) {
+            objectToCountMap.put(key, new IntBox());
+        } else {
+            ++count.value;
+        }
     }
     
     public boolean contains(V key) {
@@ -23,16 +34,16 @@ public class Bag<V> {
     }
     
     public int occurrenceCount(V key) {
-        Integer count = objectToCountMap.get(key);
-        return (count == null) ? 0 : count.intValue();
+        IntBox count = objectToCountMap.get(key);
+        return (count == null) ? 0 : count.value;
     }
     
     public V commonestItem() {
         V result = null;
         int resultCount = 0;
         
-        for (Map.Entry<V, Integer> entry : objectToCountMap.entrySet()) {
-            int thisCount = entry.getValue();
+        for (Map.Entry<V, IntBox> entry : objectToCountMap.entrySet()) {
+            int thisCount = entry.getValue().value;
             if (thisCount > resultCount) {
                 result = entry.getKey();
                 resultCount = thisCount;
@@ -44,9 +55,8 @@ public class Bag<V> {
     public String toString() {
         StringBuilder result = new StringBuilder();
         result.append("Bag[");
-        for (Map.Entry<V, Integer> entry : objectToCountMap.entrySet()) {
-            int thisCount = entry.getValue();
-            result.append("<" + entry.getKey() + ", " + thisCount + ">");
+        for (Map.Entry<V, IntBox> entry : objectToCountMap.entrySet()) {
+            result.append("<" + entry.getKey() + ", " + entry.getValue() + ">");
         }
         result.append("]");
         return result.toString();
