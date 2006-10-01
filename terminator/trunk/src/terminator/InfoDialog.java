3618a43c72d0f1ea4ae689b7d9115fe3bda69181
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-10-01 05:25:56 +0000

    src/terminator/InfoDialog.java: an improved work-around for one of the GTK LAF's bugs: the fact that setBorder(null) doesn't stop a JTextField's border from being drawn.

--- terminator/trunk/src/terminator/InfoDialog.java
+++ terminator/trunk/src/terminator/InfoDialog.java
@@ -39,10 +39,16 @@ public class InfoDialog {
             // You lose the visual clue that you can select and copy the text, but that's less important than obscuring the visual clue on editable fields that they're editable.
             // FIXME: at the moment, we're far too wide when there are lots of processes with access to the terminal.
             // FIXME: a PTextArea would retain the selection behavior but add wrapping, but we need to change FormPanel first because GridBagLayout won't let us do the right thing when (say) the "dimensions" and "processes" need one line-height each, but "log filename" needs two line-heights.
-            // FIXME: because of the way the GTK+ LAF renders text fields, this looks awful. setBorder effectively just removes the padding, and setOpaque has no effect.
+            // FIXME: because of the way the GTK+ LAF renders text fields, this looks awful. setBorder effectively just removes the padding, and setOpaque has no effect. See getName for a partial work-around.
             setBorder(null);
             setOpaque(false);
             setEditable(false);
+        }
+        
+        public String getName() {
+            // The GTK+ LAF insists on rendering a border unless we're a tree cell editor. So pretend to be a tree cell editor.
+            // This still doesn't look quite right, but it's the best I know how to do, and it's significantly better than doing nothing.
+            return GuiUtilities.isGtk() ? "Tree.cellEditor" : super.getName();
         }
     }
     
