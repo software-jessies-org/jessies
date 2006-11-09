51d6186040491e81d6d042ab116433817bb08b1b
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-11-09 05:37:13 +0000

    src/e/ptextarea/PXmlTextStyler.java: document a limitation of our coloring that the ".wxs" files bring to light.

--- salma-hayek/trunk/src/e/ptextarea/PXmlTextStyler.java
+++ salma-hayek/trunk/src/e/ptextarea/PXmlTextStyler.java
@@ -8,8 +8,11 @@ import java.util.*;
 public class PXmlTextStyler extends PAbstractLanguageStyler {
     public PXmlTextStyler(PTextArea textArea) {
         super(textArea);
+        
         // We add the tag-recognizing applicator first so that the standard hyperlink applicator doesn't interfere with it.
+        // FIXME: we don't cope with tags split across multiple lines because our styling works on a per-line basis; we'd need to remember state like we do for multi-line comments.
         textArea.addStyleApplicatorFirst(new RegularExpressionStyleApplicator(textArea, "(<.*?>)", PStyle.KEYWORD));
+        
         // FIXME: we probably need code similar to what we have in the Java styler to recognize bad/unclosed entities.
         textArea.addStyleApplicator(new RegularExpressionStyleApplicator(textArea, "(&.*?;)", PStyle.KEYWORD));
     }
