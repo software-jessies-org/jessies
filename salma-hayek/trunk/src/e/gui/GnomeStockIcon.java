ed53d2a21b5fa84283a23bad59dc476d81985202
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2006-11-26 23:14:12 +0000

    src/e/gui/GnomeStockIcon.java: work around a couple of Java 6 GTK LAF bugs.

--- salma-hayek/trunk/src/e/gui/GnomeStockIcon.java
+++ salma-hayek/trunk/src/e/gui/GnomeStockIcon.java
@@ -1,6 +1,8 @@
 package e.gui;
 
 import e.util.*;
+import java.awt.*;
+import java.awt.image.*;
 import javax.swing.*;
 
 public class GnomeStockIcon {
@@ -41,7 +43,37 @@ public class GnomeStockIcon {
         Icon icon = getStockIcon(name, Size.GTK_ICON_SIZE_BUTTON);
         if (icon != null) {
             button.setIcon(icon);
+            workAroundLafBugs(button);
         }
+    }
+    
+    private static void workAroundLafBugs(JButton button) {
+        // FIXME: there seems to be a bug with the GTK LAF that means when the button is disabled, we see the icon and "...", and only when it's enabled do we see "Commit". This is a work-around.
+        // Note that the code below for the disabled icon relies on this (the call to getPreferredSize forces the image to be loaded).
+        button.setPreferredSize(button.getPreferredSize());
+        
+        // FIXME: Java 6 looks like it will ship with a bug that means disabled icons aren't right for their theme.
+        // This workaround is close enough (but not exact) for the Ubuntu 6.10 "Human" theme.
+        Icon icon = button.getIcon();
+        Image image = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics g = image.getGraphics();
+        icon.paintIcon(null, g, 0, 0);
+        g.dispose();
+        RGBImageFilter filter = new RGBImageFilter() {
+            // Based on the comment from "jansan" on http://weblogs.java.net/blog/joshy/archive/2006/08/windows_lf_bugs.html but with a different alpha scale factor to better match Ubuntu 6.10's "Human" theme.
+            public int filterRGB(int x, int y, int rgb) {
+                int red = (rgb >> 16) & 0xff;
+                int green = (rgb >> 8) & 0xff;
+                int blue = rgb & 0xff;
+                int average = (red + green + blue) / 3;
+                int alpha = (rgb >> 24) & 0xff;
+                int newAlpha = (int) (alpha * 0.2);
+                int newRgb = (newAlpha << 24) | (average << 16) | (average << 8) | average;
+                return newRgb;
+            }
+        };
+        Image grayImage = Toolkit.getDefaultToolkit().createImage(new FilteredImageSource(image.getSource(), filter));
+        button.setDisabledIcon(new ImageIcon(grayImage));
     }
     
     /**
