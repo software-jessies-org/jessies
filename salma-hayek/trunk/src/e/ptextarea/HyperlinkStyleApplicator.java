3b62727e883b2c83d54e715cd2b8a210a7142897
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-01-22 20:40:01 +0000

    src/e/ptextarea/HyperlinkStyleApplicator.java: A "zero-width negative lookbehind assertion" causes all the examples to be rendered correctly. The intention is to say "as you were but the last character of the URL mustn't be [),.]". (I've written URLs at the ends of sentences several times and then been annoyed into rephrasing it to fix the linking.)
    bin/invoke-java.rb: Revert unintentional check-in of wrong file.  That's for later today.

--- salma-hayek/trunk/src/e/ptextarea/HyperlinkStyleApplicator.java
+++ salma-hayek/trunk/src/e/ptextarea/HyperlinkStyleApplicator.java
@@ -29,12 +29,10 @@ class HyperlinkStyleApplicator extends RegularExpressionStyleApplicator {
     // This character class and the regular expression below are based on the BNF in RFC 1738.
     // Compromises have been made to fit the grammar into a fairly readable regular expression.
     // If we needed to, I think we could write an exact regular expression.
-    // Although ")," should be in here, realistically they never occur, and including them just means we're confused by delimiters in the surrounding text.
-    // We should disallow a final "." too, but if I knew how to conveniently do that, I'd have done the same for ")," too.
-    private static final String SEARCH_CHARS = "[/A-Za-z0-9;:@&=%!*'($_.+-]";
+    private static final String SEARCH_CHARS = "[/A-Za-z0-9;:@&=%!*'(),$_.+-]";
     
     public HyperlinkStyleApplicator(PTextArea textArea) {
-        super(textArea, "\\b(https?://[A-Za-z0-9.:-]+[A-Za-z0-9](/~?"+SEARCH_CHARS+"*(\\?"+SEARCH_CHARS+"*)?)?(\\#"+SEARCH_CHARS+"+)?)", PStyle.HYPERLINK);
+        super(textArea, "\\b(https?://[A-Za-z0-9.:-]+[A-Za-z0-9](/~?"+SEARCH_CHARS+"*(\\?"+SEARCH_CHARS+"*)?)?(\\#"+SEARCH_CHARS+"+)?)(?<![),.])", PStyle.HYPERLINK);
     }
     
     @Override
