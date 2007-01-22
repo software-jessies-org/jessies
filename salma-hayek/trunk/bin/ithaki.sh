36c5c6225c9892bca3e483a430727eb4af76e7fe
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-01-22 23:58:06 +0000

    bin/ithaki.sh: Undo the hacks now I've (a) fixed libx11-dev on wide.us.dev.bluearc.com and (b) fixed the makerules etc to cope with different architectures on the same OS sharing the same work area.  (It didn't require a very big hammer after all - apt-get was just asking me, in a particularly obtuse way, to ask it to install that package individually, so it could tell me that it would have to remove some obsolete package that one of its dependencies replaces.)
    www/downloads.html: Flip the links to the new downloads/ tree, whose one .htaccess file is now conveniently under source code control.  Advertise the amd64 builds.  I haven't seen how this looks in a browser.  I'm not attached to the form of my changes - I was just trying to minimize the amount of duplication in the HTML to minimize the likelihood of my making a mistake.

--- salma-hayek/trunk/bin/ithaki.sh
+++ salma-hayek/trunk/bin/ithaki.sh
@@ -27,19 +27,14 @@ DEB_DIRECTORY=/u/u154/software.jessies.org/
 mkdir -p $DEB_DIRECTORY
 cd $DEB_DIRECTORY
 # Remove any .debs with obsolete names.
-# FIXME: But don't do this yet, so the amd64 .debs that I created remain there, even though I'm not building them regularly.
-#rm *.deb
+rm *.deb
 
 # Run the latest version of the nightly build script, rather than the version from yesterday.
 # This has the advantage that the update won't overwrite the running script - which Ruby doesn't like.
 NIGHTLY_BUILD_SCRIPT=~martind/software.jessies.org/work/salma-hayek/bin/nightly-build.rb
 $NIGHTLY_BUILD_SCRIPT clean native-dist
-# We have to copy the i386 .debs before we clean, before we build the amd64 .debs,
-# because we use the same directories for the intermediate binary files.
+echo $NIGHTLY_BUILD_SCRIPT native-dist | ssh wide bash --login
 find ~martind/software.jessies.org/nightlies/ -name "*.deb" | xargs cp --target-directory=.
-# FIXME: Don't do this because libx11-dev is broken on wide and I don't really want to leave duezer:/usr/include/X11 mounted on wide:/usr/include/X11 which is what I did before.
-#echo $NIGHTLY_BUILD_SCRIPT clean native-dist | ssh wide bash --login
-#find ~martind/software.jessies.org/nightlies/ -name "*.deb" | xargs cp --target-directory=.
 
 # If we don't have a Packages file (as well as Packages.gz) we get:
 # Failed to fetch http://deb/software.jessies.org/./Release  Unable to find expected entry  Packages in Meta-index file (malformed Release file?)
