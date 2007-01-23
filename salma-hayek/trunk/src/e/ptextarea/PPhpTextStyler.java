116c7b626092b7167b019bff185ee12426b847bb
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-01-23 05:41:14 +0000

    src/e/ptextarea/FileType.java: src/e/ptextarea/PPBasicTextStyler.java: src/e/ptextarea/PPhpTextStyler.java: support for PBASIC and PHP coloring.

---
+++ salma-hayek/trunk/src/e/ptextarea/PPhpTextStyler.java
@@ -0,0 +1,112 @@
+package e.ptextarea;
+
+import java.util.*;
+
+public class PPhpTextStyler extends PAbstractLanguageStyler {
+    // From http://www.phpbuilder.com/manual/en/reserved.php, sorted by sort(1).
+    private static final String[] KEYWORDS = new String[] {
+        "abstract",
+        "and",
+        "array",
+        "as",
+        "break",
+        "case",
+        "catch",
+        "cfunction",
+        "class",
+        "__CLASS__",
+        "clone",
+        "const",
+        "continue",
+        "declare",
+        "default",
+        "die",
+        "do",
+        "echo",
+        "else",
+        "elseif",
+        "empty",
+        "enddeclare",
+        "endfor",
+        "endforeach",
+        "endif",
+        "endswitch",
+        "endwhile",
+        "eval",
+        "exception",
+        "exit",
+        "extends",
+        "extends",
+        "__FILE__",
+        "final",
+        "for",
+        "foreach",
+        "function",
+        "__FUNCTION__",
+        "global",
+        "if",
+        "implements",
+        "include",
+        "include_once",
+        "interface",
+        "isset",
+        "__LINE__",
+        "list",
+        "__METHOD__",
+        "new",
+        "old_function",
+        "or",
+        "php_user_filter",
+        "print",
+        "private",
+        "protected",
+        "public",
+        "require",
+        "require_once",
+        "return",
+        "static",
+        "switch",
+        "throw",
+        "try",
+        "unset",
+        "use",
+        "var",
+        "while",
+        "xor"
+    };
+    
+    public PPhpTextStyler(PTextArea textArea) {
+        super(textArea);
+    }
+    
+    @Override
+    protected boolean isStartOfCommentToEndOfLine(String line, int atIndex) {
+        // FIXME: why doesn't this work here, when it works fine for assembler, C++, and Java?
+        //return line.startsWith("//'", atIndex);
+        return (line.length() - atIndex >= 2) && line.charAt(atIndex) == '/' && line.charAt(atIndex) == '/';
+    }
+    
+    @Override
+    protected boolean supportMultiLineComments() {
+        return true;
+    }
+    
+    @Override
+    protected String multiLineCommentStart() {
+        return "/*";
+    }
+    
+    @Override
+    protected String multiLineCommentEnd() {
+        return "*/";
+    }
+    
+    @Override
+    protected boolean isQuote(char ch) {
+        return (ch == '\'' || ch == '\"');
+    }
+    
+    public void addKeywordsTo(Collection<String> collection) {
+        collection.addAll(Arrays.asList(KEYWORDS));
+    }
+}
