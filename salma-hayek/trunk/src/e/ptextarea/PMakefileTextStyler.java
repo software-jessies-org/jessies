f5d68e2c2593bc26bb7782a04022e8800f82f8b5
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-01-25 07:17:39 +0000

    src/e/ptextarea/PMakefileTextStyler.java: handle more of make than we used to, and do a better job of bits we used to understand.

--- salma-hayek/trunk/src/e/ptextarea/PMakefileTextStyler.java
+++ salma-hayek/trunk/src/e/ptextarea/PMakefileTextStyler.java
@@ -3,71 +3,63 @@ package e.ptextarea;
 import java.util.*;
 
 public class PMakefileTextStyler extends PAbstractLanguageStyler {
-    // Regenerate with:
-    // wget -q -O - http://www.gnu.org/software/make/manual/html_node/Name-Index.html | ruby -ne '$_.match(/<code>([a-z][^A-Z]*)<\/code>/) && puts("\"#$1\",")' | sort -u | grep -Ev '^"make(file)?",'
-    private static final String[] KEYWORDS = new String[] {
-        "abspath",
-        "addprefix",
-        "addsuffix",
-        "and",
-        "basename",
-        "bindir",
-        "call",
+    // http://www.gnu.org/software/make/manual/make.html#Quick-Reference
+    private static final String[] DIRECTIVES = new String[] {
         "define",
-        "dir",
-        "else",
         "endef",
+        "ifdef",
+        "ifndef",
+        "ifeq",
+        "ifneq",
+        "else",
         "endif",
-        "error",
-        "eval",
-        "exec_prefix",
+        "include",
+        "sinclude",
+        "override",
         "export",
+        "unexport",
+        "vpath",
+    };
+    private static final String[] FUNCTIONS = new String[] {
+        "subst",
+        "patsubst",
+        "strip",
+        "findstring",
         "filter",
         "filter-out",
-        "findstring",
-        "firstword",
-        "flavor",
-        "foreach",
-        "if",
-        "ifdef",
-        "ifeq",
-        "ifndef",
-        "ifneq",
-        "include",
-        "info",
-        "join",
-        "lastword",
-        "libexecdir",
-        "notdir",
-        "or",
-        "origin",
-        "override",
-        "patsubst",
-        "prefix",
-        "realpath",
-        "sbindir",
-        "shell",
         "sort",
-        "strip",
-        "subst",
-        "suffix",
-        "unexport",
-        "value",
-        "vpath",
-        "warning",
-        "wildcard",
         "word",
         "wordlist",
         "words",
+        "firstword",
+        "lastword",
+        "dir",
+        "notdir",
+        "suffix",
+        "basename",
+        "addsuffix",
+        "addprefix",
+        "join",
+        "wildcard",
+        "realpath",
+        "abspath",
+        "if",
+        "or",
+        "and",
+        "foreach",
+        "call",
+        "value",
+        "eval",
+        "origin",
+        "flavor",
+        "shell",
+        "error",
+        "warning",
+        "info",
     };
     
     public PMakefileTextStyler(PTextArea textArea) {
         super(textArea);
-    }
-    
-    @Override
-    protected String getKeywordRegularExpression() {
-        return "\\b([a-z_-]+)\\b";
     }
     
     @Override
@@ -82,10 +74,25 @@ public class PMakefileTextStyler extends PAbstractLanguageStyler {
     
     @Override
     protected boolean isQuote(char ch) {
+        // FIXME: strictly, you can use ' instead of " after ifeq or ifneq, and it might be better to treat ' as a quote in rules' commands, but naively accepting ' leads to false matches with $(warning) text.
         return (ch == '\"');
     }
     
+    @Override
+    public void initStyleApplicators() {
+        // Make functions are -- except in special cases like when they're arguments to "call" -- always preceded by "$(" or "${".
+        // I didn't realize until testing this (and I've checked the source to see I'm not imagining things) that you can't have whitespace between the "$(" and the keyword.
+        textArea.addStyleApplicator(new KeywordStyleApplicator(textArea, new HashSet<String>(Arrays.asList(FUNCTIONS)), "\\b(?<=\\$[({])([a-z_-]+)\\b"));
+        // Make directives are just plain old words.
+        textArea.addStyleApplicator(new KeywordStyleApplicator(textArea, new HashSet<String>(Arrays.asList(DIRECTIVES)), "\\b(\\w+)\\b"));
+        // There are also various automatic variables.
+        textArea.addStyleApplicator(new RegularExpressionStyleApplicator(textArea, "(\\$[@%<?^+*]|\\$\\([@%<?^+*][DF]\\))", PStyle.KEYWORD));
+        // And finally, special built-in target names.
+        textArea.addStyleApplicator(new RegularExpressionStyleApplicator(textArea, "^(\\.(PHONY|SUFFIXES|DEFAULT|PRECIOUS|INTERMEDIATE|SECONDARY|SECONDEXPANSION|DELETE_ON_ERROR|IGNORE|LOW_RESOLUTION_TIME|SILENT|EXPORT_ALL_VARIABLES|NOTPARALLEL))", PStyle.KEYWORD));
+    }
+    
     public void addKeywordsTo(Collection<String> collection) {
-        collection.addAll(Arrays.asList(KEYWORDS));
+        collection.addAll(Arrays.asList(DIRECTIVES));
+        collection.addAll(Arrays.asList(FUNCTIONS));
     }
 }
