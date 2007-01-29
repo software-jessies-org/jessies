8f7ea6a809888353d80118413914952945f914a6
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-01-29 18:44:45 +0000

    bin/mailpatch.rb: Update the idiom - use parentheses on method calls, use spaces round assignment, use double quotes unless there's a reason not to.  I have a bug to fix too but I'll do that in the next check-in.

--- salma-hayek/trunk/bin/mailpatch.rb
+++ salma-hayek/trunk/bin/mailpatch.rb
@@ -1,20 +1,20 @@
 #!/usr/bin/ruby -w
 
-require 'pathname'
+require "pathname"
 
-script_path=Pathname.new(__FILE__).realpath.dirname
-$: << script_path.dirname
+script_path = Pathname.new(__FILE__).realpath().dirname()
+$: << script_path.dirname()
 
-require 'patch-to-html-email.rb'
+require "patch-to-html-email.rb"
 
-from_address=ENV['LOGNAME']
+from_address = ENV["LOGNAME"]
 if from_address == ""
-  from_address=`whoami`.chomp
+  from_address = `whoami`.chomp()
 end
-to_address=from_address
-reply_to_address=to_address # Use "nil" to reply to author.
-subject="patch"
-preamble=""
+to_address = from_address
+reply_to_address = to_address # Use "nil" to reply to author.
+subject = "patch"
+preamble = ""
 changes = ARGF.readlines()
 
 sendHtmlEmail(from_address, to_address, reply_to_address, subject, preamble, changes)
