02253b739e9b7ee701ad4883322469304500220c
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-02-04 22:16:57 +0000

    bin/ithaki.sh: Build on wide.us.dev.bluearc.com in case Eric's booted it into a 32 bit kernel, in which case we won't have a 64 bit build that day, but at least we won't have overwritten the sarge-era libc version with an etch-era libc version.  (ithaki isn't old enough to be running libc22 but Ed couldn't run binaries compiled on duezer before, despite both machines running variants of (iirc) libc23.)

--- salma-hayek/trunk/bin/ithaki.sh
+++ salma-hayek/trunk/bin/ithaki.sh
@@ -33,8 +33,8 @@ rm *.deb
 # This has the advantage that the update won't overwrite the running script - which Ruby doesn't like.
 NIGHTLY_BUILD_SCRIPT=~martind/software.jessies.org/work/salma-hayek/bin/nightly-build.rb
 $NIGHTLY_BUILD_SCRIPT clean
-$NIGHTLY_BUILD_SCRIPT native-dist
 echo $NIGHTLY_BUILD_SCRIPT native-dist | ssh wide bash --login
+$NIGHTLY_BUILD_SCRIPT native-dist
 find ~martind/software.jessies.org/nightlies/ -name "*.deb" | xargs cp --target-directory=.
 
 # If we don't have a Packages file (as well as Packages.gz) we get:
