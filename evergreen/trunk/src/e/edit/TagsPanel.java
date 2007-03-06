85cb1a6c94f0997c46b748f52c222c564d961733
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-03-06 07:03:55 +0000

    src/e/edit/Evergreen.java: src/e/edit/TagsPanel.java: use BorderFactory where possible.

--- evergreen/trunk/src/e/edit/TagsPanel.java
+++ evergreen/trunk/src/e/edit/TagsPanel.java
@@ -58,7 +58,7 @@ public class TagsPanel extends JPanel {
     public void showError(String error) {
         JLabel label = new JLabel("<html><body>" + error);
         label.setBackground(UIManager.getColor("Tree.background"));
-        label.setBorder(new javax.swing.border.EmptyBorder(4, 4, 4, 4));
+        label.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
         label.setOpaque(true);
         setVisibleComponent(label);
     }
