f6ecd797e199133158a8aac99c7fe69e36352a34
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-03-25 01:26:10 +0000

    bin/invoke-java.rb: use a technique mentioned long ago on the ruby-talk list (and seemingly ignored, despite being a great idea for rubyw.exe) to effectively add an uncaught exception handler. There doesn't seem to be a way to do this globally in Ruby, so we might want to make some kind of abstract Invoker class and let complicated scripts such as bin/terminator provide behavior and have it run by the invoke method.
    bin/show-alert.rb: rewrite to make it clear that you get one show_alert function which has the same signature on each platform. It was only ever inside-out because the three were written independently. Ditch the Applescript, because it's slow, ugly, unreadable, unmaintainable, and doesn't play nicely with escaping/quoting. I fought for long enough trying to get it to behave before giving up and writing...
    
    native/Darwin/NSRunAlertPanel/NSRunAlertPanel.mm: a trivial utility to call NSRunInformationalAlertPanel. I couldn't work out how to do this using Ruby's "dl/import". We should be able to use RubyCocoa when we switch to requiring Java 6 (which isn't likely to ever be available for Mac OS 10.4). This code doesn't get the right icon, but neither did the Applescript. It doesn't seem worth fixing this; we can fix the RubyCocoa version when we have it. (Plus no-one should ever see this anyway.)

--- salma-hayek/trunk/bin/show-alert.rb
+++ salma-hayek/trunk/bin/show-alert.rb
@@ -1,60 +1,45 @@
 #!/usr/bin/ruby -w
 
-require "pathname.rb"
-salma_hayek = Pathname.new(__FILE__).realpath().dirname().dirname()
-require "#{salma_hayek}/bin/target-os.rb"
-
 if defined?(show_alert)
     # Allow this library to be multiply included without warning.
-elsif target_os() == "Darwin"
-    
+else
     def show_alert(title, message)
-        # AppleScript's "display alert" isn't available before 10.4.
-        # Before then we need "display dialog".
-        # See http://daringfireball.net/2005/10/css_checker_101 for an explanation of this work-around.
-        # Running osascript(1) twice is slow, but slightly less unclear and who cares about performance in a failure case anyway?
-        has_display_alert = `osascript -e 'property AS_VERSION_1_10 : 17826208' -e '((system attribute \"ascv\") >= AS_VERSION_1_10)'`.chomp() == "true"
-        if has_display_alert
-            display_command = "display alert \"#{title}\" message \"#{message}\" as informational"
-        else
-            display_command = "display dialog \"#{title}\" & return & return & \"#{message}\" buttons { \"OK\" } default button 1 with icon note"
-        end
-        system("osascript -e 'tell application \"Finder\"' -e 'activate' -e '#{display_command}' -e 'end tell' > /dev/null")
-    end
-    
-elsif target_os() == "Linux"
-    
-    def show_alert(caption, message)
-        text = "#{caption}\n\n#{message}"
-        reported_okay = false
-        # FIXME: this assumes that a KDE user doesn't have the GNOME zenity(1) installed. Which is probably true.
-        if File.exist?("/usr/bin/zenity")
-            command = [ "zenity" ]
-            # The GNOME HIG suggests that dialog titles should be empty, but zenity(1) doesn't currently ensure this.
-            command << "--title="
-            command << "--info"
-            command << "--text"
-            command << text
-            reported_okay = system(*command)
-        end
-        if reported_okay == false && File.exist?("/usr/bin/kdialog")
-            command = [ "kdialog" ]
-            command << "--msgbox"
-            command << text
-            reported_okay = system(*command)
-        end
-        if reported_okay == false
-            $stderr.puts(text)
+        require "pathname.rb"
+        salma_hayek = Pathname.new(__FILE__).realpath().dirname().dirname()
+        
+        require "#{salma_hayek}/bin/target-os.rb"
+        if target_os() == "Darwin"
+            # FIXME: Mac OS 10.5 should give us RubyCocoa. Until then, we need a separate helper executable.
+            ENV["PATH"] = "#{salma_hayek}/bin:#{salma_hayek}/.generated/#{target_directory()}/bin"
+            command = [ "NSRunAlertPanel", title, message ]
+            system(*command)
+        elsif target_os() == "Linux"
+            text = "#{caption}\n\n#{message}"
+            reported_okay = false
+            # FIXME: this assumes that a KDE user doesn't have the GNOME zenity(1) installed. Which is probably true.
+            if File.exist?("/usr/bin/zenity")
+                command = [ "zenity" ]
+                # The GNOME HIG suggests that dialog titles should be empty, but zenity(1) doesn't currently ensure this.
+                command << "--title="
+                command << "--info"
+                command << "--text"
+                command << text
+                reported_okay = system(*command)
+            end
+            if reported_okay == false && File.exist?("/usr/bin/kdialog")
+                command = [ "kdialog" ]
+                command << "--msgbox"
+                command << text
+                reported_okay = system(*command)
+            end
+            if reported_okay == false
+                $stderr.puts(text)
+            end
+        elsif target_os() == "Cygwin" || target_os() == "Windows"
+                require "Win32API"
+                Win32API.new('user32', 'MessageBox', %w(p p p i), 'i').call(0, message, caption, 0)
         end
     end
-    
-elsif target_os() == "Cygwin" || target_os() == "Windows"
-    
-    def show_alert(caption, message)
-        require "Win32API"
-        Win32API.new('user32', 'MessageBox', %w(p p p i), 'i').call(0, message, caption, 0)
-    end
-    
 end
 
 if __FILE__ == $0
