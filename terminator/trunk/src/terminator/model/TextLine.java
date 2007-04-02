570592a3555dc3a36235bc3ba93e6555640ed7e2
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-04-02 01:24:38 +0000

    src/terminator/model/StyledText.java: we were previously quite sloppy about the foreground and background values for styles that didn't have a specific foreground or background. The value would depend on how we'd arrived at the present style, meaning the "short" representation of a style wasn't suitable for comparison. That didn't matter before (as far as I can think), but it matters now...
    src/terminator/model/TextLine.java: rewrite the style-handling side of this class to lazily create the short[] of style data the first time non-default styling is needed for that line. The reasoning being that most lines only ever use the default style, and many of those lines that do use non-default styles are short-lived (because they're on the alternate buffer because you're running something like Vim). Before, the cost per character was two bytes for the character data plus two bytes for the style data for each character (plus some per-line overhead). Now, the cost per character is two bytes for the character itself, and you only pay for the extra two bytes of style data if needed. There's some unnecessary copying in the new code, both for simplicity's sake (copying with the case where there's no style data has added some complexity) but also because with the likely size of a styled line (<= 80 characters) it's not obvious that avoiding copying a few bytes saves more than the extra code and method calls cost; we should measure, I think, before we rush to put the cleverness back.

--- terminator/trunk/src/terminator/model/TextLine.java
+++ terminator/trunk/src/terminator/model/TextLine.java
@@ -3,30 +3,28 @@ package terminator.model;
 import java.util.*;
 
 /**
-A TextLine maintains the characters to be printed on a particular line, and the styles to be applied
-to each.  It also provides mutator methods for removing and writing text, specifically designed for
-use in a virtual terminal.
-
-<p>The text we store internally contains information about tabs.  When text is passed back out to the
-outside world, we either convert the tab information to spaces, or to tab characters.  Internally, a tab
-is marked as beginning at a '\t' character, and each following character (assuming *all* characters are
-the same width) which forms part of the area covered by that tab is denoted by a '\r' character.
-We have to internally store all this tab position and length information because tab positions can change
-in the outside world at any time, but each TextLine must retain its integrity once the tabs have been
-inserted into it.
-
-@author Phil Norman
-*/
-
+ * Ties together the String containing the characters on a particular line, and the styles to be applied to each character.
+ * TextLines are mutable, though it's not possible to change style information without rewriting the corresponding characters (because that's not how terminals work).
+ */
 public class TextLine {
-	private int lineStartIndex;
-	private String text;
-	private short[] styles;
-	
+	// The text we store internally contains information about tabs.
+	// When text is passed back out to the outside world, we either convert the tab information to spaces (for the display), or to tab characters (for the clipboard).
+	// Internally, a tab is marked as beginning with TAB_START.
+	// Each following display position (assuming *all* characters are the same width) covered by the tab is denoted by TAB_CONTINUE.
+	// We have to internally store all this tab position and length information because tab positions can change in the outside world at any time, but each TextLine must retain its integrity once the tabs have been inserted into it.
 	private static final char TAB_START = '\t';
 	private static final char TAB_CONTINUE = '\r';
 	
-	private static final short[] EMPTY_STYLES = new short[0];
+	// The index in characters into the containing buffer of the first character of this line.
+	private int lineStartIndex;
+	
+	// The characters on this line.
+	private String text;
+	
+	// The styles to be applied to the characters on this line.
+	// styles == null => all characters use the default style.
+	// Otherwise, styles.length == text.length(), and the style information for text.charAt(i) is styles[i].
+	private short[] styles;
 	
 	public TextLine() {
 		clear();
@@ -41,24 +39,28 @@ public class TextLine {
 	}
 	
 	public short getStyleAt(int index) {
-		return styles[index];
+		return (styles == null) ? StyledText.getDefaultStyle() : styles[index];
 	}
 	
 	public List<StyledText> getStyledTextSegments() {
-		if (styles.length == 0) {
+		final int textLength = text.length();
+		if (textLength == 0) {
 			return Collections.emptyList();
 		}
+		String string = getString();
 		ArrayList<StyledText> result = new ArrayList<StyledText>();
 		int startIndex = 0;
-		short startStyle = styles[0];
-		for (int i = 1; i < styles.length; i++) {
-			if (styles[i] != startStyle) {
-				result.add(new StyledText(getString().substring(startIndex, i), startStyle));
-				startIndex = i;
-				startStyle = styles[i];
+		short startStyle = getStyleAt(0);
+		if (styles != null) {
+			for (int i = 1; i < textLength; ++i) {
+				if (styles[i] != startStyle) {
+					result.add(new StyledText(string.substring(startIndex, i), startStyle));
+					startIndex = i;
+					startStyle = styles[i];
+				}
 			}
 		}
-		result.add(new StyledText(getString().substring(startIndex, styles.length), startStyle));
+		result.add(new StyledText(string.substring(startIndex, textLength), startStyle));
 		return result;
 	}
 	
@@ -129,7 +131,7 @@ public class TextLine {
 	
 	public void clear() {
 		text = "";
-		styles = EMPTY_STYLES;
+		styles = null;
 	}
 	
 	public void killText(int startIndex, int endIndex) {
@@ -138,7 +140,7 @@ public class TextLine {
 		}
 		endIndex = Math.min(endIndex, text.length());
 		text = text.substring(0, startIndex) + text.substring(endIndex);
-		styles = removeStyleDataFrom(startIndex, endIndex);
+		removeStyleData(startIndex, endIndex);
 	}
 	
 	public void insertTabAt(int offset, int tabLength, short style) {
@@ -155,8 +157,8 @@ public class TextLine {
 			text = text.substring(0, indexAfterTab) + TAB_START + text.substring(indexAfterTab + 1);
 		}
 	}
-
-	private String getTabString(int tabLength) {
+	
+	private static String getTabString(int tabLength) {
 		char[] spaces = new char[tabLength];
 		spaces[0] = TAB_START;
 		Arrays.fill(spaces, 1, spaces.length, TAB_CONTINUE);
@@ -166,8 +168,8 @@ public class TextLine {
 	/** Inserts text at the given position, moving anything already there further to the right. */
 	public void insertTextAt(int offset, String newText, short style) {
 		ensureOffsetIsOK(offset);
-		styles = insertStyleDataInto(offset, newText.length(), style);
 		text = text.substring(0, offset) + newText + text.substring(offset);
+		insertStyleData(offset, newText.length(), style);
 	}
 	
 	/** Writes text at the given position, overwriting anything underneath. */
@@ -175,13 +177,12 @@ public class TextLine {
 		ensureOffsetIsOK(offset);
 		if (offset + newText.length() < text.length()) {
 			text = text.substring(0, offset) + newText + text.substring(offset + newText.length());
-			Arrays.fill(styles, offset, offset + newText.length(), style);
 		} else {
 			text = text.substring(0, offset) + newText;
-			styles = overwriteStyleData(offset, newText.length(), style);
 		}
+		overwriteStyleData(offset, newText.length(), style);
 	}
-
+	
 	private void ensureOffsetIsOK(int offset) {
 		if (offset < 0) {
 			throw new IllegalArgumentException("Negative offset " + offset);
@@ -194,33 +195,57 @@ public class TextLine {
 	private void appendPadding(int count) {
 		char[] pad = new char[count];
 		Arrays.fill(pad, ' ');
+		int oldTextLength = text.length();
 		text += new String(pad);
-		styles = appendStyleData(count, StyledText.getDefaultStyle());
+		insertStyleData(oldTextLength, count, StyledText.getDefaultStyle());
 	}
 	
-	private short[] appendStyleData(int count, short value) {
-		return insertStyleDataInto(styles.length, count, value);
+	private void overwriteStyleData(int offset, int count, short value) {
+		if (styles == null && value == StyledText.getDefaultStyle()) {
+			return;
+		}
+		short[] oldStyleData = maybeResizeStyleData();
+		if (oldStyleData != null) {
+			System.arraycopy(oldStyleData, 0, styles, 0, oldStyleData.length);
+		} else {
+			Arrays.fill(styles, 0, offset, StyledText.getDefaultStyle());
+		}
+		Arrays.fill(styles, offset, offset + count, value);
 	}
 	
-	private short[] overwriteStyleData(int offset, int count, short value) {
-		short[] result = new short[offset + count];
-		System.arraycopy(styles, 0, result, 0, offset);
-		Arrays.fill(result, offset, offset + count, value);
-		return result;
+	private void insertStyleData(int offset, int count, short value) {
+		if (styles == null && value == StyledText.getDefaultStyle()) {
+			return;
+		}
+		short[] oldStyleData = maybeResizeStyleData();
+		if (oldStyleData != null) {
+			System.arraycopy(oldStyleData, 0, styles, 0, offset);
+			Arrays.fill(styles, offset, offset + count, value);
+			System.arraycopy(oldStyleData, offset, styles, offset + count, oldStyleData.length - offset);
+		} else {
+			Arrays.fill(styles, 0, styles.length, StyledText.getDefaultStyle());
+			Arrays.fill(styles, offset, offset + count, value);
+		}
 	}
 	
-	private short[] insertStyleDataInto(int offset, int count, short value) {
-		short[] result = new short[styles.length + count];
-		System.arraycopy(styles, 0, result, 0, offset);
-		Arrays.fill(result, offset, offset + count, value);
-		System.arraycopy(styles, offset, result, offset + count, styles.length - offset);
-		return result;
+	private void removeStyleData(int startIndex, int endIndex) {
+		if (styles == null) {
+			return;
+		}
+		short[] oldStyleData = maybeResizeStyleData();
+		System.arraycopy(oldStyleData, 0, styles, 0, startIndex);
+		System.arraycopy(oldStyleData, endIndex, styles, startIndex, oldStyleData.length - endIndex);
 	}
 	
-	private short[] removeStyleDataFrom(int startIndex, int endIndex) {
-		short[] result = new short[styles.length - (endIndex - startIndex)];
-		System.arraycopy(styles, 0, result, 0, startIndex);
-		System.arraycopy(styles, endIndex, result, startIndex, styles.length - endIndex);
-		return result;
+	/**
+	 * Ensures that the "styles" array is the right size for the current "text".
+	 * You should only call this if you know that the line requires non-default styling.
+	 */
+	private short[] maybeResizeStyleData() {
+		short[] oldStyleData = styles;
+		if (styles == null || styles.length != text.length()) {
+			styles = new short[text.length()];
+		}
+		return oldStyleData;
 	}
 }
