d5201c713419e747d9eaf8e612fa7cf6e0e66150
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-04-14 23:35:31 +0000

    bin/ensure-suitable-mac-os-version.rb: don't worry about Java here; just worry about the Mac OS version, as the script's name suggests. This script predates me making the Java-version-checking code available for all OSes, so removing this lets us take advantage of the (since) more sophisticated code in "invoke-java.rb". The only drawback is that we now don't tell the user all of their problems at once, but practically speaking that's unlikely ever to be a real problem because Apple Java releases tend to be tied to OS releases, and if they weren't, you'd only have one thing to upgrade anyway. "package-for-distribution.rb" ensures that this Mac OS version test is run before our Java version test, and that's the important thing.
    bin/package-for-distribution.rb: stop messing with the path. It's now fully "invoke-java.rb"'s problem to worry about finding a suitable java(1). This will have the effect of breaking any Java (not ours) that was (unknowingly) relying on being run by our Java to work around an unsuitable java(1), but that's the user's (or other author's) concern, not ours.

--- salma-hayek/trunk/bin/ensure-suitable-mac-os-version.rb
+++ salma-hayek/trunk/bin/ensure-suitable-mac-os-version.rb
@@ -1,24 +1,17 @@
 #!/usr/bin/ruby -w
 
-# Reports (via its status code on exit) whether we're running on a suitable
-# Mac OS installation.
+# Reports (via its status code on exit) whether we're running on a suitable Mac OS installation.
 
 # This script is based on the idea at
 #  http://weblog.bignerdranch.com/?p=13
 # which was written in response to the complaint at
 #  http://www.gigliwood.com/weblog/Cocoa/AppleBugFriday__Min.html
-# about Apple's solution (which only works on 10.4 and doesn't tell you what
-# version is required or what version you have).
+# about Apple's solution.
+# The original only works on 10.4 and doesn't tell you what version is required or what version you have.
 
-# We look like the best parts of each, I think, and also include information
-# about what version you have in addition to what version is needed. Although
-# a user may well know what exact version of Mac OS they're running (though I'm
-# not convinced) they probably won't know what exact version of Apple's Java
-# they have.
+# Usage: ensure-suitable-mac-os-version.rb && program-requiring-10.4
 
-# Usage: ensure-suitable-mac-os-version.rb && program-requiring-10.4-with-Java-5.0
-
-# FIXME: we should take the required Mac OS and Java versions as parameters.
+# FIXME: we should take the required Mac OS version as parameters.
 # FIXME: we should be clever enough to offer to open Software Update if you only need to go up a minor revision.
 
 require "pathname.rb"
@@ -28,16 +21,7 @@ require "#{salma_hayek}/bin/show-alert.rb"
 # Do we have a good enough version of Mac OS?
 actual_mac_os_version = `sw_vers -productVersion`.chomp()
 if actual_mac_os_version.match(/^10\.[4-9]/) == nil
-    show_alert("This application requires a newer version of Mac OS X.", "This application requires Mac OS 10.4, but you have Mac OS #{actual_mac_os_version}.\n\nPlease upgrade.")
-    exit(1)
-end
-
-# Do we have a good enough version of Java?
-actual_java_version = `java -fullversion 2>&1`.chomp()
-actual_java_version.match(/java full version "(.*)"/)
-actual_java_version = $1
-if actual_java_version.match(/^1\.[5-9]\.0/) == nil
-    show_alert("This application requires a newer version of Java.", "This application requires Java 5, but you have Java #{actual_java_version} for Mac OS #{actual_mac_os_version}.\n\nPlease upgrade.")
+    show_alert("This application requires a newer version of Mac OS X.", "This application requires at least Mac OS 10.4, but you have Mac OS #{actual_mac_os_version}.\n\nPlease upgrade.")
     exit(1)
 end
 
