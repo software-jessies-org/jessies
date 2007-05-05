4467a868e2559f79f0bc055b8992eea3ff54be35
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-05-05 20:25:23 +0000

    bin/ensure-repository-orthography.rb: The svn switch command is as obtuse, in a different way, as bk parent.  I could mail round a copy-and-paste command but it would be quite long, because the URL includes the user name and we wouldn't want to mess up anyone who's using anonymous svn access.  It would be nice to run this automatically after everyone updates but I haven't thought of a robust way to do that.  So we'll have to rely on people running it manually.  Hopefully everyone updates often enough to get this script before we turn off svn access to Evergreen-with-a-capital-E.
    bin/make-new-project: We've already started suggesting that people check-out using "evergreen" rather than "Evergreen" as the directory name.
    
    parent.make: Unlike nightly-build.rb, which goes looking for files in the child directories, this script relies on a list of known projects.
    
    www/header.html: The URL orthography is now all lower-case.

---
+++ salma-hayek/trunk/bin/ensure-repository-orthography.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby -w
+
+# Cope with symbolic links to this script.
+require "pathname.rb"
+require "fileutils"
+salma_hayek = Pathname.new(__FILE__).realpath().dirname().dirname()
+evergreen = Pathname.new("#{salma_hayek}/../Evergreen").realpath()
+if evergreen.exist?() == false
+    exit(0)
+end
+Dir.chdir(evergreen) {
+    # The "website" repository has no "/trunk" on the end.
+    if `svn info`.match(/^URL: (.*\/Evergreen.*)/)
+        oldUrl = $1
+        newUrl = oldUrl.gsub("Evergreen", "evergreen")
+        system("svn switch --relocate #{oldUrl} #{newUrl}")
+    end
+}
+# Fix the case, even on a case-insensitive file system.
+# Will fail if there's already an "evergreen" as well as an "Evergreen".
+newPath = evergreen.dirname() + "evergreen"
+evergreen.rename(newPath)
