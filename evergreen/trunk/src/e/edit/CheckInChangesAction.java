bd72894bc27cb34efc09bc2000c5c340ed8dc25f
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-05-06 17:34:02 +0000

    src/e/edit/CheckInChangesAction.java: src/e/edit/ShowHistoryAction.java: search for our scripts rather than assuming they're on the path, so Evergreen.app and SCM.app automatically work together without the user having to fix anything.

--- evergreen/trunk/src/e/edit/CheckInChangesAction.java
+++ evergreen/trunk/src/e/edit/CheckInChangesAction.java
@@ -1,11 +1,13 @@
 package e.edit;
 
+import e.util.*;
+
 /**
  * Runs CheckInTool.
  */
 public class CheckInChangesAction extends ExternalToolAction {
     public CheckInChangesAction() {
-        super("Check in Changes...", ToolInputDisposition.NO_INPUT, ToolOutputDisposition.ERRORS_WINDOW, "cd $EDIT_CURRENT_DIRECTORY ; SCM_EDITOR=evergreen checkintool");
+        super("Check in Changes...", ToolInputDisposition.NO_INPUT, ToolOutputDisposition.ERRORS_WINDOW, "cd $EDIT_CURRENT_DIRECTORY ; SCM_EDITOR=" + FileUtilities.findScriptFromBundle("evergreen", "org.jessies.Evergreen") + " " + FileUtilities.findScriptFromBundle("checkintool", "org.jessies.SCM"));
         setChecksEverythingSaved(true);
     }
 }
