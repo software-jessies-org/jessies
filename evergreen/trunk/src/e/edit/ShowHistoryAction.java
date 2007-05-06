b06f9d153e084b42f542e9ebfbb57ed5ff5c8c63
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-05-06 17:34:02 +0000

    src/e/edit/CheckInChangesAction.java: src/e/edit/ShowHistoryAction.java: search for our scripts rather than assuming they're on the path, so Evergreen.app and SCM.app automatically work together without the user having to fix anything.

--- evergreen/trunk/src/e/edit/ShowHistoryAction.java
+++ evergreen/trunk/src/e/edit/ShowHistoryAction.java
@@ -1,11 +1,13 @@
 package e.edit;
 
+import e.util.*;
+
 /**
  * Runs RevisionTool.
  */
 public class ShowHistoryAction extends ExternalToolAction {
     public ShowHistoryAction() {
-        super("Show History...", ToolInputDisposition.NO_INPUT, ToolOutputDisposition.ERRORS_WINDOW, "revisiontool $EDIT_CURRENT_FILENAME:$EDIT_CURRENT_LINE_NUMBER");
+        super("Show History...", ToolInputDisposition.NO_INPUT, ToolOutputDisposition.ERRORS_WINDOW, FileUtilities.findScriptFromBundle("revisiontool", "org.jessies.SCM") + " $EDIT_CURRENT_FILENAME:$EDIT_CURRENT_LINE_NUMBER");
         putValue(ACCELERATOR_KEY, e.util.GuiUtilities.makeKeyStroke("H", true));
         setNeedsFile(true);
     }
