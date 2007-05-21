d0bc0cd21cb69b33f37b31c8a81fe0e988e1b9f7
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-05-21 19:06:07 +0000

    bin/show-alert.rb: If we ever support "Windows Ruby", then the user might expect to stick one of our ".rbw" files on the desktop (and utilize the association between ".rbw" and the "Windows Ruby" rubyw.exe).  This problem, that ruby-launcher is now taking care of for us, could then recur.  That tempted me not to remove this code but, on looking more closely, it uses /proc, which ain't going to work with "Windows Ruby".  At least Windows Ruby is unlikely to shoot itself in the foot with its setting of RUBYOPT.

--- salma-hayek/trunk/bin/show-alert.rb
+++ salma-hayek/trunk/bin/show-alert.rb
@@ -60,21 +60,6 @@ else
     # Use this to report exceptions thrown by the given block.
     def report_exceptions(app_name, &block)
         begin
-            require "pathname.rb"
-            salma_hayek = Pathname.new(__FILE__).realpath().dirname().dirname()
-            
-            require "#{salma_hayek}/bin/target-os.rb"
-            # rubyw.exe throws an exception if it can't write its "Insecure world writable dir" warnings
-            # to $stderr every time it uses Kernel.system or Kernel.`.
-            if target_os() == "Cygwin"
-                if File.exists?("/proc/self/fd/1") == false
-                    $stdout = File.open("/dev/null", "w")
-                end
-                if File.exists?("/proc/self/fd/2") == false
-                    $stderr = File.open("/dev/null", "w")
-                end
-            end
-            
             block.call()
         rescue Exception => e
             show_uncaught_exception(app_name, e)
