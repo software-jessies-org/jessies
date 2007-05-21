40a4fa58bba87a5981cad48c4b035e52a9412d76
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-05-21 05:05:03 +0000

    native/all/ruby-launcher/ruby-launcher.cpp: The promised code for ensuring that stderr (which is the motivating case, and stdout, for completeness) are opened before invoking Ruby, so that any warnings generated by Ruby libraries don't kill us by with EBADF.

--- salma-hayek/trunk/native/all/ruby-launcher/ruby-launcher.cpp
+++ salma-hayek/trunk/native/all/ruby-launcher/ruby-launcher.cpp
@@ -1,11 +1,13 @@
 // Fix signature of unsetenv
 #define __DARWIN_UNIX03 1
 
+#include <fcntl.h>
 #include <iostream>
 #include <stdlib.h>
 #include <string>
 #include <unistd.h>
 
+#include "toString.h"
 #include "unix_exception.h"
 
 void usage() {
@@ -15,11 +17,41 @@ void usage() {
     exit(1);
 }
 
+void ensureDescriptorIsOpen(int targetFd) {
+    // This might look obscure but the man page suggests that it's a POSIX-compliant way
+    // of testing whether a file descriptor is open.
+    int rc = fcntl(targetFd, F_GETFL);
+    if (rc != -1) {
+        return;
+    }
+    if (errno != EBADF) {
+        throw unix_exception("fcntl(" + toString(targetFd) + ", F_GETFL)");
+    }
+    int openedFd = open("/dev/null", O_WRONLY);
+    if (openedFd == -1) {
+        throw unix_exception("open(\"/dev/null\", O_WRONLY)");
+    }
+    // If we've already opened the right descriptor, we mustn't close it!
+    if (openedFd == targetFd) {
+        return;
+    }
+    rc = dup2(openedFd, targetFd);
+    if (rc == -1) {
+        throw unix_exception("dup2(" + toString(openedFd) + ", " + toString(targetFd) + ")");
+    }
+    rc = close(openedFd);
+    if (rc == -1) {
+        throw unix_exception("close(" + toString(openedFd) + ")");
+    }
+}
+
 int main(int, char** argValues) {
     int rc = unsetenv("RUBYOPT");
     if (rc != 0) {
         throw unix_exception("unsetenv(\"RUBYOPT\")");
     }
+    ensureDescriptorIsOpen(1);
+    ensureDescriptorIsOpen(2);
     ++ argValues;
     if (*argValues == 0) {
         usage();
