b5333f07d6bb2f78a5a8673b6a385381ddb15bc3
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-08-24 23:34:26 +0000

    src/e/edit/AddWorkspaceAction.java: src/e/edit/EditWorkspaceAction.java: src/e/edit/WorkspaceProperties.java: factor out the duplication between the implementations of "Add Workspace..." and "Edit Workspace...".

--- evergreen/trunk/src/e/edit/AddWorkspaceAction.java
+++ evergreen/trunk/src/e/edit/AddWorkspaceAction.java
@@ -2,13 +2,9 @@ package e.edit;
 
 import java.awt.event.*;
 import javax.swing.*;
-import e.forms.*;
-import e.gui.*;
-import e.util.*;
 
 /**
- * Opens a dialog where the user can specify a name and root directory for a new workspace,
- * which will then be created.
+ * Opens a dialog where the user can specify the details of a new workspace, which will then be created.
  */
 public class AddWorkspaceAction extends AbstractAction {
     public AddWorkspaceAction() {
@@ -16,24 +12,12 @@ public class AddWorkspaceAction extends AbstractAction {
     }
     
     public void actionPerformed(ActionEvent e) {
-        JTextField nameField = new JTextField("", 40);
+        WorkspaceProperties properties = new WorkspaceProperties();
+        properties.name = "";
+        properties.rootDirectory = null;
         
-        FilenameChooserField filenameChooserField = new FilenameChooserField(JFileChooser.DIRECTORIES_ONLY);
-        filenameChooserField.setCompanionNameField(nameField);
-        
-        FormBuilder form = new FormBuilder(Evergreen.getInstance().getFrame(), "Add Workspace");
-        FormPanel formPanel = form.getFormPanel();
-        formPanel.addRow("Root Directory:", filenameChooserField);
-        formPanel.addRow("Name:", nameField);
-        
-        while (form.show("Add")) {
-            String message = FileUtilities.checkDirectoryExistence(filenameChooserField.getPathname());
-            if (message != null) {
-                Evergreen.getInstance().showAlert(message, "The pathname you supply must exist, and must be a directory.");
-            } else {
-                Evergreen.getInstance().createWorkspace(nameField.getText(), filenameChooserField.getPathname());
-                return;
-            }
+        if (properties.showWorkspacePropertiesDialog("Add Workspace", "Add") == true) {
+            Evergreen.getInstance().createWorkspace(properties.name, properties.rootDirectory);
         }
     }
 }
