125716d140b1263209aa46e1c9a6b450b6f297be
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-08-24 00:04:58 +0000

    src/e/edit/Evergreen.java: Marsell P-K ("Tom") reminds me that I never meant to sort workspaces case-sensitively.

--- evergreen/trunk/src/e/edit/Evergreen.java
+++ evergreen/trunk/src/e/edit/Evergreen.java
@@ -407,7 +407,7 @@ public class Evergreen {
         int index = 0;
         for (int i = 0; i < tabbedPane.getTabCount(); i++) {
             String title = tabbedPane.getTitleAt(i);
-            if (name.compareTo(title) <= 0) {
+            if (name.compareToIgnoreCase(title) <= 0) {
                 break;
             }
             index++;
