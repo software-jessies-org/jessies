172a46858857bce60e2325e52f9e4f3d300fa23b
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-09-20 17:09:13 +0000

    src/e/edit/EColumn.java: remove the unnecessary @author and HTML formatting. (We need to get round to having AUTHORS files and finishing the "Credits" button in the about dialog.)

--- evergreen/trunk/src/e/edit/EColumn.java
+++ evergreen/trunk/src/e/edit/EColumn.java
@@ -11,12 +11,9 @@ import javax.swing.event.*;
 /**
  * A column containing views.
  * 
- * <font color="red">Note that you have you call addComponent and not add. I don't know why I
- * didn't override add and invoke super.add at the critical point; this seems like a serious design
- * error that someone should check out and fix. As things stand, accidentally invoking add will
- * cause a whole can of Whoop Ass to be spilt in our lap.</font>
- * 
- * @author Elliott Hughes <enh@acm.org>
+ * Note that you have you call addComponent and not add.
+ * I don't know why I didn't override add and invoke super.add at the critical point; this seems like a serious design error that someone should check out and fix.
+ * As things stand, accidentally invoking add will cause a whole can of Whoop Ass to be spilled in our lap.
  */
 public class EColumn extends JSplitPane {
     private static final int MIN_HEIGHT = ETitleBar.TITLE_HEIGHT + 5;
