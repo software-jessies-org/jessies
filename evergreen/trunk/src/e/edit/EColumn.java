3afa97c8b90a93ec4452947e20e60c2384d02128
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-09-22 00:47:12 +0000

    src/e/edit/EColumn.java: fix the warning now I again understand the code better.

--- evergreen/trunk/src/e/edit/EColumn.java
+++ evergreen/trunk/src/e/edit/EColumn.java
@@ -11,9 +11,7 @@ import javax.swing.event.*;
 /**
  * A column containing views.
  * 
- * Note that you have you call addComponent and not add.
- * I don't know why I didn't override add and invoke super.add at the critical point; this seems like a serious design error that someone should check out and fix.
- * As things stand, accidentally invoking add will cause a whole can of Whoop Ass to be spilled in our lap.
+ * Note that you must call addComponent and not add, because our second parameter isn't an index or an abstract location, it's a y-coordinate.
  */
 public class EColumn extends JPanel {
     private static final int MIN_HEIGHT = ETitleBar.TITLE_HEIGHT + 5;
