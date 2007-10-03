3f73f361e2d6e33dde73bf22e8f801a8e5655251
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-10-03 05:17:30 +0000

    bin/find-jdk-root.rb: bin/which.rb: inline "which.rb" into its only caller. There's also a Talc replacement for this.

--- salma-hayek/trunk/bin/find-jdk-root.rb
+++ salma-hayek/trunk/bin/find-jdk-root.rb
@@ -81,13 +81,28 @@
 #   available.)
 #
 
+# Looks for the first occurrence of program within path.
+# Returns nil if not found.
+# (From Ruby Facets' FileUtils, and hopefully part of Ruby at some point.)
+def which(program, path = ENV["PATH"])
+  path.split(File::PATH_SEPARATOR).each {
+    |directory|
+    file = File.join(directory, program)
+    # Avoid /usr/lib/ruby, for example
+    if File.executable?(file) && File.directory?(file) == false
+      return file
+    end
+  }
+  # This differs from the behavior of `which `, but it makes it easier to find errors, and it's the behavior of the Ruby Facets' FileUtils extension.
+  nil
+end
+
 def find_jdk_root()
   require "pathname.rb"
   
   # Load helper libraries, coping with symbolic links to this script.
   bin = Pathname.new(__FILE__).realpath().dirname()
   require "#{bin}/target-os.rb"
-  require "#{bin}/which.rb"
   
   # Kees Jongenburger says this is the way things are done on Gentoo.
   # It sounds like a per-user equivalent of /etc/alternatives.
