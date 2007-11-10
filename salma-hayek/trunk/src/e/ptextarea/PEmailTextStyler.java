155c8d4951911060e64452506bfd6054412a9ec4
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2007-11-10 18:56:32 +0000

    src/e/ptextarea/PEmailTextStyler.java: fix parochialisms (and then find that almost none of the comment was true any longer), remove a dead field, order fields into the traditional order, stop considering ":" and "|" to be quoting characters because I haven't seen anyone use those since 1997 and they do cause quoted Ruby source to be rather too colorful, remove STRING and KEYWORD as suitable quoting styles (COMMENT is okay for now), disable the signature detection until it's improved, and comment various other problems.

--- salma-hayek/trunk/src/e/ptextarea/PEmailTextStyler.java
+++ salma-hayek/trunk/src/e/ptextarea/PEmailTextStyler.java
@@ -5,41 +5,37 @@ import java.util.regex.*;
 import e.util.*;
 
 /**
- * A PEmailTextStyler styles the text of emails.  It understands how to
- * recognise signatures (which are coloured grey), and deals with quoted text
- * (in 3 repeating colours).
- * 
- * The signature finding code is a bit basic
- * 
+ * Styles the text of emails.
  * 
  * @author Phil Norman
  */
 public class PEmailTextStyler extends PAbstractTextStyler {
-    private int signatureLineIndex = Integer.MAX_VALUE;
-    private static final String SIGNATURE_START = "-- ";
-    private static final String QUOTING_CHARACTERS = ">:|";
-    
-    private static final PStyle[] QUOTE_STYLES = new PStyle[] {
-        PStyle.COMMENT, PStyle.STRING, PStyle.KEYWORD,
-    };
-    
-    private static final PStyle SIGNATURE_STYLE = PStyle.PREPROCESSOR;
+    private static final String QUOTING_CHARACTERS = ">";
+    private static final PStyle[] QUOTE_STYLES = new PStyle[] { PStyle.COMMENT };
     private static final Pattern SIGNATURE_PATTERN = Pattern.compile("\n-- \n");
+    
+    private int signatureLineIndex = Integer.MAX_VALUE;
     
     public PEmailTextStyler(PTextArea textArea) {
         super(textArea);
-        initTextListener();
         textArea.setTextStyler(this);
+        initTextListener();
         findSignatureLine();
     }
     
     private void findSignatureLine() {
+        // FIXME: few people use "-- " to introduce signatures any more.
+        // FIXME: some of those that do try actually use "--" instead.
+        // FIXME: this doesn't cope with mailing list digests, because it doesn't realize that a signature isn't necessarily the last thing in a mail.
+        // FIXME: see also the problem described in getTextSegments.
+        /*
         Matcher matcher = SIGNATURE_PATTERN.matcher(textArea.getTextBuffer());
         int newIndex = matcher.find() ? textArea.getLineOfOffset(matcher.start()) : Integer.MAX_VALUE;
         if (signatureLineIndex != newIndex) {
             signatureLineIndex = newIndex;
             textArea.repaint();
         }
+        */
     }
     
     private void initTextListener() {
@@ -71,6 +67,8 @@ public class PEmailTextStyler extends PAbstractTextStyler {
     }
     
     private int getQuoteLevel(int lineIndex, String line) {
+        // FIXME: this doesn't work for Outlook-style "-----Original Message-----"-quoted mail.
+        // FIXME: the attempt to give the "On ...:" line the same quote level as the corresponding quoted text doesn't work well with mail sent by Mail.app because it tends to leave an empty line between the "On ... :" line and the first quoted line.
         int result = getLeadingQuoteCharacterCount(line);
         if (line.endsWith(":") && (lineIndex < textArea.getLineCount() - 1)) {
             String lineBelow = textArea.getLineContents(lineIndex + 1).toString();
@@ -87,10 +85,12 @@ public class PEmailTextStyler extends PAbstractTextStyler {
         List<PLineSegment> result = new ArrayList<PLineSegment>();
         PStyle style = PStyle.NORMAL;
         if (lineIndex > signatureLineIndex) {
-            style = SIGNATURE_STYLE;
+            // FIXME: this is a bad idea because PREPROCESSOR is meaningful to PTextArea and in particular means that the URLs and email addresses people put in their signatures become unclickable.
+            // style = PStyle.PREPROCESSOR;
         } else {
             int quoteLevel = getQuoteLevel(lineIndex, line);
             if (quoteLevel > 0) {
+                // FIXME: this didn't work as intended because of an off-by-one error, but it doesn't matter while we only have one QUOTE_STYLE anyway.
                 style = QUOTE_STYLES[quoteLevel % QUOTE_STYLES.length];
             }
         }
