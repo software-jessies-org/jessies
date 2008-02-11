01ea76cd5207f3d06db3b2d4ce5f0a4dd1a34e05
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2008-02-11 06:31:22 +0000

    src/e/ptextarea/PIndenter.java: extract another sliver of functionality so subclasses can get the indentation part of an arbitrary string.
    src/e/ptextarea/PNewlineInserter.java: Vim's indentation code is written as if Vim itself has already copied the previous line's indentation. This does seem to make for a fairly comfortable style, shouldn't affect our existing PIndenter subclasses, and makes it easier to translate a Vim "indent file".
    
    src/e/ptextarea/PPythonIndenter.java: rewrite the Python indenter in the style of Vim's Python indenter. There's some ugly duplication here, but that gives strong clues as to the kind of facilities we might want to provide. The main functionality not mentioned here would be the searchpair function.

--- salma-hayek/trunk/src/e/ptextarea/PPythonIndenter.java
+++ salma-hayek/trunk/src/e/ptextarea/PPythonIndenter.java
@@ -4,77 +4,97 @@ import java.util.regex.*;
 
 /**
  * Basic Python auto-indenter.
- * 
- * There are several ways in which this could be cleverer:
- * 1. we know that "else" and "elsif" belong with an "if", and "except" and "finally" with a "try", and not just any old block.
- * 2. we could cope with split lines, where the ":" isn't on the the same line as the "class" or "def" or whatever.
  */
-public class PPythonIndenter extends  PSimpleIndenter {
-    private Pattern END_OF_BLOCK = Pattern.compile("^\\s*(break|continue|raise|return|pass)\\b.*");
-    private Pattern SAME_AS_BLOCK = Pattern.compile("^\\s*(else|elif|except|finally)\\b.*");
-    private Pattern START_OF_NEW_BLOCK = Pattern.compile("^\\s*(class|def|elif|else|except|if|for|while|try)\\b.*");
-    
+public class PPythonIndenter extends PSimpleIndenter {
     public PPythonIndenter(PTextArea textArea) {
         super(textArea);
     }
     
+    @Override
     public boolean isElectric(char c) {
-        return "abcdefghijklmnopqrstuvwxyz:".indexOf(c) != -1;
+        return ("abcdefghijklmnopqrstuvwxyz:".indexOf(c) != -1);
     }
     
     @Override
     protected String calculateNewIndentation(int lineNumber) {
+        // Python indenter, based on "python.vim".
+        // Most of the comments are recognizably similar to the Vim comments.
+        // Original Vim indenter author was David Bustos <bustos@caltech.edu>.
+        
+        // FIXME: handle \-continued lines.
+        
+        // FIXME: handle lines within multi-line strings.
+        
+        // Search backwards for the first non-empty line (called simply "previous line" later).
+        // If this is the first non-empty line, use no indentation.
         final int previousNonBlankLineNumber = getPreviousNonBlankLineNumber(lineNumber);
         if (previousNonBlankLineNumber == -1) {
             return "";
-        } else {
-            // A simplified variant of this code:
-            // http://developer.kde.org/documentation/library/3.4-api/kate/html/kateautoindent_8cpp-source.html
-            // (Extended to cope with Python classes and exceptions.)
-            int pos = textArea.getLineStartOffset(lineNumber);
-            int nestLevel = 0;
-            boolean levelFound = false;
-            int i = previousNonBlankLineNumber;
-            for (; i >= 0; --i) {
-                String line = textArea.getLineText(i);
-                if (START_OF_NEW_BLOCK.matcher(line).matches()) {
-                    if ((levelFound == false && nestLevel == 0) || (levelFound && nestLevel - 1 <= 0)) {
-                        pos = textArea.getLineStartOffset(i);
-                        break;
+        }
+        
+        // FIXME: handle unfinished parenthesized expressions (see also the end of this method).
+        
+        // Get the indentation of the previous line.
+        final String indentation = getCurrentIndentationOfLine(previousNonBlankLineNumber);
+        
+        // Get the previous line and remove any trailing comment.
+        // FIXME: use styler information.
+        String previousLine = textArea.getLineText(previousNonBlankLineNumber);
+        int commentIndex = previousLine.indexOf("#");
+        if (commentIndex != -1) {
+            previousLine = previousLine.substring(0, commentIndex);
+        }
+        final String currentLine = textArea.getLineText(lineNumber);
+        
+        // If the previous line ended with a colon, indent this line.
+        if (previousLine.trim().endsWith(":")) {
+            return increaseIndentation(indentation);
+        }
+        
+        // If the previous line was a stop-execution statement...
+        if (previousLine.matches("^\\s*(break|continue|raise|return|pass)\\b.*$")) {
+            // If the user has already reduced the indentation, trust them.
+            if (getCurrentIndentationOfLine(lineNumber).length() <= getCurrentIndentationOfLine(previousNonBlankLineNumber).length() - textArea.getIndentationString().length()) {
+                return getCurrentIndentationOfLine(lineNumber);
+            }
+            // They haven't, so do it for them.
+            return decreaseIndentation(indentation);
+        }
+        
+        // If the current line begins with a keyword that lines up with "try"...
+        if (currentLine.matches("^\\s*(except|finally)\\b.*$")) {
+            // Find the matching "try".
+            for (int tryLineNumber = lineNumber - 1; tryLineNumber >= 0; --tryLineNumber) {
+                String tryLine = textArea.getLineText(tryLineNumber);
+                if (tryLine.matches("^\\s*(try|except)\\b.*$")) {
+                    String tryIndentation = indentationOf(tryLine);
+                    if (tryIndentation.length() >= getCurrentIndentationOfLine(lineNumber).length()) {
+                        // Indentation is already less than this.
+                        return getCurrentIndentationOfLine(lineNumber);
                     }
-                    --nestLevel;
-                } else if (END_OF_BLOCK.matcher(line).matches()) {
-                    ++nestLevel;
-                    levelFound = true;
+                    // Line up with this try/except.
+                    return tryIndentation;
                 }
             }
-            if (i == -1) {
-                return "";
-            }
-            String indentation = getCurrentIndentationOfLine(previousNonBlankLineNumber);
-            String previousLine = textArea.getLineText(previousNonBlankLineNumber);
-            /*
-            System.err.println("previous non-blank line #" + previousNonBlankLineNumber + ":");
-            System.err.println(previousLine);
-            System.err.println("indentation:");
-            System.err.println(indentation + "|<");
-            System.err.println("previous block line #" + i + ":");
-            System.err.println(textArea.getLineText(i));
-            System.err.println("indentation:");
-            System.err.println(getCurrentIndentationOfLine(i) + "|<");
-            */
-            
-            String currentLine = textArea.getLineText(lineNumber);
-            if (SAME_AS_BLOCK.matcher(currentLine).matches()) {
-                indentation = decreaseIndentation(indentation);
-            } else if (SAME_AS_BLOCK.matcher(previousLine).matches()) {
-                indentation = increaseIndentation(indentation);
-            } else if (START_OF_NEW_BLOCK.matcher(previousLine).matches()) {
-                indentation = increaseIndentation(indentation);
-            } else if (END_OF_BLOCK.matcher(previousLine).matches()) {
-                indentation = decreaseIndentation(indentation);
-            }
-            return indentation;
+            // No matching try/except.
+            return getCurrentIndentationOfLine(lineNumber);
         }
+        
+        // If the current line begins with a header keyword, reduce the indentation.
+        if (currentLine.matches("^\\s*(elif|else)\\b.*$")) {
+            // Unless the previous line was a one-liner...
+            if (previousLine.matches("^\\s*(for|if|try)\\b.*$")) {
+                return indentation;
+            }
+            // ...or the user has already reduced the indentation.
+            if (getCurrentIndentationOfLine(lineNumber).length() <= getCurrentIndentationOfLine(previousNonBlankLineNumber).length() - textArea.getIndentationString().length()) {
+                return getCurrentIndentationOfLine(lineNumber);
+            }
+            return decreaseIndentation(indentation);
+        }
+        
+        // FIXME: Vim has a final special case here for the conclusion of a multi-line parenthesized expression.
+        
+        return getCurrentIndentationOfLine(lineNumber);
     }
 }
