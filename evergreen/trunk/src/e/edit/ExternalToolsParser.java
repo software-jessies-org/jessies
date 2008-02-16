577ef2b03acc324994c9bdeabefcbbee18d4e580
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2008-02-16 21:37:45 +0000

    src/e/edit/ExternalToolsParser.java: add support for arbitrary (non-stock) icons, and factor out the keyboard-equivalent and icon code into their own methods.

--- evergreen/trunk/src/e/edit/ExternalToolsParser.java
+++ evergreen/trunk/src/e/edit/ExternalToolsParser.java
@@ -76,36 +76,50 @@ public abstract class ExternalToolsParser {
             action.setNeedsFile(Parameters.getParameter(prefix + "needsFile", needsFile));
             action.setRequestsConfirmation(Parameters.getParameter(prefix + "requestConfirmation", false));
             
-            String keyboardEquivalent = Parameters.getParameter(prefix + "keyboardEquivalent", null);
-            if (keyboardEquivalent != null) {
-                keyboardEquivalent = keyboardEquivalent.trim().toUpperCase();
-                // For now, the heuristic is that special keys (mainly just the function keys) have names and don't want any extra modifiers.
-                // Simple letter keys, to keep from colliding with built-in keystrokes, automatically get the platform default modifier plus shift.
-                // Neither part of this is really right.
-                // For one thing, you might well want to have f1, shift-f1, and so on all do different (though probably related) things.
-                // For another, we already use various of the "safe" combinations for built-in functionality; "Find in Files", for example.
-                // I can't remember what's wrong with KeyStroke.getKeyStroke(String); I believe the problems were:
-                // 1. complex case sensitivity for key names.
-                // 2. no support for the idea of a platform-default modifier.
-                // 3. users can get themselves into trouble with pressed/released/typed.
-                // In the long run, though, we're going to want to implement our own replacement for that, or a pre-processor.
-                int modifiers = 0;
-                if (keyboardEquivalent.length() == 1) {
-                    modifiers = GuiUtilities.getDefaultKeyStrokeModifier() | InputEvent.SHIFT_MASK;
-                }
-                
-                KeyStroke keyStroke = GuiUtilities.makeKeyStrokeWithModifiers(modifiers, keyboardEquivalent);
-                if (keyStroke != null) {
-                    action.putValue(Action.ACCELERATOR_KEY, keyStroke);
-                }
-            }
-            
-            String stockIcon = Parameters.getParameter(prefix + "stockIcon", null);
-            if (stockIcon != null) {
-                GnomeStockIcon.useStockIcon(action, stockIcon);
-            }
+            configureKeyboardEquivalent(action, prefix);
+            configureIcon(action, prefix);
             
             addItem(action);
+        }
+    }
+    
+    private void configureKeyboardEquivalent(Action action, String prefix) {
+        String keyboardEquivalent = Parameters.getParameter(prefix + "keyboardEquivalent", null);
+        if (keyboardEquivalent != null) {
+            keyboardEquivalent = keyboardEquivalent.trim().toUpperCase();
+            // For now, the heuristic is that special keys (mainly just the function keys) have names and don't want any extra modifiers.
+            // Simple letter keys, to keep from colliding with built-in keystrokes, automatically get the platform default modifier plus shift.
+            // Neither part of this is really right.
+            // For one thing, you might well want to have f1, shift-f1, and so on all do different (though probably related) things.
+            // For another, we already use various of the "safe" combinations for built-in functionality; "Find in Files", for example.
+            // I can't remember what's wrong with KeyStroke.getKeyStroke(String); I believe the problems were:
+            // 1. complex case sensitivity for key names.
+            // 2. no support for the idea of a platform-default modifier.
+            // 3. users can get themselves into trouble with pressed/released/typed.
+            // In the long run, though, we're going to want to implement our own replacement for that, or a pre-processor.
+            int modifiers = 0;
+            if (keyboardEquivalent.length() == 1) {
+                modifiers = GuiUtilities.getDefaultKeyStrokeModifier() | InputEvent.SHIFT_MASK;
+            }
+            
+            KeyStroke keyStroke = GuiUtilities.makeKeyStrokeWithModifiers(modifiers, keyboardEquivalent);
+            if (keyStroke != null) {
+                action.putValue(Action.ACCELERATOR_KEY, keyStroke);
+            }
+        }
+    }
+    
+    private void configureIcon(Action action, String prefix) {
+        // Allow users to specify a GNOME stock icon, and/or the pathname to an icon.
+        String stockIcon = Parameters.getParameter(prefix + "stockIcon", null);
+        if (stockIcon != null) {
+            GnomeStockIcon.useStockIcon(action, stockIcon);
+        } else {
+            String icon = Parameters.getParameter(prefix + "icon", null);
+            if (icon != null) {
+                // We trust the user to specify an appropriately-sized icon.
+                action.putValue(Action.SMALL_ICON, new ImageIcon(icon));
+            }
         }
     }
 }
