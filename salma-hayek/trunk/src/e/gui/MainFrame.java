43c6589bf37e3d7714624e54e377283e2dd55c75
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2008-02-27 04:45:50 +0000

    src/e/gui/AboutBox.java: src/e/gui/JFrameUtilities.java: src/e/gui/MainFrame.java: mad points out that an old change of mine re-introduced a "VM doesn't exit" bug. Fix JFrameUtilities.makeSimpleWindow to use our major idiom for windows where we don't want to do anything clever (we just want them to go away when closed, however they're closed) and don't want to make assumptions about how they'll be used (which you're doing when you use DISPOSE_ON_CLOSE or EXIT_ON_CLOSE) because we hand the resulting JFrame out to arbitrary application code to do with as it sees fit. Also document in MainFrame why this is the best idiom for making windows dispose themselves (assuming you don't have anything more complicated to do).

--- salma-hayek/trunk/src/e/gui/MainFrame.java
+++ salma-hayek/trunk/src/e/gui/MainFrame.java
@@ -3,6 +3,20 @@ package e.gui;
 import e.util.*;
 import javax.swing.*;
 
+/**
+ * Handles the simple case of an application with a single main window.
+ * It's surprisingly awkward to ensure that native resources are freed up in
+ * all cases, and if they're not, the VM won't exit (which means that users
+ * who try to quit will find your application hanging around on their system,
+ * invisible but still running, which may cause future instances of your
+ * application trouble if they run a server, say).
+ * 
+ * This seems to be the simplest implementation, even if it is rather ugly.
+ * 
+ * This class also does various things to make your application look more
+ * native: ensuring it's sensibly sized and positioned, using the application's
+ * icon, and so forth.
+ */
 public class MainFrame extends JFrame {
     public MainFrame() {
         this("");
@@ -10,6 +24,9 @@ public class MainFrame extends JFrame {
     
     public MainFrame(String title) {
         super(title);
+        // EXIT_ON_CLOSE is simple and effective, but it's unfriendly, especially if you become embedded.
+        // DISPOSE_ON_CLOSE is tempting, but doesn't cover setVisible(false).
+        // HIDE_ON_CLOSE and then manually doing a dispose in setVisible covers everything.
         setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
     }
     
