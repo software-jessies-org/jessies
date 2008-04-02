542ab5f5e1be0e4fd63a67555f1fd58df1f6bab7
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2008-04-02 06:27:30 +0000

    src/e/ptextarea/PNewCppIndenter.java: a first rough draft of a new C++ indenter. Not ready for general use, but everyone's welcome to try it out.

---
+++ salma-hayek/trunk/src/e/ptextarea/PNewCppIndenter.java
@@ -0,0 +1,100 @@
+package e.ptextarea;
+
+import java.util.regex.*;
+
+/**
+ * Basic C++ auto-indenter.
+ * 
+ * This is still under development, and even when finished won't necessarily
+ * guarantee compatibility with the old C++ indenter.
+ * 
+ * Major goals relative to the old C++ indenter (in no meaningful order):
+ * 
+ * 1. ability to (optionally) not indent "namespace".
+ * 2. ability to cope with wrapped lines.
+ * 3. ability to cope with continued lines (for hairy macros).
+ * 4. ability to cope with if/else blocks without braces.
+ * 5. ability to (optionally) apply a different amount of indentation to
+ *    "public:" "protected:" and "private:".
+ * 6. ability to support both common styles of indenting switch statements,
+ *    and possibly also backwards-compatible support for our old idiosyncratic
+ *    mess.
+ */
+public class PNewCppIndenter extends PSimpleIndenter {
+    public PNewCppIndenter(PTextArea textArea) {
+        super(textArea);
+    }
+    
+    /**
+     * Which characters cause a re-indent of the current line.
+     * At the moment, very few.
+     */
+    @Override
+    public boolean isElectric(char c) {
+        return (c == '{' || c == '}' || c == '#');
+    }
+    
+    /**
+     * Returns the line number of the first line before 'startLineNumber' that
+     * contains content relevant for indentation. Preprocessor directives and
+     * comments don't count.
+     */
+    private int getPreviousInterestingLineNumber(int startLineNumber) {
+        for (int lineNumber = startLineNumber - 1; lineNumber >= 0; --lineNumber) {
+            String activePart = getActivePartOfLine(lineNumber);
+            // FIXME: why is this even included by getActivePartOfLine? isn't that a bug? any why doesn't it include, say, PStyle.STRING? surely that's a bug?
+            if (activePart.startsWith("#")) {
+                continue;
+            }
+            if (activePart.length() > 0) {
+                return lineNumber;
+            }
+        }
+        return -1;
+    }
+    
+    @Override
+    protected String calculateNewIndentation(int lineNumber) {
+        // Preprocessor directives live in column 0.
+        final String currentLine = textArea.getLineText(lineNumber);
+        final String activePartOfCurrent = getActivePartOfLine(lineNumber);
+        if (activePartOfCurrent.startsWith("#")) {
+            return "";
+        }
+        
+        // Search backwards for the first interesting line (called simply
+        // "previous line" later, on the assumption that the intervening lines
+        // really are uninteresting).
+        final int previousLineNumber = getPreviousInterestingLineNumber(lineNumber);
+        if (previousLineNumber == -1) {
+            return "";
+        }
+        
+        // Get the indentation of the previous line.
+        String indentation = getCurrentIndentationOfLine(previousLineNumber);
+        
+        // Get the previous line, and the non-comment part of the previous line.
+        final String previousLine = textArea.getLineText(previousLineNumber);
+        final String activePartOfPrevious = getActivePartOfLine(previousLineNumber);
+        
+        //System.err.println("'" + activePartOfPrevious + "'; indentation '" + indentation + "'");
+        
+        // Handle simple cases.
+        if (activePartOfPrevious.trim().endsWith("{") && activePartOfPrevious.matches("^namespace\\b.*") == false) {
+            indentation = increaseIndentation(indentation);
+        } else if (activePartOfCurrent.trim().endsWith("}")) {
+            indentation = decreaseIndentation(indentation);
+        }
+        
+        return indentation + extraBlockCommentArtForLine(lineNumber);
+    }
+    
+    /**
+     * Whether PNewlineInserter should also add a trailing ';' if it inserts a
+     * closing '}' for the given line.
+     */
+    @Override
+    public boolean isInNeedOfClosingSemicolon(String line) {
+        return line.matches(".*\\b(class|enum|struct|union)\\b.*");
+    }
+}
