c825d91a8137eb90e33493f4c0a7bebb856700fa
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2008-05-27 03:25:04 +0000

    native/all/ruby-launcher/ruby-launcher.cpp: James Lowe reports that his desktop shortcut doesn't work and was kind enough to prove that it was ruby-launcher that was silently failing.  We can't tell why, but at least we might be able to after this change.  Clearly, I've been spoiled by Ruby, as even linking the previous code with the console subsystem produces nothing more informative than a coredump.  We need to handle the exception and, more particularly, we need to inform the user of its message via a MessageBox.  Hmm... don't we have code for that somewhere?
    native/all/java-launcher/java-launcher.cpp: Yes we do, including an Apple variant, whose output I haven't ever had the misfortune to see.  I'll have to provoke that shortly, as ...
    
    native/Headers/reportFatalErrorViaGui.h: ... even three lines of Objective-C++ is enough to baffle me.  The message we report on Windows for Terminator failures takes a turn for what will presumably be regarded as "the worse" under this change (the part about installing a decent Java now comes after the Ctrl-C hint).  No biggie, methinks.
    
    Outputting to std::cerr doesn't seem to cause us problems when started from a Windows shortcut, even when reporting a failure before ensuring that file descriptor 2 is open but, still, I'll do that after the MessageBox, just in case.
    
    I don't exit(1) here, though I was tempted, because the java-launcher code didn't.

---
+++ salma-hayek/trunk/native/Headers/reportFatalErrorViaGui.h
@@ -0,0 +1,37 @@
+#ifndef REPORT_FATAL_ERROR_VIA_GUI_H_included
+#define REPORT_FATAL_ERROR_VIA_GUI_H_included
+
+#ifdef __APPLE__
+#include "ScopedAutoReleasePool.h"
+#include <Cocoa/Cocoa.h>
+#endif
+
+#ifdef __CYGWIN__
+#include <windows.h>
+#endif
+
+#include <iostream>
+#include <sstream>
+
+void reportFatalErrorViaGui(const std::string& programName, const std::string& applicationMessage) {
+    std::ostringstream os;
+#if defined(__CYGWIN__)
+    os << "Please copy this message to the clipboard with Ctrl-C and mail it to software@jessies.org.";
+    os << std::endl;
+    os << "(Windows won't let you select the text but Ctrl-C works anyway.)";
+    os << std::endl;
+    os << std::endl;
+#endif
+    os << applicationMessage;
+    std::string platformMessage(os.str());
+#if defined(__CYGWIN__)
+    MessageBox(GetActiveWindow(), platformMessage.c_str(), programName.c_str(), MB_OK);
+#elif defined(__APPLE__)
+    ScopedAutoReleasePool pool;
+    [NSApplication sharedApplication];
+    NSRunInformationalAlertPanel([NSString stringWithUTF8String:programName.c_str()], [NSString stringWithUTF8String:platformMessage.c_str()], nil, nil, nil);
+#endif
+    std::cerr << platformMessage;
+}
+
+#endif
