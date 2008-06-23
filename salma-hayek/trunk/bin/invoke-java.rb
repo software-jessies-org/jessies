5beebd4ea060d13f13708fd0a526ec6628f92582
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2008-06-23 23:40:04 +0000

    bin/invoke-java.rb: The code to act on RUBY_LAUNCHER_INVOKING can never have worked.  The only reason it can have given the impression of working is that in the in-app client hasn't been working for Windows for ages.
    bin/show-alert.rb: If I put it here, then it does work, although this code isn't anything to do with reporting exceptions.  "launch" was a better name for its home.  "launch" and "launch0" give the impression that the intention was for eg Terminator to sub-class invoke-java.rb's Java class.  Perhaps that would be the cleaner solution, though I'm not a fan of derivation except for interface implementation.

--- salma-hayek/trunk/bin/invoke-java.rb
+++ salma-hayek/trunk/bin/invoke-java.rb
@@ -335,18 +335,7 @@ class Java
   end
   
   def launch(app_name)
-    report_exceptions(app_name) {
-      # Our Windows desktop shortcuts get started from Cygwin's /bin directory.
-      # We want to behave as if started from the invoking user's home directory.
-      if ENV["RUBY_LAUNCHER_INVOKING"]
-        ENV["RUBY_LAUNCHER_INVOKING"] = nil
-        Dir.chdir() {
-          launch0()
-        }
-      else
-        launch0()
-      end
-    }
+    report_exceptions(app_name) { launch0() }
   end
   
   def launch0()
