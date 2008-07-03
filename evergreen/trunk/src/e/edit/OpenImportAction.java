3739a436ef9b885da4330ec2a2d113bd0c59026c
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2008-07-03 06:56:52 +0000

    src/e/edit/OpenImportAction.java: make "Open Import" able to cope with a selected line as well as just a selected header name. Add the equivalent for Java, too. Enhance the file-finding code so that we interpret non-absolute paths as being rooted at the current workspace's root. Show the path used if we fail to find anything, to give the user more of a clue about what's wrong.

--- evergreen/trunk/src/e/edit/OpenImportAction.java
+++ evergreen/trunk/src/e/edit/OpenImportAction.java
@@ -1,8 +1,11 @@
 package e.edit;
 
 import e.gui.*;
+import e.ptextarea.*;
 import e.util.*;
 import java.awt.event.*;
+import java.io.*;
+import java.util.*;
 
 /**
  * Opens the file corresponding to the selected import.
@@ -17,27 +20,49 @@ public class OpenImportAction extends ETextAction {
     }
     
     public void actionPerformed(ActionEvent e) {
+        final Evergreen editor = Evergreen.getInstance();
+        final FileType fileType = getFocusedTextWindow().getFileType();
+        
         // FIXME: if there's no selection, take the whole of the current line.
         String path = getSelectedText().trim();
-        // FIXME: chop off boilerplate like "#include <...>" or "import ...;", and rewrite '.' in Java (but not C++!) as File.separatorChar.
+        
+        // Rewrite boilerplate to get a path fragment.
+        if (fileType == FileType.C_PLUS_PLUS) {
+            // Get rid of "#include <...>" in C++.
+            path = path.replaceAll("^\\s*#\\s*include\\s+[<\"]([^>\"]+)[\">].*", "$1");
+        } else if (fileType == FileType.JAVA) {
+            // Get rid of "import ...;", rewrite '.' as File.separatorChar, and append ".java".
+            path = path.replaceAll("^\\s*import\\s+(.+)\\s*;.*", "$1");
+            path = path.replace('.', '/') + ".java";
+        }
+        
         if (path.startsWith("~") || path.startsWith("/")) {
             // If we have an absolute name, we can go straight there.
             // The user probably meant to use "Open Quickly", but there's no need to punish them.
-            Evergreen.getInstance().openFile(path);
-        } else {
-            // FIXME: use per-language import paths. This temporarily hard-coded one obviously only applies to C++.
-            // FIXME: allow these to be overridden.
-            // FIXME: allow path elements that can refer to EDIT_WORKSPACE_ROOT, as in something like "$(EDIT_WORKSPACE_ROOT)/../READONLY/google3/".
-            String[] importPath = { "/usr/include/", "/usr/include/c++/4.2/" };
-            // FIXME: what if there are multiple matches? does that ever happen in real life?
-            for (String importDir : importPath) {
-                if (FileUtilities.exists(importDir, path)) {
-                    Evergreen.getInstance().openFile(importDir + java.io.File.separator + path);
-                    return;
-                }
-            }
-            // FIXME: show the path.
-            Evergreen.getInstance().showAlert("Couldn't open imported file", "There was no file \"" + path + "\" under any of the directories on the import path.");
+            editor.openFile(path);
+            return;
         }
+        
+        // FIXME: use per-language import paths. This temporarily hard-coded one obviously only applies to C++.
+        // FIXME: allow the path(s) to be overridden.
+        // FIXME: we should probably allow "." and interpret it as "the directory containing the current file". maybe just implicitly always check there first?
+        List<String> importPath = Arrays.asList("/usr/include/:/usr/include/c++/4.2/:native/Headers/".split(":"));
+        for (String importDir : importPath) {
+            File file;
+            if (importDir.startsWith("/")) {
+                file = FileUtilities.fileFromParentAndString(importDir, path);
+            } else {
+                // We interpret non-absolute paths as being rooted at the current workspace's root.
+                String root = editor.getCurrentWorkspace().getCanonicalRootDirectory();
+                file = new File(FileUtilities.fileFromParentAndString(root, importDir), path);
+            }
+            
+            if (file.exists()) {
+                // Even if there would be multiple matches, we just take the first one we come across.
+                editor.openFile(file.toString());
+                return;
+            }
+        }
+        editor.showAlert("Couldn't open imported file", "There was no file \"" + path + "\" under any of the directories on the import path:\n" + StringUtilities.join(importPath, ":"));
     }
 }
