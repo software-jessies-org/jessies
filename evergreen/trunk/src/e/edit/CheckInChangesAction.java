905dc1d2dc0f5b4a2eb46910f0bf7bffc2810291
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2008-08-05 06:15:26 +0000

    src/e/edit/CheckInChangesAction.java: src/e/edit/ShowHistoryAction.java: mnemonics for the "SCM" menu.

--- evergreen/trunk/src/e/edit/CheckInChangesAction.java
+++ evergreen/trunk/src/e/edit/CheckInChangesAction.java
@@ -7,7 +7,7 @@ import e.util.*;
  */
 public class CheckInChangesAction extends ExternalToolAction {
     public CheckInChangesAction() {
-        super("Check in Changes...", ToolInputDisposition.NO_INPUT, ToolOutputDisposition.ERRORS_WINDOW, "cd $EDIT_CURRENT_DIRECTORY ; SCM_EDITOR=" + FileUtilities.findScriptFromBundle("evergreen", "org.jessies.Evergreen") + " " + FileUtilities.findScriptFromBundle("checkintool", "org.jessies.SCM"));
+        super("Check in _Changes...", ToolInputDisposition.NO_INPUT, ToolOutputDisposition.ERRORS_WINDOW, "cd $EDIT_CURRENT_DIRECTORY ; SCM_EDITOR=" + FileUtilities.findScriptFromBundle("evergreen", "org.jessies.Evergreen") + " " + FileUtilities.findScriptFromBundle("checkintool", "org.jessies.SCM"));
         setChecksEverythingSaved(true);
     }
 }
--- evergreen/trunk/src/e/edit/ShowHistoryAction.java
+++ evergreen/trunk/src/e/edit/ShowHistoryAction.java
@@ -7,7 +7,7 @@ import e.util.*;
  */
 public class ShowHistoryAction extends ExternalToolAction {
     public ShowHistoryAction() {
-        super("Show History...", ToolInputDisposition.NO_INPUT, ToolOutputDisposition.ERRORS_WINDOW, FileUtilities.findScriptFromBundle("revisiontool", "org.jessies.SCM") + " $EDIT_CURRENT_FILENAME:$EDIT_CURRENT_LINE_NUMBER");
+        super("Show _History...", ToolInputDisposition.NO_INPUT, ToolOutputDisposition.ERRORS_WINDOW, FileUtilities.findScriptFromBundle("revisiontool", "org.jessies.SCM") + " $EDIT_CURRENT_FILENAME:$EDIT_CURRENT_LINE_NUMBER");
         putValue(ACCELERATOR_KEY, e.util.GuiUtilities.makeKeyStroke("H", true));
         setNeedsFile(true);
     }
