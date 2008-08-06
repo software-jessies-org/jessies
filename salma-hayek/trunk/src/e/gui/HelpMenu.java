cf01de865943aeea890f3547168e7012ed663290
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2008-08-06 05:23:29 +0000

    src/e/gui/HelpMenu.java: add support for a "Help" menu link to the manual.

--- salma-hayek/trunk/src/e/gui/HelpMenu.java
+++ salma-hayek/trunk/src/e/gui/HelpMenu.java
@@ -14,12 +14,16 @@ public class HelpMenu {
     public JMenu makeJMenu() {
         JMenu menu = new JMenu("Help");
         
-        // We don't support this yet, because we've got nothing to point it to.
-        //menu.add(new PlaceholderAction(Log.getApplicationName() + " Help"));
-        //menu.addSeparator();
-        
         String webSiteAddress = AboutBox.getSharedInstance().getWebSiteAddress();
         if (webSiteAddress != null) {
+            if (Boolean.getBoolean("e.gui.HelpMenu.hasManual")) {
+                // FIXME: GNOME usually says "Contents", but also usually implies there's a local copy.
+                Action manualAction = new WebLinkAction("View " + Log.getApplicationName() + " _Manual", webSiteAddress + "manual.html");
+                GnomeStockIcon.useStockIcon(manualAction, "gtk-help");
+                menu.add(manualAction);
+                menu.addSeparator();
+            }
+            // FIXME: how useful are the non-FAQ links, especially since you can get to them from the FAQ?
             menu.add(new WebLinkAction("View " + Log.getApplicationName() + " _Web Page", webSiteAddress));
             menu.add(new WebLinkAction("View " + Log.getApplicationName() + " _Change Log", webSiteAddress + "ChangeLog.html"));
             menu.add(new WebLinkAction("View " + Log.getApplicationName() + " _FAQ", webSiteAddress + "faq.html"));
@@ -27,7 +31,7 @@ public class HelpMenu {
         }
         
         menu.add(DebugMenu.makeJMenu());
-        // FIXME: anyone else using HelpMenu is going to want some control over this. "bk sendbug" and Safari's "Report Bugs to Apple..." are both good role models.
+        // FIXME: anyone else using HelpMenu is going to want some control over this; add a system property, and set a default in invoke-java?
         menu.add(new WebLinkAction("Report a Problem", "mailto:software@jessies.org?subject=" + AboutBox.getSharedInstance().getProblemReportSubject()));
         // We don't support this yet, because we've got nothing to point it to.
         menu.add(new PlaceholderAction("View Bugs List"));
