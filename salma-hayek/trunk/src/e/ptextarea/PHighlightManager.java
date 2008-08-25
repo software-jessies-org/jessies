fb11a01935d7a5df184a5671c436a64ff86cfb30
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2008-08-25 00:07:30 +0000

    src/e/ptextarea/PHighlight.java: make PHighlight comparable and trivially instantiable so we can...
    src/e/ptextarea/PHighlightManager.java: ...remove Wrapper and reimplement ProbeWrapper as a subclass of PHighlight. This saves us a 24-byte object per spelling mistake, which saves about 10MiB in my test case. I've also switched to using bulk insert in getHighlightsOverlapping, though the existing code was probably no less efficient.

--- salma-hayek/trunk/src/e/ptextarea/PHighlightManager.java
+++ salma-hayek/trunk/src/e/ptextarea/PHighlightManager.java
@@ -3,7 +3,7 @@ package e.ptextarea;
 import java.util.*;
 
 public class PHighlightManager {
-    private Map<String, HighlightSet> highlighterSets = new LinkedHashMap<String, HighlightSet>();
+    private final Map<String, HighlightSet> highlighterSets = new LinkedHashMap<String, HighlightSet>();
     
     public synchronized int countHighlightsOfType(String highlighterName) {
         HighlightSet set = highlighterSets.get(highlighterName);
@@ -57,14 +57,14 @@ public class PHighlightManager {
     }
     
     private static class HighlightSet {
-        private TreeSet<Wrapper> highlights = new TreeSet<Wrapper>();
+        private TreeSet<PHighlight> highlights = new TreeSet<PHighlight>();
         
         private void add(PHighlight highlight) {
-            highlights.add(new Wrapper(highlight));
+            highlights.add(highlight);
         }
         
         private void remove(PHighlight highlight) {
-            highlights.remove(new Wrapper(highlight));
+            highlights.remove(highlight);
         }
         
         private int size() {
@@ -72,72 +72,52 @@ public class PHighlightManager {
         }
         
         private PHighlight getHighlightAfter(int offset) {
-            SortedSet<Wrapper> after = highlights.tailSet(new ProbeWrapper(offset));
-            return (after.size() == 0) ? null : after.first().get();
+            SortedSet<PHighlight> after = highlights.tailSet(new ProbeHighlight(offset));
+            return (after.size() == 0) ? null : after.first();
         }
         
         private PHighlight getHighlightBefore(int offset) {
-            SortedSet<Wrapper> before = highlights.headSet(new ProbeWrapper(offset));
-            return (before.size() == 0) ? null : before.last().get();
+            SortedSet<PHighlight> before = highlights.headSet(new ProbeHighlight(offset));
+            return (before.size() == 0) ? null : before.last();
         }
         
         private List<PHighlight> getHighlightsOverlapping(int beginOffset, int endOffset) {
             // The 'firstItem' is to be the lowest-indexed highlight wrapper which *overlaps* the range.
             // We must check highlights which start before beginOffset to determine if they end past beginOffset.
-            Wrapper firstItem = new ProbeWrapper(beginOffset);
-            SortedSet<Wrapper> highlightsBeforeStart = highlights.headSet(firstItem);
+            PHighlight firstItem = new ProbeHighlight(beginOffset);
+            SortedSet<PHighlight> highlightsBeforeStart = highlights.headSet(firstItem);
             if (highlightsBeforeStart.size() > 0) {
-                Wrapper lastBefore = highlightsBeforeStart.last();
-                if (lastBefore.get().getEndIndex() > beginOffset) {
+                PHighlight lastBefore = highlightsBeforeStart.last();
+                if (lastBefore.getEndIndex() > beginOffset) {
                     firstItem = lastBefore;
                 }
             }
             
             // Now we have the start, and the end too, so we can simply grab the subset between these two extremes (inclusive of firstItem) and return as a list.
-            SortedSet<Wrapper> highlightsInRange = highlights.subSet(firstItem, new ProbeWrapper(endOffset));
-            List<PHighlight> result = new ArrayList<PHighlight>(highlightsInRange.size());
-            for (Wrapper wrapper : highlightsInRange) {
-                result.add(wrapper.get());
-            }
+            SortedSet<PHighlight> highlightsInRange = highlights.subSet(firstItem, new ProbeHighlight(endOffset));
+            List<PHighlight> result = new ArrayList<PHighlight>(highlightsInRange);
             return result;
         }
     }
     
-    private static class Wrapper implements Comparable<Wrapper> {
-        private PHighlight highlight;
+    private static final class ProbeHighlight extends PHighlight {
+        private final int fixedOffset;
         
-        private Wrapper(PHighlight highlight) {
-            this.highlight = highlight;
-        }
-        
-        public boolean equals(Object obj) {
-            return (obj instanceof Wrapper) && (((Wrapper) obj).getOffset() == getOffset());
-        }
-        
-        public int compareTo(Wrapper other) {
-            return getOffset() - other.getOffset();
-        }
-        
-        protected int getOffset() {
-            return highlight.getStartIndex();
-        }
-        
-        private PHighlight get() {
-            return highlight;
-        }
-    }
-    
-    private static class ProbeWrapper extends Wrapper {
-        private int fixedOffset;
-        
-        private ProbeWrapper(int fixedOffset) {
-            super(null);
+        private ProbeHighlight(int fixedOffset) {
+            super();
             this.fixedOffset = fixedOffset;
         }
         
-        @Override
-        protected int getOffset() {
+        @Override public int getStartIndex() {
             return fixedOffset;
+        }
+        
+        public String getHighlighterName() {
+            throw new UnsupportedOperationException();
+        }
+        
+        protected void paintHighlight(java.awt.Graphics2D g, PCoordinates start, PCoordinates end, java.awt.Insets insets, int lineHeight, int firstLineIndex, int lastLineIndex) {
+            throw new UnsupportedOperationException();
         }
     }
 }
