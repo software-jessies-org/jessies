25a0ae932f921ade468889ebb158977e47aa9563
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2008-09-02 03:03:43 +0000

    src/e/edit/EColumn.java: there's only one caller of GraphicsUtilities.paintPaper, and only one method in GraphicsUtilities. Let's not pretend it's ever likely to be used anywhere else.

--- evergreen/trunk/src/e/edit/EColumn.java
+++ evergreen/trunk/src/e/edit/EColumn.java
@@ -74,9 +74,35 @@ public class EColumn extends JPanel {
     @Override
     public void paint(Graphics g) {
         if (getComponentCount() == 0) {
-            GraphicsUtilities.paintPaper(g, 240);
+            paintBackground(g, 240);
         } else {
             super.paint(g);
+        }
+    }
+    
+    /**
+     * Paints the background using a paper effect similar to that in early Mac OS X. The
+     * first line in the pattern is brightest, followed by one medium gray, one dark
+     * gray, another medium then repeat until fade. There's no kind of ripple to the
+     * lines. We're not going out of our way to be slow, so we stick to horizontal
+     * line drawing only!
+     */
+    private void paintBackground(Graphics g, int brightestGray) {
+        final int grayStep = 10; // Ed had 5, but 10 is more like Mac OS X, which was our model.
+        final Rectangle clipRect = g.getClipBounds();
+        final int startY = clipRect.y / 4 * 4 - 4;
+        for (int y = startY; y < clipRect.y + clipRect.height; y+=4) {
+            g.setColor(new Color(brightestGray, brightestGray, brightestGray));
+            g.drawLine(clipRect.x, y, clipRect.x + clipRect.width, y);
+            
+            int middleGray = brightestGray - grayStep;
+            g.setColor(new Color(middleGray, middleGray, middleGray));
+            g.drawLine(clipRect.x, y + 1, clipRect.x + clipRect.width, y + 1);
+            g.drawLine(clipRect.x, y + 3, clipRect.x + clipRect.width, y + 3);
+            
+            int darkestGray = middleGray - grayStep;
+            g.setColor(new Color(darkestGray, darkestGray, darkestGray));
+            g.drawLine(clipRect.x, y + 2, clipRect.x + clipRect.width, y + 2);
         }
     }
     
