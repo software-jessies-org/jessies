8b50c3cb29a85d5e0695b58bce20345ae3ba0008
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2008-09-18 05:08:34 +0000

    src/e/util/Rewriter.java: test/e/util/RewriterTest.java: convert the Rewriter tests.

--- salma-hayek/trunk/src/e/util/Rewriter.java
+++ salma-hayek/trunk/src/e/util/Rewriter.java
@@ -1,6 +1,7 @@
 package e.util;
 
 import java.util.regex.*;
+import org.jessies.test.*;
 
 /**
  * A rewriter does a global substitution in the strings passed to its
@@ -66,5 +67,48 @@ public abstract class Rewriter {
         }
         matcher.appendTail(destination);
         return destination;
+    }
+    
+    @Test private static void testInches() {
+        // Rewrite an ancient unit of length in SI units.
+        String result = new Rewriter("([0-9]+(\\.[0-9]+)?)[- ]?(inch(es)?)") {
+            public String replacement() {
+                float inches = Float.parseFloat(group(1));
+                return Float.toString(2.54f * inches) + " cm";
+            }
+        }.rewrite("a 17 inch display");
+        Assert.equals(result, "a 43.18 cm display");
+    }
+    
+    @Test private static void testAlmanac() {
+        // The "Searching and Replacing with Non-Constant Values Using a
+        // Regular Expression" example from the Java Almanac.
+        String result = new Rewriter("([a-zA-Z]+[0-9]+)") {
+            public String replacement() {
+                return group(1).toUpperCase();
+            }
+        }.rewrite("ab12 cd efg34");
+        Assert.equals(result, "AB12 cd EFG34");
+    }
+    
+    @Test private static void testDollars() {
+        String result = new Rewriter("([0-9]+) US cents") {
+            public String replacement() {
+                long dollars = Long.parseLong(group(1))/100;
+                return "$" + dollars;
+            }
+        }.rewrite("5000 US cents");
+        Assert.equals(result, "$50");
+    }
+    
+    @Test private static void testMilliseconds() {
+        // Rewrite durations in milliseconds in ISO 8601 format.
+        String result = new Rewriter("(\\d+)\\s*ms") {
+            public String replacement() {
+                long milliseconds = Long.parseLong(group(1));
+                return TimeUtilities.msToIsoString(milliseconds);
+            }
+        }.rewrite("232341243 ms");
+        Assert.equals(result, "P64H32M21.243S");
     }
 }
