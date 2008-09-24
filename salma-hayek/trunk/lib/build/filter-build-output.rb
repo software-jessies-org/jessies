21f7f21057d574a92d3bffd7883f4c5508ffc8b7
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2008-09-24 00:25:13 +0000

    lib/build/filter-build-output.rb: Use a more maintainable style.  There should be no functional change in this check-in.  The functional change follows...

--- salma-hayek/trunk/lib/build/filter-build-output.rb
+++ salma-hayek/trunk/lib/build/filter-build-output.rb
@@ -1,26 +1,41 @@
 #!/usr/bin/ruby -w
+
+def isWorthyOfOutput(line)
+  # We need to match:
+  # universal.make:378: *** Unable to find /bin/javac --- do you only have a JRE installed?.  Stop.
+  # make: *** [recurse] Error 2
+  # gmake[2]: *** [recurse] Error 2
+  if line.match(/\*\*\*/)
+    return true
+  end
+  
+  # We also want to see javac(1) warnings:
+  # src/Test.java:174: warning: [deprecation] toURL() in java.io.File has been deprecated
+  #     URL url = f.toURL();
+  #                ^
+  if line.match(/warning: /)
+    return true
+  end
+  
+  # We also want to see findbugs warnings, which:
+  # /Users/mad/software.jessies.org/work/terminator/src/terminator/FindDialog.java:80:80 IS: Inconsistent synchronization of terminator.FindDialog.formDialog; locked 77% of time (M)
+  # /Users/mad/software.jessies.org/work/terminator/src/terminator/terminal/TerminalControl.java:-1:-1 UuF: Unused field: terminator.terminal.TerminalControl.stepModeReader (M)
+  # But, rather lamely, I'll take the simple, low risk approach of matching the final warning counter.
+  # Warnings generated: 55
+  if line.match(/Warnings generated: /)
+    return true
+  end
+  
+  return false
+end
+
 def filterBuildOutput(inputIo)
   lines = []
   sawError = false
   while line = inputIo.gets()
     line = line.chomp()
     lines << line
-    # We need to match:
-    # universal.make:378: *** Unable to find /bin/javac --- do you only have a JRE installed?.  Stop.
-    # make: *** [recurse] Error 2
-    # gmake[2]: *** [recurse] Error 2
-    
-    # We also want to see javac(1) warnings:
-    # src/Test.java:174: warning: [deprecation] toURL() in java.io.File has been deprecated
-    #     URL url = f.toURL();
-    #                ^
-    
-    # We also want to see findbugs warnings, which:
-    # /Users/mad/software.jessies.org/work/terminator/src/terminator/FindDialog.java:80:80 IS: Inconsistent synchronization of terminator.FindDialog.formDialog; locked 77% of time (M)
-    # /Users/mad/software.jessies.org/work/terminator/src/terminator/terminal/TerminalControl.java:-1:-1 UuF: Unused field: terminator.terminal.TerminalControl.stepModeReader (M)
-    # But, rather lamely, I'll take the simple, low risk approach of matching the final warning counter.
-    # Warnings generated: 55
-    if line.match(/\*\*\*/) || line.match(/warning: /) || line.match(/Warnings generated: /)
+    if isWorthyOfOutput(line)
       $stderr.puts(lines)
       while line = inputIo.gets()
         $stderr.puts(line)
