148dc57f43057de1a24ac873650486956c78607d
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2008-10-16 01:37:59 +0000

    native/all/setsid/setsid.cpp: I don't think there is a portable way out of this dilemma.  Fortunate, then, that it only afflicts us on Windows.  I still don't know how to fix it, but not continuing on regardless is helping me to use Evergreen, to investigate a different issue.

--- salma-hayek/trunk/native/all/setsid/setsid.cpp
+++ salma-hayek/trunk/native/all/setsid/setsid.cpp
@@ -8,11 +8,15 @@ static void ensureCallerIsProcessGroupLeaderAndHasNoControllingTerminal() {
   if (getpid() == getpgrp()) {
     // Thanks to a modification by John Fremlin, Debian's setsid(1) forks in this situation,
     // running the child in the background, which isn't what we'd want.
-    // We don't currently run this program as a process group leader.
+    // On most platforms, we don't currently run this program as a process group leader.
     // We may be hiding a different implementation of setsid, so have the grace to fail with an informative error message.
     // FIXME: This case always happens on Windows, which is very annoying if this setsid is first on Evergreen's PATH.
-    std::cerr << "the software.jessies.org implementation of setsid does not work for process group leaders." << std::endl;
-    exit(EXIT_FAILURE);
+    std::cerr << "The software.jessies.org implementation of setsid does not work for process group leaders." << std::endl;
+    std::cerr << "We are a process group leader, so that Evergreen's kill with a negative pid will work." << std::endl;
+    std::cerr << "But we still have a controlling terminal, so ssh may try to ask for a password." << std::endl;
+    std::cerr << "We can't fork and wait because then Evergreen's kill wouldn't kill the right process group." << std::endl;
+    return;
+    //exit(EXIT_FAILURE);
   }
 
   if (setsid() < 0) {
