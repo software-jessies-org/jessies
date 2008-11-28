8e5da82cd3229817982de195a4b53ed59e12ef61
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2008-11-28 03:47:01 +0000

    lib/scripts/list-ruby-words.rb: collect global variables too.
    lib/data/spelling-exceptions-Ruby: stop hard-coding names we now get for free, and add one we've never had (but which I've always thought an ugly Perlism that nice people don't use anyway).

--- evergreen/trunk/lib/scripts/list-ruby-words.rb
+++ evergreen/trunk/lib/scripts/list-ruby-words.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/ruby -w
 
-# Outputs an unsorted list of unique words found in class and method names
-# via reflection. This is used to seed Evergreen's Ruby spelling exceptions.
+# Outputs an unsorted list of unique words found in variable, class, and method
+# names via reflection. This is used to seed Evergreen's Ruby spelling
+# exceptions.
 # The assumption is that the Ruby interpreter that's called is the Ruby
 # interpreter you're targeting, or is close enough that you don't care.
 
@@ -11,6 +12,7 @@ require "set.rb"
 require "pathname.rb"
 require "thread.rb"
 
+# Collect class and method names.
 names = Set.new()
 ObjectSpace.each_object(Class) {
     |c|
@@ -31,5 +33,20 @@ ObjectSpace.each_object(Class) {
         end
     }
 }
+
+# Collect variable names.
+Kernel.global_variables.each() {
+    |v|
+    # Remove the leading "$", and any "-" (as in "$-I").
+    variable_name = v.to_s().sub(/^[\$]-?/, "")
+    # Ignore variables like "$?" or "$d".
+    if variable_name =~ /[A-Za-z]{2,}/
+        names.add(variable_name)
+    end
+}
+
 # We don't bother sorting, because the calling code does that anyway.
-puts(names.to_a())
+name_list = names.to_a()
+# Drop anything too short to be considered a spelling mistake.
+name_list = name_list.delete_if() { |name| name.size() <= 3 }
+puts(name_list)
