d91ba60f6ac04ae6da753d835df87bc6832d33de
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2009-01-18 22:44:40 +0000

    src/e/edit/AddWorkspaceAction.java: I've been adding and removing workspaces a fair bit recently, mainly but not always as a work-around for having too many files open at once and not being at a convenient point to stop and work out which ones I can close.  When I'm creating an overlapping workspace like this, the Add Workspace dialog could choose defaults for all three fields that would always be perfect.  I never bother manually setting the build target on the "child" workspace to be the same as the "parent" and that's sure to cause me confusion at some point.
    When I've done cd /tmp && apt-get source <package> && cd <package>-<tab> && sgrep <identifier> && evergreen <grep result>, the file ends up on some arbitrary workspace.  I usually want to do more searches in the same package's source.  Here, Add Workspace could populate the dialog with choices that would be perfect some of the time and easily edited the rest of the time.  Yeah, typing /tmp isn't much hassle but the empty defaults has usually led me back to the terminal.

--- evergreen/trunk/src/e/edit/AddWorkspaceAction.java
+++ evergreen/trunk/src/e/edit/AddWorkspaceAction.java
@@ -2,6 +2,7 @@ package e.edit;
 
 import e.util.*;
 import java.awt.event.*;
+import java.io.*;
 import javax.swing.*;
 
 /**
@@ -17,6 +18,24 @@ public class AddWorkspaceAction extends AbstractAction {
         properties.name = "";
         properties.rootDirectory = null;
         properties.buildTarget = null;
+        Workspace workspace = Evergreen.getInstance().getCurrentWorkspace();
+        if (workspace != null) {
+            properties.buildTarget = workspace.getBuildTarget();
+            ETextWindow textWindow = ETextAction.getFocusedTextWindow();
+            if (textWindow != null) {
+                String friendlyDirectory = FileUtilities.getUserFriendlyName(FileUtilities.fileFromString(textWindow.getFilename()).getParent());
+                properties.rootDirectory = friendlyDirectory;
+                if (friendlyDirectory.startsWith(workspace.getRootDirectory())) {
+                    int prefixCharsToSkip = workspace.getRootDirectory().length();
+                    String pathWithinWorkspace = friendlyDirectory.substring(prefixCharsToSkip);
+                    // When adding a workspace which overlaps an existing one, choose a name that will sort close to but after the current workspace.
+                    properties.name = workspace.getTitle() + " " + pathWithinWorkspace;
+                } else {
+                    // Where the file prompting the workspace addition is from elsewhere, the best name is likely to be just one component but deleting is less work than typing.
+                    properties.name = friendlyDirectory;
+                }
+            }
+        }
         
         if (properties.showWorkspacePropertiesDialog("Add Workspace", "Add") == true) {
             Evergreen.getInstance().createWorkspace(properties);
