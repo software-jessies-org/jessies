01cb93e3272e21b795e509f662139e506a26e6af
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2009-03-07 04:16:04 +0000

    src/e/edit/ESwitchButton.java: src/e/edit/ShowCounterpartAction.java: teach anyone who sees the tool tip what the keyboard equivalent is.

--- evergreen/trunk/src/e/edit/ShowCounterpartAction.java
+++ evergreen/trunk/src/e/edit/ShowCounterpartAction.java
@@ -2,13 +2,16 @@ package e.edit;
 
 import e.util.*;
 import java.awt.event.*;
+import javax.swing.*;
 
 /**
  * Switches to the counterpart of the current file (for example, the .h file for a .cpp or vice versa).
  */
 public class ShowCounterpartAction extends ETextAction {
+    public static final KeyStroke KEYSTROKE = GuiUtilities.makeKeyStroke("P", true);
+    
     public ShowCounterpartAction() {
-        super("_Switch to Header/Source", GuiUtilities.makeKeyStroke("P", true));
+        super("_Switch to Header/Source", KEYSTROKE);
     }
     
     @Override public boolean isEnabled() {
