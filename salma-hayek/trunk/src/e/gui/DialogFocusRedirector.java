8e24cbb9dc1654aae760d808dab56b51a67dab0a
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2009-03-07 03:37:16 +0000

    src/e/gui/DialogFocusRedirector.java: comment the only semi-plausible case I can think of where the selectAll heuristic wouldn't be sufficient.

--- salma-hayek/trunk/src/e/gui/DialogFocusRedirector.java
+++ salma-hayek/trunk/src/e/gui/DialogFocusRedirector.java
@@ -37,6 +37,7 @@ public class DialogFocusRedirector {
                 component.requestFocus();
                 // If there's no selection, select all so the user can conveniently replace everything.
                 // If there is a selection, assume that's because the dialog's creator has done something clever.
+                // FIXME: this seems okay for now, but doesn't let us implement a dialog whose first text field should always be appended to. If the need ever arises, use a client property on the JTextField to signal we don't want the selectAll?
                 JTextField textField = (JTextField) component;
                 if (textField.getSelectionStart() == textField.getSelectionEnd()) {
                     textField.selectAll();
