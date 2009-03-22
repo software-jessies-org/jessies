53e066ff6a70ea46a17c8e8b5e3809259423336f
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2009-03-22 21:05:17 +0000

    src/e/edit/EditServer.java: take advantage of ProcessUtilities.spawn's switch to varargs.

--- evergreen/trunk/src/e/edit/EditServer.java
+++ evergreen/trunk/src/e/edit/EditServer.java
@@ -90,7 +90,7 @@ public final class EditServer {
             try {
                 this.window = editor.openFileNonInteractively(filename);
                 if (GuiUtilities.isMacOs()) {
-                    ProcessUtilities.spawn(null, new String[] { FileUtilities.findOnPath("BringProcessToFront").toString() });
+                    ProcessUtilities.spawn(null, FileUtilities.findOnPath("BringProcessToFront").toString());
                 } else {
                     editor.getFrame().toFront();
                 }
