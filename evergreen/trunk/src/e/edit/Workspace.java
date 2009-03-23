1848082e75b1e5623205380a7c956231e95996f0
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2009-03-23 00:40:42 +0000

    src/e/edit/Workspace.java: inline a one-line public method with a single caller in the same class.

--- evergreen/trunk/src/e/edit/Workspace.java
+++ evergreen/trunk/src/e/edit/Workspace.java
@@ -161,14 +161,10 @@ public class Workspace extends JPanel {
         return dirtyTextWindows;
     }
     
-    public EWindow findWindowByName(String name) {
-        return leftColumn.findWindowByName(name);
-    }
-    
     /** Returns the EWindow corresponding to the given file. If the file's open, shows the given address. */
     public EWindow findIfAlreadyOpen(String filename, String address) {
-        /* Check we don't already have this open as a file or directory. */
-        final EWindow window = findWindowByName(filename);
+        // Check we don't already have this open as a file or directory.
+        final EWindow window = leftColumn.findWindowByName(filename);
         if (window != null) {
             leftColumn.setSelectedWindow(window);
             window.ensureSufficientlyVisible();
