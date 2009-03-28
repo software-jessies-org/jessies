0051961d42b32c7e69be3c98e62bc2af7af6ea0a
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2009-03-28 03:15:52 +0000

    src/e/gui/EStatusBar.java: every time I move the cursor left and then right again in Evergreen to get the status bar to redisplay my current location, I think "I should fix that". Some status messages are only worth passing mention, but the main user of this code (Evergreen) would be better served by status messages that remained visible until they were replaced with a more up-to-date message. Removing this behavior also makes EStatusBar more widely applicable.

--- salma-hayek/trunk/src/e/gui/EStatusBar.java
+++ salma-hayek/trunk/src/e/gui/EStatusBar.java
@@ -5,15 +5,9 @@ import java.awt.event.*;
 import javax.swing.*;
 
 /**
- * A simple status bar. Messages time-out after a certain delay.
+ * A simple status bar. Works around the various problems you'd have if you used a JLabel directly.
  */
 public class EStatusBar extends JLabel {
-    /** Tells us when to clear the status bar. */
-    private Timer statusBarClearingTimer;
-    
-    /** How many milliseconds to wait before clearing the status bar. */
-    private static final int STATUS_BAR_CLEAR_DELAY = 20 * 1000;
-    
     /**
      * Constructs a status bar with no initial message.
      */
@@ -26,32 +20,12 @@ public class EStatusBar extends JLabel {
         } else if (GuiUtilities.isMacOs()) {
             setFont(UIManager.getFont("ToolTip.font"));
         }
-        
-        initStatusBarClearingTimer();
-    }
-    
-    /** Initializes the timer. */
-    private void initStatusBarClearingTimer() {
-        statusBarClearingTimer = new Timer(STATUS_BAR_CLEAR_DELAY, new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                clearStatusBar();
-            }
-        });
-        statusBarClearingTimer.setRepeats(false);
     }
     
     /**
      * Sets the text to display in the status bar.
      */
     public void setText(String status) {
-        // Restarts the timer that will cause this message to be hidden after a reasonable delay.
-        // We have to check that the timer's been created because our superconstructor invokes
-        // setText. This is why some people argue that a class' implementation should never
-        // make use of its overrideable public API.
-        if (statusBarClearingTimer != null) {
-            statusBarClearingTimer.restart();
-        }
-        
         // Works around a Swing misfeature whereby a JLabel showing the empty string is zero-height,
         // which is likely to mess up your layout. We substitute a single space for the empty string.
         if (status.length() == 0) {
