ad441dd0adad2e2c43c4b4fbebb2fe15bc2d7898
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2009-04-02 01:38:10 +0000

    native/all/setsid/setsid.cpp: This case is very annoying on Windows even when another setsid is first on Evergreen's PATH.  In fact, it's more annoying then, because you don't get warned to beware of it.

--- salma-hayek/trunk/native/all/setsid/setsid.cpp
+++ salma-hayek/trunk/native/all/setsid/setsid.cpp
@@ -10,7 +10,8 @@ static void ensureCallerIsProcessGroupLeaderAndHasNoControllingTerminal() {
     // running the child in the background, which isn't what we'd want.
     // On most platforms, we don't currently run this program as a process group leader.
     // We may be hiding a different implementation of setsid, so have the grace to fail with an informative error message.
-    // FIXME: This case always happens on Windows, which is very annoying if this setsid is first on Evergreen's PATH.
+    // FIXME: This case always happens on Windows.
+    // How can we divest ourselves of the controlling terminal on Cygwin, even if it means using non-portable code?
     std::cerr << "The software.jessies.org implementation of setsid does not work for process group leaders." << std::endl;
     std::cerr << "We are a process group leader, so that Evergreen's kill with a negative pid will work." << std::endl;
     std::cerr << "But we still have a controlling terminal, so ssh may try to ask for a password." << std::endl;
