0a4295a3e45f22755b4eb897e761d5cf81f61dcf
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2009-04-04 19:14:05 +0000

    native/all/pty/terminator_terminal_PtyProcess.cpp: src/terminator/terminal/PtyProcess.java: the only reason I pulled pid into a local was to make the code look nicer. If it's now making it uglier, it's outlived its usefulness.

--- terminator/trunk/src/terminator/terminal/PtyProcess.java
+++ terminator/trunk/src/terminator/terminal/PtyProcess.java
@@ -44,7 +44,7 @@ public class PtyProcess {
     }
     
     private int fd = -1;
-    private int weaklyTypedPid;
+    private int pid;
     private String slavePtyName;
     
     private boolean didDumpCore = false;
@@ -108,7 +108,7 @@ public class PtyProcess {
     public PtyProcess(String executable, String[] argv, String workingDirectory) throws Exception {
         ensureLibraryLoaded();
         startProcess(executable, argv, workingDirectory);
-        if (weaklyTypedPid == -1) {
+        if (pid == -1) {
             throw new IOException("Could not start process \"" + executable + "\".");
         }
         inStream = new PtyInputStream();
@@ -124,7 +124,7 @@ public class PtyProcess {
     }
     
     public int getPid() {
-        return weaklyTypedPid;
+        return pid;
     }
     
     private void startProcess(final String executable, final String[] argv, final String workingDirectory) throws Exception {
@@ -178,7 +178,7 @@ public class PtyProcess {
     
     @Override
     public String toString() {
-        String result = "PtyProcess[pid=" + weaklyTypedPid + ",fd=" + fd + ",pty=\"" + slavePtyName + "\"";
+        String result = "PtyProcess[pid=" + pid + ",fd=" + fd + ",pty=\"" + slavePtyName + "\"";
         if (didExitNormally) {
             result += ",didExitNormally,exitValue=" + exitValue;
         }
