01128e7d369c9752b560cf0797b1f669d8d87bdb
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2009-04-15 05:07:16 +0000

    src/e/util/FileFinder.java: how many times have I written a simple serial file tree walk with a comment saying "parallelize"? Too many. This one's good enough to replace the ones in Evergreen and mp3 and be used elsewhere.

---
+++ salma-hayek/trunk/src/e/util/FileFinder.java
@@ -0,0 +1,80 @@
+package e.util;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Finds files in a directory tree.
+ * 
+ * FIXME: parallelize internally.
+ * FIXME: allow for parallelism in the caller. return a blocking iterator? accept a Processor functor?
+ */
+public class FileFinder {
+    /**
+     * Used to filter results.
+     * We could use java.io.FileFilter, but having our own interface lets us add more functionality in future.
+     * If we write our own JNI, we can pass in the equivalent of a 'struct stat'.
+     * (Given a 'struct stat', it would be tempting to just have one method, but having two makes it clear that the caller needs to think about directories.)
+     * We could also add explicit interface for asking about symbolic links (instead of calling acceptFile).
+     */
+    public interface Filter {
+        /**
+         * Return true if 'file' should be included in the resulting list of files, false to ignore the file.
+         * This method will be called for all non-directory names in the file system (not just regular files).
+         */
+        public boolean acceptFile(File file);
+        
+        /** Return true if 'directory' should be recursed into, false otherwise. */
+        public boolean enterDirectory(File directory);
+    }
+    
+    private static class DefaultFilter implements Filter {
+        public boolean acceptFile(File file) {
+            return true;
+        }
+        
+        public boolean enterDirectory(File directory) {
+            return true;
+        }
+    }
+    
+    public FileFinder() {
+    }
+    
+    /**
+     * Returns all files under 'root', entering all directories and accepting all files.
+     */
+    public List<File> filesUnder(File root) {
+        return filesUnder(root, null);
+    }
+    
+    /**
+     * Returns files under 'root', using 'filter' to decide which directories to enter and which files to accept.
+     */
+    public List<File> filesUnder(File root, Filter filter) {
+        if (filter == null) {
+            filter = new DefaultFilter();
+        }
+        final ArrayList<File> files = new ArrayList<File>();
+        findFilesInDirectory(files, root, filter);
+        return files;
+    }
+    
+    private void findFilesInDirectory(List<File> result, File directory, Filter filter) {
+        File[] files = directory.listFiles();
+        if (files == null) {
+            return;
+        }
+        for (File file : files) {
+            if (file.isDirectory()) {
+                if (filter.enterDirectory(file)) {
+                    findFilesInDirectory(result, file, filter);
+                }
+            } else {
+                if (filter.acceptFile(file)) {
+                    result.add(file);
+                }
+            }
+        }
+    }
+}
