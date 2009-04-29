e468d9769272a0b0221b3d779b0a7503b2a5fc56
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2009-04-29 06:08:59 +0000

    src/e/edit/SimplePatchDialog.java: use PTextArea to display patches in a style similar to Trac. We use the appropriate PTextArea stylers on the source itself, and color the background to show whether the lines are added, removed, or context. There's support for rendering intraline changes if the information is provided, but in this patch we still call diff(1). One unrelated change I've been meaning to make for some time is to clean up the temporary files as soon as we're finished with them, rather than waiting for the VM to exit.
    src/e/edit/ETextWindow.java: I've made the SimplePatchDialog API make more sense, so the caller has to change.
    
    lib/scripts/ediff.py: this script provides intraline change information. What it doesn't do is replace large runs of unchanged lines with the usual "@@ -a,b +c,d @@" lines, so for now we still call diff(1) instead.

--- evergreen/trunk/src/e/edit/SimplePatchDialog.java
+++ evergreen/trunk/src/e/edit/SimplePatchDialog.java
@@ -4,93 +4,169 @@ import e.forms.*;
 import e.ptextarea.*;
 import e.util.*;
 import java.awt.*;
+import java.io.*;
 import java.util.*;
+import java.util.List;
 import javax.swing.*;
 
 public class SimplePatchDialog {
-    /** Background color for the @@ lines. */
-    private static final Color VERY_LIGHT_GRAY = new Color(230, 230, 230, 128);
+    /** Highlight color for intraline removals. */
+    private static final Color DARK_RED = Color.decode("#ee9999");
     
-    /** Background color for the +++ lines. */
-    private static final Color TRIPLE_PLUS_BACKGROUND = new Color(0xcc, 0xcc, 0xff, 128);
+    /** Highlight color for - lines. */
+    private static final Color LIGHT_RED = Color.decode("#ffdddd");
     
-    /** Background color for the --- lines. */
-    private static final Color TRIPLE_MINUS_BACKGROUND = new Color(0xff, 0xcc, 0xcc, 128);
+    /** Highlight color for intraline additions. */
+    private static final Color DARK_GREEN = Color.decode("#99ee99");
     
-    private static final String PREFIX = "e.edit.SimplePatchDialog-";
+    /** Highlight color for + lines. */
+    private static final Color LIGHT_GREEN = Color.decode("#ddffdd");
+    
+    /** Highlight color for the @@ lines. */
+    private static final Color VERY_LIGHT_GRAY = Color.decode("#eeeeee");
     
     private SimplePatchDialog() {
     }
     
-    public static JComponent makeScrollablePatchView(String fromName, String fromContent, String toName, String toContent) {
-        return new JScrollPane(makePatchView(fromName, fromContent, toName, toContent));
+    public static JComponent makeScrollablePatchView(Font font, String fromName, String fromContent, String toName, String toContent) {
+        return new JScrollPane(makePatchView(font, fromName, fromContent, toName, toContent));
     }
     
-    private static int runDiff(ArrayList<String> lines, ArrayList<String> errors, String fromName, String fromFile, String toName, String toFile, boolean useGnuExtensions) {
-        String[] command = new String[] { "diff", "-u", "-b", "-B", "-L", toName, toFile, "-L", fromName, fromFile };
-        if (useGnuExtensions == false) {
-            // Use only POSIX diff(1) options http://www.opengroup.org/onlinepubs/009695399/utilities/diff.html
-            // One day, son, all this useless variation will be a bad memory.
-            command = new String[] { "diff", "-u", "-b", toFile, fromFile };
-        }
-        return ProcessUtilities.backQuote(null, command, lines, errors);
-    }
-    
-    public static JComponent makePatchView(String fromName, String fromContent, String toName, String toContent) {
-        String fromFile = FileUtilities.createTemporaryFile(PREFIX, "file containing " + fromName, fromContent);
-        String toFile = FileUtilities.createTemporaryFile(PREFIX, "file containing " + toName, toContent);
+    private static List<String> runDiff(String fromName, String fromContent, String toName, String toContent) {
+        final String PREFIX = "e.edit.SimplePatchDialog-";
+        final String fromFile = FileUtilities.createTemporaryFile(PREFIX, "file containing " + fromName, fromContent);
+        final String toFile = FileUtilities.createTemporaryFile(PREFIX, "file containing " + toName, toContent);
         
-        ArrayList<String> lines = new ArrayList<String>();
-        ArrayList<String> errors = new ArrayList<String>();
-        int status = runDiff(lines, errors, fromName, fromFile, toName, toFile, true);
-        if (status == 2) {
-            lines.clear();
-            errors.clear();
-            status = runDiff(lines, errors, fromName, fromFile, toName, toFile, false);
-            lines.add(0, "@@ (You should consider upgrading to GNU diff(1).) @@");
+        final String[] command = new String[] { "diff", "-u", "-b", "-B", "-L", fromName, fromFile, "-L", toName, toFile };
+        //final String[] command = new String[] { Evergreen.getResourceFilename("lib", "scripts", "ediff.py"), fromName, fromFile, toName, toFile };
+        final ArrayList<String> lines = new ArrayList<String>();
+        final ArrayList<String> errors = new ArrayList<String>();
+        final int status = ProcessUtilities.backQuote(null, command, lines, errors);
+        // Output on stderr is not expected, and worth showing to the user.
+        if (lines.size() == 0) {
+            lines.addAll(errors);
         }
-        
+        // POSIX says:
+        //   0 => no differences were found.
+        //   1 => differences were found.
+        //  >1 => an error occurred.
         if (status == 0) {
             lines.add("(No non-whitespace differences.)");
         } else if (status > 1) {
             lines.add("diff(1) failed.");
         }
         
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < lines.size(); ++i) {
-            builder.append(lines.get(i));
-            builder.append('\n');
+        // Clean up the temporary files.
+        FileUtilities.fileFromString(fromFile).delete();
+        FileUtilities.fileFromString(toFile).delete();
+        
+        return lines;
+    }
+    
+    public static JComponent makePatchView(Font font, String fromName, String fromContent, String toName, String toContent) {
+        final PTextArea textArea = new PTextArea(20, 80);
+        textArea.setEditable(false);
+        textArea.setFont(font);
+        
+        // Try to configure the text area appropriately for the specific content.
+        final String probableFilename = fromName.indexOf(File.separatorChar) != -1 ? fromName : toName;
+        final String probableContent = fromContent.length() > toContent.length() ? fromContent : toContent;
+        FileType.guessFileType(probableFilename, probableContent).configureTextArea(textArea);
+        // FIXME: BugDatabaseHighlighter?
+        // FIXME: spelling exceptions?
+        
+        final List<HighlightInfo> highlights = new ArrayList<HighlightInfo>();
+        Color color = null;
+        int lineNumber = 0; // Lines beginning with '?' don't count!
+        for (String line : runDiff(fromName, fromContent, toName, toContent)) {
+            if (line.startsWith("?")) {
+                // A '?' line always follows a '+' or '-' line, so choose the dark color corresponding to the last color we used.
+                highlightDifferencesInLine(highlights, textArea, (color == LIGHT_GREEN) ? DARK_GREEN : DARK_RED, lineNumber - 1, line);
+                continue;
+            } else if (line.startsWith("+++")) {
+                color = DARK_GREEN;
+            } else if (line.startsWith("+")) {
+                color = LIGHT_GREEN;
+            } else if (line.startsWith("---")) {
+                color = DARK_RED;
+            } else if (line.startsWith("-")) {
+                color = LIGHT_RED;
+            } else if (line.startsWith("@@ ")) {
+                color = VERY_LIGHT_GRAY;
+            } else {
+                color = null;
+            }
+            textArea.append(line + "\n");
+            if (color != null) {
+                final int lineStart = textArea.getLineStartOffset(lineNumber);
+                final int lineEnd = textArea.getLineEndOffsetBeforeTerminator(lineNumber) + 1;
+                highlights.add(new HighlightInfo(lineStart, lineEnd, color));
+            }
+            ++lineNumber;
         }
         
-        PTextArea textArea = new PTextArea(10, 80);
-        textArea.setEditable(false);
-        textArea.setTextStyler(new PPatchTextStyler(textArea));
-        textArea.setText(builder.toString());
-        textArea.setFont(ChangeFontAction.getConfiguredFixedFont());
-        
-        for (int i = 0; i < textArea.getLineCount(); ++i) {
-            String lineText = textArea.getLineText(i);
-            Color color = null;
-            if (lineText.startsWith("+++")) {
-                color = TRIPLE_PLUS_BACKGROUND;
-            } else if (lineText.startsWith("---")) {
-                color = TRIPLE_MINUS_BACKGROUND;
-            } else if (lineText.startsWith("@@ ")) {
-                color = VERY_LIGHT_GRAY;
-            }
-            if (color != null) {
-                int start = textArea.getLineStartOffset(i);
-                int end = textArea.getLineEndOffsetBeforeTerminator(i) + 1;
-                textArea.addHighlight(new PPatchTextStyler.PatchHighlight(textArea, start, end, color));
-            }
+        // Apply the collected highlights.
+        // Doing this in a second pass avoids the problems inherent in changing the text and its highlighting at the same time.
+        for (HighlightInfo highlight : highlights) {
+            highlight.apply(textArea);
         }
         
         return textArea;
     }
     
+    private static void highlightDifferencesInLine(List<HighlightInfo> highlights, PTextArea textArea, Color color, int lineNumber, String pattern) {
+        final int lineStart = textArea.getLineStartOffset(lineNumber);
+        for (int i = 1; i < pattern.length();) {
+            if (pattern.charAt(i) == ' ') {
+                ++i;
+            } else {
+                final int highlightStart = lineStart + i;
+                while (i < pattern.length() && pattern.charAt(i) != ' ') {
+                    ++i;
+                }
+                final int highlightEnd = lineStart + i;
+                highlights.add(new HighlightInfo(highlightStart, highlightEnd, color));
+            }
+        }
+    }
+    
+    static class HighlightInfo {
+        int start;
+        int end;
+        Color color;
+        
+        HighlightInfo(int start, int end, Color color) {
+            this.start = start;
+            this.end = end;
+            this.color = color;
+        }
+        
+        void apply(PTextArea textArea) {
+            textArea.addHighlight(new PPatchTextStyler.PatchHighlight(textArea, start, end, color));
+        }
+    }
+    
     public static void showPatchBetween(String title, String fromName, String fromContent, String toName, String toContent) {
-        FormBuilder form = new FormBuilder(Evergreen.getInstance().getFrame(), title);
-        form.getFormPanel().addRow("Differences:", makeScrollablePatchView(fromName, fromContent, toName, toContent));
+        showPatchBetween(Evergreen.getInstance().getFrame(), ChangeFontAction.getConfiguredFixedFont(), title, fromName, fromContent, toName, toContent);
+    }
+    
+    public static void showPatchBetween(Frame parent, Font font, String title, String fromName, String fromContent, String toName, String toContent) {
+        FormBuilder form = new FormBuilder(parent, title);
+        form.getFormPanel().addRow("Differences:", makeScrollablePatchView(font, fromName, fromContent, toName, toContent));
         form.showNonModal();
+    }
+    
+    // For testing from the command line.
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("usage: SimplePatchDialog FILE1 FILE2");
+            System.exit(1);
+        }
+        final String file1 = args[0];
+        final String file2 = args[1];
+        GuiUtilities.initLookAndFeel();
+        final Font font = new Font(GuiUtilities.getMonospacedFontName(), Font.PLAIN, 12);
+        final String title = "Patch between '" + file1 + "' and '" + file2 + "'";
+        showPatchBetween(null, font, title, file1, StringUtilities.readFile(file1), file2, StringUtilities.readFile(file2));
     }
 }
