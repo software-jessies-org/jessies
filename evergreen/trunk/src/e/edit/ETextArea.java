b1b94eee02336e41c1637a3bac23df7d991587d8
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2009-05-10 22:20:07 +0000

    src/e/edit/ETextArea.java: src/e/edit/ETextWindow.java: remove yet more of ETextArea.

--- evergreen/trunk/src/e/edit/ETextArea.java
+++ evergreen/trunk/src/e/edit/ETextArea.java
@@ -8,23 +8,9 @@ import javax.swing.*;
  */
 public class ETextArea extends PTextArea {
     public ETextArea() {
-        // Disable the default find action so we can offer our own.
-        getActionMap().remove(PActionFactory.makeFindAction().getValue(Action.NAME));
     }
     
-    @Override
-    public String reformatPastedText(String pastedText) {
-        // Turn non-breakable spaces into normal spaces.
-        // These are a problem if, for example, you paste a signature from JavaDoc.
-        pastedText = pastedText.replace('\u00a0', ' ');
-        // Turn old-style Mac line endings into something sensible.
-        // These are a problem if, for example, you paste example code from Apple's on-line documentation.
-        pastedText = pastedText.replaceAll("\r\n?", "\n");
-        return pastedText;
-    }
-    
-    @Override
-    protected void updateFindResults() {
+    @Override protected void updateFindResults() {
         ETextWindow textWindow = (ETextWindow) SwingUtilities.getAncestorOfClass(ETextWindow.class, this);
         FindAction.INSTANCE.repeatLastFind(textWindow);
     }
