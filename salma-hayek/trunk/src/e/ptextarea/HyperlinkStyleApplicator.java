7d7ffaf539f91653006d83372c2de0ceb3ee239d
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2009-05-11 01:16:34 +0000

    src/e/ptextarea/HyperlinkStyleApplicator.java: apply a patch from Matt Hillsdon to turn our big comment full of examples into a unit test.

--- salma-hayek/trunk/src/e/ptextarea/HyperlinkStyleApplicator.java
+++ salma-hayek/trunk/src/e/ptextarea/HyperlinkStyleApplicator.java
@@ -1,25 +1,18 @@
 package e.ptextarea;
 
-import e.gui.*;
-import java.util.regex.*;
+import e.gui.WebLinkAction;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.jessies.test.Assert;
+import org.jessies.test.Test;
 
 /**
  * Links to web sites from written-out URLs.
  * 
- * Examples:
- *   http://software.jessies.org
- *   http://software.jessies.org/
- *   http://software.jessies.org/software/make/manual/html_mono/make.html
- *   http://software.jessies.org/viewcvs/gtk%2B/gtk/gtkstock.h?view=markup
- *   <a href="http://software.jessies.org">Software Jessies</a>
- *   "http://software.jessies.org"
- *   (http://software.jessies.org)
- *   (http://software.jessies.org/)
- *   <http://software.jessies.org>
- *   http://software.jessies.org, http://software.jessies.org.
- *   http://software.jessies.org/, http://software.jessies.org/.
- *   http://software.jessies.org/~user/
- *   http://software.jessies.org/~user/page.html#target
+ * See test below for example URLs.
  */
 class HyperlinkStyleApplicator extends RegularExpressionStyleApplicator {
     // This character class and the regular expression below are based on the BNF in RFC 1738.
@@ -42,5 +35,35 @@ class HyperlinkStyleApplicator extends RegularExpressionStyleApplicator {
     protected void configureSegment(PTextSegment segment, Matcher matcher) {
         String url = matcher.group(1);
         segment.setLinkAction(new WebLinkAction("Web Link", url));
+    }
+    
+    @Test private static void testExamples() {
+        assertMatches("http://software.jessies.org");
+        assertMatches("http://software.jessies.org/");
+        assertMatches("http://software.jessies.org/software/make/manual/html_mono/make.html");
+        assertMatches("http://software.jessies.org/viewcvs/gtk%2B/gtk/gtkstock.h?view=markup");
+        assertMatches("<a href=\"http://software.jessies.org\">Software Jessies</a>", "http://software.jessies.org");
+        assertMatches("http://software.jessies.org");
+        assertMatches("(http://software.jessies.org)", "http://software.jessies.org");
+        assertMatches("(http://software.jessies.org/)", "http://software.jessies.org/");
+        assertMatches("<http://software.jessies.org>", "http://software.jessies.org");
+        assertMatches("http://software.jessies.org, http://software.jessies.org.", "http://software.jessies.org", "http://software.jessies.org");
+        assertMatches("http://software.jessies.org/, http://software.jessies.org/.", "http://software.jessies.org/", "http://software.jessies.org/");
+        assertMatches("http://software.jessies.org/~user/");
+        assertMatches("http://software.jessies.org/~user/page.html#target");
+    }
+    
+    private static void assertMatches(final String text) {
+        assertMatches(text, text);
+    }
+    
+    private static void assertMatches(final String text, final String... expectedUrls) {
+        final List<String> expected = Arrays.asList(expectedUrls);
+        final List<String> actual = new ArrayList<String>();
+        final Matcher matcher = LINK_PATTERN.matcher(text);
+        while (matcher.find()) {
+            actual.add(matcher.group(1));
+        }
+        Assert.equals(expected, actual);
     }
 }
