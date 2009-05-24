97bfee88e3aa0690cf8d75ae60873083067e1460
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2009-05-24 21:53:03 +0000

    src/terminator/TerminatorPreferences.java: manually split the preferences between three panes. Also add another dark-on-light alternative.

--- terminator/trunk/src/terminator/TerminatorPreferences.java
+++ terminator/trunk/src/terminator/TerminatorPreferences.java
@@ -6,6 +6,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.*;
 import java.awt.image.*;
+import java.util.*;
 import java.util.List;
 import javax.swing.*;
 
@@ -36,6 +37,7 @@ public class TerminatorPreferences extends Preferences {
      */
     public static final String USE_ALT_AS_META = "useAltAsMeta";
     
+    private static final Color CREAM = new Color(0xfefaea);
     private static final Color LIGHT_BLUE = new Color(0xb3d4ff);
     private static final Color NEAR_BLACK = new Color(0x181818);
     private static final Color NEAR_GREEN = new Color(0x72ff00);
@@ -51,40 +53,52 @@ public class TerminatorPreferences extends Preferences {
         setHelperForClass(Double.class, new AlphaHelper());
         setHelperForClass(Color[].class, new PaletteHelper());
         
-        addPreference(ALPHA, Double.valueOf(1.0), "Terminal opacity");
-        addPreference(ANTI_ALIAS, Boolean.FALSE, "Anti-alias text");
-        addPreference(BLINK_CURSOR, Boolean.TRUE, "Blink cursor");
-        addPreference(BLOCK_CURSOR, Boolean.FALSE, "Use block cursor");
-        addPreference(FANCY_BELL, Boolean.TRUE, "High-quality rendering of the visual bell");
-        addPreference(VISUAL_BELL, Boolean.TRUE, "Visual bell (as opposed to no bell)");
-        addPreference(FONT, new Font(GuiUtilities.getMonospacedFontName(), Font.PLAIN, 12), "Font");
-        addPreference(HIDE_MOUSE_WHEN_TYPING, Boolean.TRUE, "Hide mouse when typing");
-        addPreference(INITIAL_COLUMN_COUNT, Integer.valueOf(80), "New terminal width");
-        addPreference(INITIAL_ROW_COUNT, Integer.valueOf(24), "New terminal height");
-        addPreference(SCROLL_ON_KEY_PRESS, Boolean.TRUE, "Scroll to bottom on key press");
-        addPreference(SCROLL_ON_TTY_OUTPUT, Boolean.FALSE, "Scroll to bottom on output");
-        addPreference(USE_ALT_AS_META, Boolean.FALSE, "Use alt key as meta key (for Emacs)");
+        addTab("Behavior");
+        addTab("Appearance");
+        addTab("Presets");
         
-        addPreference(PALETTE, Palettes.fromString("ANSI"), "Palette");
+        addPreference("Behavior", INITIAL_COLUMN_COUNT, Integer.valueOf(80), "New terminal width");
+        addPreference("Behavior", INITIAL_ROW_COUNT, Integer.valueOf(24), "New terminal height");
+        addPreference("Behavior", SCROLL_ON_KEY_PRESS, Boolean.TRUE, "Scroll to bottom on key press");
+        addPreference("Behavior", SCROLL_ON_TTY_OUTPUT, Boolean.FALSE, "Scroll to bottom on output");
+        addPreference("Behavior", HIDE_MOUSE_WHEN_TYPING, Boolean.TRUE, "Hide mouse when typing");
+        addPreference("Behavior", VISUAL_BELL, Boolean.TRUE, "Visual bell (as opposed to no bell)");
+        addPreference("Behavior", USE_ALT_AS_META, Boolean.FALSE, "Use alt key as meta key (for Emacs)");
+        
+        addPreference("Appearance", ANTI_ALIAS, Boolean.FALSE, "Anti-alias text");
+        addPreference("Appearance", BLINK_CURSOR, Boolean.TRUE, "Blink cursor");
+        addPreference("Appearance", BLOCK_CURSOR, Boolean.FALSE, "Use block cursor");
+        addPreference("Appearance", FANCY_BELL, Boolean.TRUE, "High-quality rendering of the visual bell");
+        addPreference("Appearance", ALPHA, Double.valueOf(1.0), "Terminal opacity");
+        addPreference("Appearance", FONT, new Font(GuiUtilities.getMonospacedFontName(), Font.PLAIN, 12), "Font");
+        addPreference("Appearance", PALETTE, Palettes.fromString("ANSI"), "Palette");
         
         // Defaults reminiscent of SGI's xwsh(1).
-        addPreference(BACKGROUND_COLOR, VERY_DARK_BLUE, "Background");
-        addPreference(CURSOR_COLOR, Color.GREEN, "Cursor");
-        addPreference(FOREGROUND_COLOR, NEAR_WHITE, "Text foreground");
-        addPreference(SELECTION_COLOR, SELECTION_BLUE, "Selection background");
+        addPreference("Appearance", BACKGROUND_COLOR, VERY_DARK_BLUE, "Background");
+        addPreference("Appearance", CURSOR_COLOR, Color.GREEN, "Cursor");
+        addPreference("Appearance", FOREGROUND_COLOR, NEAR_WHITE, "Text foreground");
+        addPreference("Appearance", SELECTION_COLOR, SELECTION_BLUE, "Selection background");
     }
     
-    @Override
-    protected void willAddRows(List<FormPanel> formPanels) {
-        // Offer various preset color combinations.
-        // Note that these get fossilized into the user's preferences; updating values here doesn't affect users who've already clicked the button.
-        formPanels.get(1).addRow("Presets:", makePresetButton("  Terminator  ", VERY_DARK_BLUE, NEAR_WHITE, Color.GREEN, SELECTION_BLUE));
-        formPanels.get(1).addRow("", makePresetButton("Black on White", Color.WHITE, NEAR_BLACK, Color.BLUE, LIGHT_BLUE));
-        formPanels.get(1).addRow("", makePresetButton("Green on Black", Color.BLACK, NEAR_GREEN, Color.GREEN, SELECTION_BLUE));
-        formPanels.get(1).addRow("", makePresetButton("White on Black", Color.BLACK, NEAR_WHITE, Color.GREEN, Color.DARK_GRAY));
+    // Offer various preset color combinations.
+    // Note that these get fossilized into the user's preferences; updating values here doesn't affect users who've already clicked the button.
+    @Override protected void willAddRows(List<FormPanel> formPanels) {
+        ArrayList<JButton> buttons = new ArrayList<JButton>();
+        buttons.add(makePresetButton("White on Blue", VERY_DARK_BLUE, NEAR_WHITE, Color.GREEN, SELECTION_BLUE));
+        buttons.add(makePresetButton("Green on Black", Color.BLACK, NEAR_GREEN, Color.GREEN, SELECTION_BLUE));
+        buttons.add(makePresetButton("White on Black", Color.BLACK, NEAR_WHITE, Color.GREEN, Color.DARK_GRAY));
+        buttons.add(makePresetButton("Black on White", Color.WHITE, NEAR_BLACK, Color.BLUE, LIGHT_BLUE));
+        buttons.add(makePresetButton("Black on Cream", CREAM, NEAR_BLACK, Color.RED, LIGHT_BLUE));
+        ComponentUtilities.tieButtonSizes(buttons);
+        
+        String description = "Presets:";
+        for (JButton button : buttons) {
+            formPanels.get(2).addRow(description, button);
+            description = "";
+        }
     }
     
-    private JComponent makePresetButton(String name, final Color background, final Color foreground, final Color cursor, final Color selection) {
+    private JButton makePresetButton(String name, final Color background, final Color foreground, final Color cursor, final Color selection) {
         // FIXME: ideally, we'd update the button image when the user changes the anti-aliasing preference.
         JButton button = new JButton(new ImageIcon(makePresetButtonImage(name, background, foreground)));
         button.putClientProperty("JButton.buttonType", "gradient"); // Mac OS 10.5
@@ -101,7 +115,7 @@ public class TerminatorPreferences extends Preferences {
     
     private BufferedImage makePresetButtonImage(String name, Color background, Color foreground) {
         // Make a representative image for the button.
-        BufferedImage image = makeEmptyPresetButtonImageToFit(name);
+        BufferedImage image = makeEmptyPresetButtonImage();
         Graphics2D g = image.createGraphics();
         g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, getBoolean(TerminatorPreferences.ANTI_ALIAS) ? RenderingHints.VALUE_TEXT_ANTIALIAS_ON : RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);
         g.setFont(getFont(TerminatorPreferences.FONT));
@@ -119,14 +133,15 @@ public class TerminatorPreferences extends Preferences {
         return image;
     }
     
-    private BufferedImage makeEmptyPresetButtonImageToFit(String name) {
+    private BufferedImage makeEmptyPresetButtonImage() {
         // This seems ugly and awkward, but I can't think of a simpler way to get a suitably-sized BufferedImage to work with, without hard-coding dimensions.
+        // We want all images to be the same width, so we use a constant string rather than the button text.
         BufferedImage image = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
         Graphics2D g = image.createGraphics();
         g.setFont(getFont(TerminatorPreferences.FONT));
         FontMetrics metrics = g.getFontMetrics();
         final int height = (int) (1.4* metrics.getHeight());
-        final int width = (int) (metrics.getStringBounds(name, g).getWidth() * 1.2);
+        final int width = (int) (metrics.getStringBounds("XXXXXXXXXXXXXXXX", g).getWidth() * 1.2);
         g.dispose();
         return new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
     }
@@ -144,7 +159,7 @@ public class TerminatorPreferences extends Preferences {
             return Double.valueOf(valueString);
         }
         
-        public void addRow(List<FormPanel> formPanels, final String key, final String description) {
+        public void addRow(FormPanel formPanel, final String key, final String description) {
             final JSlider slider = new JSlider(0, 255);
             slider.setValue((int) (slider.getMaximum() * getDouble(key)));
             slider.addChangeListener(new javax.swing.event.ChangeListener() {
@@ -154,7 +169,7 @@ public class TerminatorPreferences extends Preferences {
             });
             // Only enable the slider if the JVM seems likely to support setFrameAlpha.
             slider.setEnabled(GuiUtilities.canSetFrameAlpha());
-            formPanels.get(1).addRow(description + ":", slider);
+            formPanel.addRow(description + ":", slider);
         }
     }
     
@@ -167,7 +182,7 @@ public class TerminatorPreferences extends Preferences {
             return Palettes.fromString(valueString);
         }
         
-        public void addRow(List<FormPanel> formPanels, final String key, final String description) {
+        public void addRow(FormPanel formPanel, final String key, final String description) {
             final JComboBox choices = new JComboBox();
             for (String paletteName : Palettes.names()) {
                 choices.addItem(paletteName);
@@ -178,7 +193,7 @@ public class TerminatorPreferences extends Preferences {
                     put(key, decode((String) choices.getSelectedItem()));
                 }
             });
-            formPanels.get(1).addRow(description + ":", choices);
+            formPanel.addRow(description + ":", choices);
         }
     }
 }
