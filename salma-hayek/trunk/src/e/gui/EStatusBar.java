d2c677d9c302c5f459a531669ee20826714a2fd6
enh <enh@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2009-06-07 20:13:07 +0000

    src/e/gui/ELabel.java: src/e/gui/EStatusBar.java: split EStatusBar in two; more places could use "a JLabel that works right" than want a "status bar".

--- salma-hayek/trunk/src/e/gui/EStatusBar.java
+++ salma-hayek/trunk/src/e/gui/EStatusBar.java
@@ -1,37 +1,23 @@
 package e.gui;
 
 import e.util.*;
-import java.awt.event.*;
 import javax.swing.*;
 
 /**
- * A simple status bar. Works around the various problems you'd have if you used a JLabel directly.
+ * A simple status bar.
+ * Doesn't suffer from JLabel's broken empty-string behavior.
+ * Chooses a font suitable for a status bar, which may not be the same as the regular label font.
  */
-public class EStatusBar extends JLabel {
+public class EStatusBar extends ELabel {
     /**
      * Constructs a status bar with no initial message.
      */
     public EStatusBar() {
-        super(" ");
-        
         // The default label fonts are a little large for status bars.
         if (System.getProperty("os.name").contains("Linux")) {
             setFont(UIManager.getFont("Menu.font"));
         } else if (GuiUtilities.isMacOs()) {
             setFont(UIManager.getFont("ToolTip.font"));
         }
-    }
-    
-    /**
-     * Sets the text to display in the status bar.
-     */
-    public void setText(String status) {
-        // Works around a Swing misfeature whereby a JLabel showing the empty string is zero-height,
-        // which is likely to mess up your layout. We substitute a single space for the empty string.
-        if (status.length() == 0) {
-            status = " ";
-        }
-        
-        super.setText(status);
     }
 }
