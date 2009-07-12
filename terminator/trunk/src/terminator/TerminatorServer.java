fb824c487d8a1b29dbe810f7d55b4558a3eaab77
mad <mad@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2009-07-12 05:03:54 +0000

    Try harder to use a single VM.  We've talked about making this change on a number of occasions.  Without it, dragging tabs between windows will be arbitrarily constrained by VM boundaries that are otherwise invisible to the user.  Without the ability to drag tabs between windows, it would be less useful to add an option to start new shells in tabs by default, and some users would like that option.  Plus dragging tabs between windows would be cool.  Making this change will make it more likely that this single VM will run out of heap.  I hope that will encourage us to finally do something about that problem.
    bin/terminator: Send the whole command line and the directory we were started from.  Uri-encode the arguments so that we can use space to separate them without misinterpreting spaces within arguments.  Reword a very BlueArc-centric comment.
    
    src/terminator/TerminatorServer.java: Apply the reverse transformation to the argument string goedelized in bin/terminator, then hand it off to parseCommandLine.
    
    src/terminator/Terminator.java: Now that bin/terminator is adding a --working-directory to the start of every command line, cope with the case where the user supplies a later --working-directory that's relative to that one.  Perhaps no-one uses --working-directory with a relative path but it was one of the first test cases that sprang to mind.
    
    Remove the code that was allegedly to ignore -xrm argument pairs but which was only ignoring the first argument of the pair.  It was getting in the way of passing a List<String> instead of a String[], not irreparably but, if it ain't working, don't maintain it.  Handle the abnormal --help case with an early return.

--- terminator/trunk/src/terminator/TerminatorServer.java
+++ terminator/trunk/src/terminator/TerminatorServer.java
@@ -1,21 +1,26 @@
 package terminator;
 
+import e.util.*;
 import java.io.*;
+import java.util.*;
 
 /**
  * Improves the performance of opening a new shell from the command line or
- * from applications such as the GNOME panel. This should not be extended to
- * execute arbitrary commands without going to appropriate lengths to secure
- * and authenticate the communication.
+ * from applications such as the GNOME panel.
+ * We allow the socket to tell us to execute arbitrary code.
+ * This is only OK because we trust the file security on the secret file.
+ * We don't accept connections except from localhost.
  */
 public class TerminatorServer {
-    public void newShell(PrintWriter out, String line) {
-        try {
-            String workingDirectory = line.substring("newShell ".length());
-            // We don't accept any arguments over the network, originally because that would have been a security hole.
-            Terminator.getSharedInstance().parseCommandLine(new String[] { "--working-directory", workingDirectory }, out);
-        } catch (Exception ex) {
-            ex.printStackTrace(out);
+    public void parseCommandLine(PrintWriter out, String line) {
+        List<String> arguments = new ArrayList<String>();
+        String[] encodedArguments = line.split(" ");
+        for (String encodedArgument : encodedArguments) {
+            String argument = StringUtilities.urlDecode(encodedArgument);
+            arguments.add(argument);
         }
+        // Discard this method's name.
+        arguments.remove(0);
+        Terminator.getSharedInstance().parseCommandLine(arguments, out);
     }
 }
