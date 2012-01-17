1ac19bdc1892c01b69766f12d267bcb99c789e5f
pcn <pcn@0f49fcda-c99d-ebdb-567f-4e9c527d7ea3>
2012-01-17 13:44:55 +0000

    Replaces the old broken auto-indentation for C family languages.  It's not perfect yet, and has a few bugs, but it's at least an improvement.

--- salma-hayek/trunk/src/e/ptextarea/PCppIndenter.java
+++ salma-hayek/trunk/src/e/ptextarea/PCppIndenter.java
@@ -9,7 +9,17 @@ public class PCppIndenter extends PCFamilyIndenter {
     public boolean isInNeedOfClosingSemicolon(String line) {
         return line.matches(".*\\b(class|enum|struct|union)\\b.*");
     }
-
+    
+    @Override
+    protected boolean isAccessSpecifier(String activePartOfLine) {
+        return activePartOfLine.matches("(private|public|protected)\\s*:");
+    }
+    
+    @Override
+    protected boolean isNamespace(String activePartOfLine) {
+        return activePartOfLine.matches("namespace\\s*\\S*\\s*\\{");
+    }
+    
     private static boolean isCppAccessSpecifier(String activePartOfLine) {
         return activePartOfLine.matches("(private|public|protected)\\s*:");
     }
