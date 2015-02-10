c45c1c0ca8a95b8431a335b2518fddd7d905fbb5
martindorey <martindorey@gmail.com>
2015-02-10 00:09:27 +0000

    src/e/edit/EvergreenPreferences.java: src/e/edit/WorkspaceFileList.java: As discussed most recently in https://groups.google.com/d/msg/evergreen-users/QKEkg7sHC1w/T6iJdfYDlN0J, we don't know how to programmatically tell the difference between eg my last several machines, running Debian Lenny, Squeeze and Wheezy, which are unusable during workspace indexing on startup unless I single-thread it, and Elliott's Google-era machines, which iiuc never have.  A preference is a cop-out but it's not nice to penalize MarkK, who doesn't run from source, and there's no sign of the behavior changing, nor of our getting to grips with it.  I wondered about a preferences listener but that would double the size of the code, which doesn't seem worth it for something that you really want to restart to test anyway.

--- evergreen/trunk/src/e/edit/EvergreenPreferences.java
+++ evergreen/trunk/src/e/edit/EvergreenPreferences.java
@@ -13,6 +13,7 @@ public class EvergreenPreferences extends Preferences {
     public static final String SHOW_LINE_NUMBERS = "showLineNumbers";
     public static final String TRIM_TRAILING_WHITESPACE = "trimTrailingWhitespace";
     public static final String UNINTERESTING_EXTENSIONS = "uninterestingExtensions";
+    public static final String MINIMIZE_INDEXING_IO = "minimizeIndexingIo";
     //public static final String BACKGROUND_COLOR = "backgroundColor";
     //public static final String FOREGROUND_COLOR = "foregroundColor";
     
@@ -29,6 +30,7 @@ public class EvergreenPreferences extends Preferences {
         addPreference(ALWAYS_USE_FIXED_FONT, Boolean.TRUE, "Always use fixed font");
         addPreference(HIDE_MOUSE_WHEN_TYPING, Boolean.TRUE, "Hide mouse when typing");
         addPreference(SHOW_LINE_NUMBERS, Boolean.FALSE, "Show line numbers");
+        addPreference(MINIMIZE_INDEXING_IO, Boolean.FALSE, "Minimize I/O when indexing");
         addSeparator();
         addPreference(UNINTERESTING_EXTENSIONS, ".a;.apk;.ap_;.aux;.bak;.bin;.class;.d;.dex;.elf;.eps;.exe;.gif;.hprof;.icns;.iml;.ipr;.iws;.jar;.jpeg;.jpg;.lib;.log;.map;.o;.obj;.orig;.pdf;.png;.ps;.pyc;.pyo;.rej;.so;.swp;.texshop;.tiff;.toc", "Don't index");
         
