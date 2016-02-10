5390e27f5123f8eaf7e6eda79135cf68bc6418bf
Martin Dorey <martindorey@gmail.com>
2016-02-09 23:04:23 -0800

    salma-hayek/lib/User32.rb: There's another set of dl code that needs upgrading to fiddle if we're to be able to show a MessageBox on modern Ruby.

--- salma-hayek/lib/User32.rb
+++ salma-hayek/lib/User32.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/ruby -w
-require "dl"
-require "dl/import"
+require "fiddle"
+require "fiddle/import"
 
 module User32
-    extend(DL::Importer)
+    extend(Fiddle::Importer)
     dlload("user32")
     extern("int MessageBox(const void*, const char*, const char*, int)")
 end
