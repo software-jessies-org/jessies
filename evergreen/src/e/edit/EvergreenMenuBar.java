07cedcc1a3cc918a807fdadb42d937da8954901b
Elliott Hughes <elliott.n.hughes@gmail.com>
2018-07-05 13:28:07 -0700

    evergreen: remove Documentation menu.
    I've kept "Show Documentation for Word" (F1) for now, moving it onto the
    "View" menu. I'm not convinced our current implementation is useful, but
    I do think the concept is useful.

--- evergreen/src/e/edit/EvergreenMenuBar.java
+++ evergreen/src/e/edit/EvergreenMenuBar.java
@@ -16,21 +16,20 @@ public class EvergreenMenuBar extends EMenuBar {
         add(makeScmMenu());
         add(makeWorkspaceMenu());
         add(makeToolsMenu());
-        add(makeDocumentationMenu());
         add(makeHelpMenu());
     }
-    
+
     private static class ExitAction extends AbstractAction {
         public ExitAction() {
             GuiUtilities.configureAction(this, GuiUtilities.isWindows() ? "E_xit" : "_Quit", null);
             GnomeStockIcon.configureAction(this);
         }
-        
+
         public void actionPerformed(ActionEvent e) {
             Evergreen.getInstance().handleQuit(null);
         }
     }
-    
+
     private JMenu makeFileMenu() {
         JMenu menu = GuiUtilities.makeMenu("File", 'F');
         menu.add(new NewFileAction());
@@ -45,10 +44,10 @@ public class EvergreenMenuBar extends EMenuBar {
         menu.add(new SaveAction());
         menu.add(new SaveAllAction());
         menu.add(new RevertToSavedAction());
-        
+
         menu.addSeparator();
         menu.add(new FilePropertiesAction());
-        
+
         if (GuiUtilities.isMacOs() == false) {
             menu.addSeparator();
             menu.add(new JMenuItem(new ExitAction()));
@@ -72,14 +71,14 @@ public class EvergreenMenuBar extends EMenuBar {
         menu.add(new JoinLinesAction());
         menu.add(new CorrectIndentationAction());
         menu.add(new InsertInterfaceAction());
-        
+
         menu.addSeparator();
         menu.add(new ShowMisspellingsAction());
         menu.add(new CompareSelectionAndClipboardAction());
-        
+
         Evergreen.getInstance().getPreferences().initPreferencesMenuItem(menu);
         menu.add(Evergreen.getInstance().getFileTypePreferences().makeShowPreferencesAction("_Filetype Preferences..."));
-        
+
         return menu;
     }
 
@@ -95,47 +94,48 @@ public class EvergreenMenuBar extends EMenuBar {
         menu.addSeparator();
         menu.add(new GoToLineAction());
         menu.add(new GoToTagAction());
-        
+
         menu.addSeparator();
         menu.add(new FindInFilesAction());
-        
+
         menu.addSeparator();
         menu.add(PActionFactory.makeFindMatchingBracketAction());
         menu.add(new ScrollToSelectionAction());
-        
+
         return menu;
     }
-    
+
     private JMenu makeViewMenu() {
         JMenu menu = GuiUtilities.makeMenu("View", 'V');
         menu.add(new ProportionalFontAction());
         menu.add(new FixedFontAction());
         menu.add(new AppropriateFontAction());
-        
+
         menu.addSeparator();
         menu.add(new ShowCounterpartAction());
-        
+        menu.add(new ShowDocumentationAction());
+
         menu.addSeparator();
         menu.add(ShowHideTagsAction.makeMenuItem());
-        
+
         menu.addSeparator();
         menu.add(new CycleWindowsAction(1));
         menu.add(new CycleWindowsAction(-1));
-        
+
         menu.addSeparator();
         menu.add(new ExpandWindowAction());
-        
+
         return menu;
     }
-    
+
     private JMenu makeToolsMenu() {
         final JMenu menu = GuiUtilities.makeMenu("Tools", 'T');
-        
+
         menu.add(new ClearErrorsAction());
-        
+
         menu.addSeparator();
         menu.add(new CheckForLintAction());
-        
+
         menu.addSeparator();
         List<ExternalToolAction> actions = ExternalTools.getAllTools();
         for (ExternalToolAction action : actions) {
@@ -145,58 +145,17 @@ public class EvergreenMenuBar extends EMenuBar {
                 menu.addSeparator();
             }
         }
-        
+
         return menu;
     }
-    
-    private JMenu makeDocumentationMenu() {
-        JMenu menu = GuiUtilities.makeMenu("Documentation", 'D');
-        menu.add(new ShowDocumentationAction());
-        
-        // UI guidelines.
-        menu.addSeparator();
-        menu.add(makeLocalOrRemoteLink("Apple _Human Interface Guidelines", "/Developer/Documentation/DocSets/com.apple.ADC_Reference_Library.CoreReference.docset/Contents/Resources/Documents/documentation/UserExperience/Conceptual/OSXHIGuidelines/index.html", "http://developer.apple.com/documentation/UserExperience/Conceptual/OSXHIGuidelines/index.html"));
-        menu.add(makeLocalOrRemoteLink("_GNOME Human Interface Guidelines", null, "http://library.gnome.org/devel/hig-book/stable/"));
-        
-        // C/C++.
-        menu.addSeparator();
-        menu.add(makeLocalOrRemoteLink("GNU _C Library Documentation", "/usr/share/doc/glibc-doc/html/index.html", "http://www.gnu.org/software/libc/manual/html_node/index.html"));
-        menu.add(makeLocalOrRemoteLink("_POSIX Header Files", null, "http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/contents.html"));
-        menu.add(makeLocalOrRemoteLink("POSIX _Functions", null, "http://pubs.opengroup.org/onlinepubs/9699919799/functions/contents.html"));
-        menu.add(makeLocalOrRemoteLink("_STL Documentation", "/usr/share/doc/stl-manual/html/index.html", "http://www.sgi.com/tech/stl/"));
-        
-        // Java.
-        menu.addSeparator();
-        menu.add(makeLocalOrRemoteLink("_Android API", "~/Downloads/android-sdk-linux/docs/reference/packages.html", "http://developer.android.com/reference/packages.html"));
-        menu.add(makeLocalOrRemoteLink("_Java 6 API", "/usr/share/doc/sun-java6-jdk/html/api/overview-summary.html", "http://java.sun.com/javase/6/docs/api/overview-summary.html"));
-        menu.add(makeLocalOrRemoteLink("Java _Language Specification, 3e", null, "http://java.sun.com/docs/books/jls/third_edition/html/j3TOC.html"));
-        menu.add(makeLocalOrRemoteLink("Java _Tutorial", null, "http://java.sun.com/docs/books/tutorial/"));
-        menu.add(makeLocalOrRemoteLink("Java _VM Specification, 2e", null, "http://java.sun.com/docs/books/vmspec/2nd-edition/html/VMSpecTOC.doc.html"));
-        
-        // Tools.
-        menu.addSeparator();
-        menu.add(makeLocalOrRemoteLink("GNU _Make Manual", null, "http://www.gnu.org/software/make/manual/make.html"));
-        
-        // General.
-        menu.addSeparator();
-        // FIXME: some day it would be nice to have our own regular expression quick reference.
-        menu.add(makeLocalOrRemoteLink("_Regular Expression Documentation", null, PatternUtilities.DOCUMENTATION_URL));
-        
-        return menu;
-    }
-    
-    private WebLinkAction makeLocalOrRemoteLink(String name, String localFilename, String remoteUrl) {
-        String url = (localFilename != null && FileUtilities.exists(localFilename)) ? "file://" + FileUtilities.fileFromString(localFilename) : remoteUrl;
-        return new WebLinkAction(name, url);
-    }
-    
+
     private JMenu makeScmMenu() {
         JMenu menu = GuiUtilities.makeMenu("SCM", 'S');
         menu.add(new CheckInChangesAction());
         menu.add(new ShowHistoryAction());
         return menu;
     }
-    
+
     private JMenu makeWorkspaceMenu() {
         JMenu menu = GuiUtilities.makeMenu("Workspace", 'W');
         menu.add(new NewWorkspaceAction());
@@ -212,12 +171,12 @@ public class EvergreenMenuBar extends EMenuBar {
         menu.add(new CycleWorkspacesAction(-1));
         return menu;
     }
-    
+
     private JMenu makeHelpMenu() {
         HelpMenu helpMenu = new HelpMenu();
         return helpMenu.makeJMenu();
     }
-    
+
     @Override protected boolean processKeyBinding(KeyStroke ks, KeyEvent event, int condition, boolean pressed) {
         int modifier = KeyEvent.ALT_MASK;
         if ((event.getModifiers() & modifier) == modifier) {
