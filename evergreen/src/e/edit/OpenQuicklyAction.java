735acfc338b684bd324e27c68eab11a8c40f922e
Phil Norman <philnorm@gmail.com>
2018-11-23 20:28:38 +0100

    evergreen/src/e/edit/OpenQuicklyAction.java: Don't rely entirely on being able to open the selection as a file, just because it starts with '/' or '~'. Rather, attempt to, but if we can't, fall back to opening the 'find quickly' dialog. This is way better behaviour than just popping up an error box saying "I couldn't open '// This is a comment.'" due to a random selection.

--- evergreen/src/e/edit/OpenQuicklyAction.java
+++ evergreen/src/e/edit/OpenQuicklyAction.java
@@ -22,9 +22,13 @@ public class OpenQuicklyAction extends AbstractAction {
         final String filename = ETextAction.getSelectedText();
         // If we have an absolute name, we can go straight there.
         // Avoid selections with newlines in: any selection that starts with a comment would look like a filename!
+        // We also only *try* to open the file. If no such file exists, we swallow the exception and
+        // fall back to showing the 'Open Quickly' dialog.
         if (filename.indexOf('\n') == -1 && (filename.startsWith("~") || filename.startsWith("/"))) {
-            Evergreen.getInstance().openFile(filename);
-            return;
+            try {
+                Evergreen.getInstance().openFileNonInteractively(filename);
+                return;
+            } catch (RuntimeException ex) {}
         }
         Evergreen.getInstance().getCurrentWorkspace().showOpenQuicklyDialog(ETextAction.getSelectedRegularExpression());
     }
