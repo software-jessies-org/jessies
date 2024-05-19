868f54ba50c4d9ac79b26b3a9b003c0ec4b9a746
Phil Norman <philnorm@gmail.com>
2024-05-19 09:38:53 +0200

    Remove the shift+ctrl+t hotkey for "reopen last-closed file", as we already use that for "open the unit test file" (which is more useful). This functionality will remain as a menu option only.

--- evergreen/src/e/edit/ReopenLastClosedAction.java
+++ evergreen/src/e/edit/ReopenLastClosedAction.java
@@ -10,10 +10,12 @@ import javax.swing.*;
  * Reopens the last file to be closed in the current workspace.
  * This is similar to how ctrl+shift+t works in Chrome, except that we restrict which file is
  * reopened to be within the current workspace, which is probably closer to what the user expects.
+ * We don't use ctrl+shift+t for this in Evergreen, because that hotkey opens the associated
+ * unit test file for the focused file (which is more useful).
  */
 public class ReopenLastClosedAction extends AbstractAction {
     public ReopenLastClosedAction() {
-        GuiUtilities.configureAction(this, "R_eopen last-closed file", GuiUtilities.makeKeyStroke("T", true));
+        GuiUtilities.configureAction(this, "R_eopen last-closed file", null);
     }
     
     @Override public boolean isEnabled() {
